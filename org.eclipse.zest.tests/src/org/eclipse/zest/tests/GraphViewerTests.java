0c3936116ff5e160055d3cb40c05577ef19b62a5
Fabian Steeg <fsteeg@gmail.com>
2011-01-11 19:50:36 +0100

    Added test to verify fix for bug 200732 also fixes 334009

--- org.eclipse.zest.tests/src/org/eclipse/zest/tests/GraphViewerTests.java
+++ org.eclipse.zest.tests/src/org/eclipse/zest/tests/GraphViewerTests.java
@@ -11,9 +11,12 @@ package org.eclipse.zest.tests;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.eclipse.jface.util.DelegatingDragAdapter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTarget;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.zest.core.viewers.GraphViewer;
 
@@ -26,15 +29,37 @@ import org.eclipse.zest.core.viewers.GraphViewer;
 public class GraphViewerTests extends TestCase {
 
 	private GraphViewer viewer;
+	private Shell shell;
+
+	/**
+	 * Set up the shell and viewer to use in the tests.
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	public void setUp() {
+		shell = new Shell();
+		viewer = new GraphViewer(shell, SWT.NONE);
+	}
 
 	/**
 	 * Create a drop target on a viewer's control and check disposal (see
 	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=200732)
 	 */
 	public void testDisposalWithDropTarget() {
-		Shell shell = new Shell();
-		viewer = new GraphViewer(shell, SWT.NONE);
 		new DropTarget(viewer.getGraphControl(), DND.DROP_MOVE | DND.DROP_COPY);
+		shell.dispose();
+		Assert.assertTrue("The viewer's graph control should be disposed",
+				viewer.getControl().isDisposed());
+	}
+
+	/**
+	 * Create a drag source on a viewer and check disposal (see
+	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=334009)
+	 */
+	public void testDisposalWithDragSource() {
+		viewer.addDragSupport(DND.DROP_MOVE,
+				new Transfer[] { TextTransfer.getInstance() },
+				new DelegatingDragAdapter());
 		shell.dispose();
 		Assert.assertTrue("The viewer's graph control should be disposed",
 				viewer.getControl().isDisposed());
