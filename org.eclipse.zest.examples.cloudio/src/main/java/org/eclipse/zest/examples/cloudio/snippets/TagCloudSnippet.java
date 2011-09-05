2c409bc4663819622c0ef92ec5ac125698c11f00
Stephan Schwiebert <schwiebert@spinfo.uni-koeln.de>
2011-09-05 20:53:11 +0200

    added snippets to demonstrate how to use TagCloud and TagCloudViewer.
    Change-Id: Ia9d553028c1fd0babb5657663de86e2549da8864

---
+++ org.eclipse.zest.examples.cloudio/src/main/java/org/eclipse/zest/examples/cloudio/snippets/TagCloudSnippet.java
@@ -0,0 +1,54 @@
+package org.eclipse.zest.examples.cloudio.snippets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.zest.cloudio.TagCloud;
+import org.eclipse.zest.cloudio.Word;
+
+/**
+ * This snippet demonstrates how to create a simple tag cloud,
+ * which will display the words "Hello" and "Cloudio".
+ * 
+ * @author sschwieb
+ *
+ */
+public class TagCloudSnippet {
+
+    public static void main(String [] args) {
+        final Display display = new Display();
+        final Shell shell = new Shell(display);
+        TagCloud cloud = new TagCloud(shell, SWT.NONE);
+
+        // Generate some dummy words - color, weight and fontdata must
+        // always be defined.
+        List<Word> words = new ArrayList<Word>();
+        Word w = new Word("Hello");
+        w.setColor(display.getSystemColor(SWT.COLOR_DARK_CYAN));
+        w.weight = 1;
+        w.setFontData(cloud.getFont().getFontData().clone());
+        words.add(w);
+        w = new Word("Cloudio");
+        w.setColor(display.getSystemColor(SWT.COLOR_DARK_GREEN));
+        w.setFontData(cloud.getFont().getFontData().clone());
+        w.weight = 0.5;
+        w.angle = -45;
+        words.add(w);
+
+        shell.setBounds(50,50, 300, 300);
+        cloud.setBounds(0,0, shell.getBounds().width, shell.getBounds().height);
+
+        // Assign the list of words to the cloud:
+        cloud.setWords(words, null);
+
+        shell.open();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) display.sleep();
+        }
+        display.dispose();
+    }
+
+}
