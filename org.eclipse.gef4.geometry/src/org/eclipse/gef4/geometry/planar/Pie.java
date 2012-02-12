45ef63920aa59b7a66c9b442dba2e7ac84def9cb
anyssen <alexander.nyssen@itemis.de>
2012-02-12 23:29:58 +0100

    Added BezierSpline, Pie, Region, and Ring classes (not implemented yet).

---
+++ org.eclipse.gef4.geometry/src/org/eclipse/gef4/geometry/planar/Pie.java
@@ -0,0 +1,5 @@
+package org.eclipse.gef4.geometry.planar;
+
+public class Pie implements IGeometry {
+
+}
---
+++ org.eclipse.gef4.geometry/src/org/eclipse/gef4/geometry/planar/Region.java
@@ -0,0 +1,16 @@
+package org.eclipse.gef4.geometry.planar;
+
+/**
+ * a combinatin of rectangles...
+ * 
+ * @author nyssen
+ * 
+ */
+public class Region extends AbstractGeometry implements IPolyShape {
+
+	public Rectangle[] getShapes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
