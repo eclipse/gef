64d39354447be8598382d593c11dc4a71426f79f
mwienand <matthias.wienand@itemis.de>
2013-06-14 17:12:42 +0200

    [410772] Add EventHandlerManagerTests to AllTests.

--- org.eclipse.gef4.swt.canvas.tests/src/org/eclipse/gef4/swt/canvas/test/AllTests.java
+++ org.eclipse.gef4.swt.canvas.tests/src/org/eclipse/gef4/swt/canvas/test/AllTests.java
@@ -17,7 +17,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ EventDispatchChainTests.class })
+@SuiteClasses({ EventDispatchChainTests.class, EventHandlerManagerTests.class })
 public class AllTests {
 
 }
