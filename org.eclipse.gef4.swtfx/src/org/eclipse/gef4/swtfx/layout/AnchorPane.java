b25bcb6d3d9c0ce9178a71bbb3c03559690274b0
mwienand <matthias.wienand@itemis.de>
2013-10-17 13:40:30 +0200

    [410772] Refactor AnchorPane.

--- org.eclipse.gef4.swtfx/src/org/eclipse/gef4/swtfx/layout/AnchorPane.java
+++ org.eclipse.gef4.swtfx/src/org/eclipse/gef4/swtfx/layout/AnchorPane.java
@@ -15,6 +15,7 @@ package org.eclipse.gef4.swtfx.layout;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.gef4.geometry.planar.Rectangle;
 import org.eclipse.gef4.swtfx.INode;
 
 public class AnchorPane extends Pane {
@@ -29,11 +30,90 @@ public class AnchorPane extends Pane {
 		this.constraints.put(node, constraints);
 	}
 
+	@Override
+	public double computePrefHeight(double width) {
+		// return super.computePrefHeight(width);
+		return computePrefRect().getHeight();
+	}
+
+	private Rectangle computePrefRect() {
+		double xMin = Double.MAX_VALUE;
+		double xMax = Double.MIN_VALUE;
+		double yMin = xMin;
+		double yMax = xMax;
+
+		for (INode child : getManagedChildren()) {
+			Rectangle rect = computePrefRect(child);
+
+			xMin = Math.min(xMin, rect.getX());
+			xMax = Math.max(xMax, rect.getX() + rect.getWidth());
+			yMin = Math.min(yMin, rect.getY());
+			yMax = Math.max(yMax, rect.getY() + rect.getHeight());
+		}
+
+		return new Rectangle(xMin, yMin, xMax - xMin, yMax - yMin);
+	}
+
+	private Rectangle computePrefRect(INode child) {
+		AnchorPaneConstraints c = constraints(child);
+		Double left = c.getLeft();
+		Double bottom = c.getBottom();
+		Double right = c.getRight();
+		Double top = c.getTop();
+
+		double w = child.computePrefWidth(-1);
+		double h = child.computePrefHeight(-1);
+
+		double horizontalConstraints = (left == null ? 0 : left)
+				+ (right == null ? 0 : right);
+		double verticalConstraints = (top == null ? 0 : top)
+				+ (bottom == null ? 0 : bottom);
+
+		return new Rectangle(0, 0, w + horizontalConstraints, h
+				+ verticalConstraints);
+	}
+
+	@Override
+	public double computePrefWidth(double height) {
+		// return super.computePrefWidth(height);
+		return computePrefRect().getWidth();
+	}
+
 	private AnchorPaneConstraints constraints(INode child) {
 		if (!constraints.containsKey(child)) {
 			constraints.put(child, new AnchorPaneConstraints());
 		}
 		return constraints.get(child);
+	}
+
+	private Rectangle getPrefRect(INode child) {
+		AnchorPaneConstraints c = constraints(child);
+		Double left = c.getLeft();
+		Double bottom = c.getBottom();
+		Double right = c.getRight();
+		Double top = c.getTop();
+
+		double w = child.computePrefWidth(-1);
+		double h = child.computePrefHeight(-1);
+
+		double x = left == null ? right == null ? 0 : getWidth() - right - w
+				: left;
+		double y = top == null ? bottom == null ? 0 : getHeight() - bottom - h
+				: top;
+		double x2 = right == null ? x + w : getWidth() - right;
+		double y2 = bottom == null ? y + h : getHeight() - bottom;
+
+		w = x2 - x;
+		h = y2 - y;
+
+		if (w < 0) {
+			w = 0;
+		}
+		if (h < 0) {
+			h = 0;
+		}
+
+		return new Rectangle(x, y, w, h);
 	}
 
 	@Override
@@ -46,38 +126,14 @@ public class AnchorPane extends Pane {
 		// availableHeight);
 
 		for (INode child : getManagedChildren()) {
-			AnchorPaneConstraints c = constraints(child);
-			Double left = c.getLeft();
-			Double bottom = c.getBottom();
-			Double right = c.getRight();
-			Double top = c.getTop();
-
-			double w = child.computePrefWidth(-1);
-			double h = child.computePrefHeight(-1);
-
-			double x = left == null ? right == null ? 0 : getWidth() - right
-					- w : left;
-			double y = top == null ? bottom == null ? 0 : getHeight() - bottom
-					- h : top;
-			double x2 = right == null ? x + w : getWidth() - right;
-			double y2 = bottom == null ? y + h : getHeight() - bottom;
-
-			w = x2 - x;
-			h = y2 - y;
-
-			if (w < 0) {
-				w = 0;
-			}
-			if (h < 0) {
-				h = 0;
-			}
+			Rectangle rect = getPrefRect(child);
 
 			// System.out.println("anchor " + child + " at " + x + ", " + y
 			// + " sized " + w + " x " + h);
 
-			child.relocate(x, y);
+			child.relocate(rect.getX(), rect.getY());
 			if (child.isResizable()) {
-				child.resize(x2 - x, y2 - y);
+				child.resize(rect.getWidth(), rect.getHeight());
 			}
 		}
 
