e09afe59b17349506efdbf7a4f98da2211c0a06c
mwienand <matthias.wienand@itemis.de>
2013-11-17 20:37:47 +0100

    [410772] Enhanced SwtFXLayoutSnippet.

--- org.eclipse.gef4.swtfx.examples/src/org/eclipse/gef4/swtfx/examples/snippets/SwtFXLayoutSnippet.java
+++ org.eclipse.gef4.swtfx.examples/src/org/eclipse/gef4/swtfx/examples/snippets/SwtFXLayoutSnippet.java
@@ -11,15 +11,24 @@ import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
+import org.eclipse.gef4.geometry.planar.CurvedPolygon;
 import org.eclipse.gef4.geometry.planar.Ellipse;
 import org.eclipse.gef4.geometry.planar.IShape;
-import org.eclipse.gef4.geometry.planar.Polygon;
+import org.eclipse.gef4.geometry.planar.Point;
+import org.eclipse.gef4.geometry.planar.PolyBezier;
 import org.eclipse.gef4.swtfx.GeometryNode;
 import org.eclipse.gef4.swtfx.SwtFXScene;
 import org.eclipse.gef4.swtfx.controls.SwtFXButton;
 import org.eclipse.gef4.swtfx.examples.SwtFXApplication;
 
 public class SwtFXLayoutSnippet extends SwtFXApplication {
+
+	// color theme
+	final public static double[] C0 = { 0.52, 0.49, 0.15 };
+	final public static double[] C1 = { 1, 1, 1 };
+	final public static double[] C2 = { 0.38, 0.08, 0.03 };
+	final public static double[] C3 = { 0.49, 0.36, 0.20 };
+	final public static double[] C4 = { 0.87, 0.83, 0.49 };
 
 	private static <T extends IShape> Node gef4shape(T shape, double r,
 			double g, double b) {
@@ -48,24 +57,31 @@ public class SwtFXLayoutSnippet extends SwtFXApplication {
 		HBox.setHgrow(col1, Priority.ALWAYS);
 		HBox.setHgrow(col2, Priority.ALWAYS);
 
-		col1.getChildren().addAll(new Button("JavaFX 1"),
-				gef4shape(new Polygon(50, 0, 100, 100, 0, 100), 0, 1, 0),
+		col1.getChildren().addAll(
+				new Button("JavaFX 1"),
+				gef4shape(
+						new CurvedPolygon(PolyBezier.interpolateCubic(
+								new Point(45, 45), new Point(40, 75),
+								new Point(25, 100), new Point(75, 75),
+								new Point(115, 100), new Point(115, 50),
+								new Point(75, 25), new Point(45, 45))
+								.toBezier()), C0[0], C0[1], C0[2]),
 				shape(new Arc(0, 0, 50, 50, 15, 120) {
 					{
 						setType(ArcType.ROUND);
 					}
-				}, 0, 1, 1), new SwtFXButton("SwtFX 1"));
+				}, C1[0], C1[1], C1[2]), new SwtFXButton("SwtFX 1"));
 
 		col2.getChildren().addAll(
-				gef4shape(new Ellipse(0, 0, 60, 80), 1, 0, 0),
-				shape(new Rectangle(0, 0, 100, 50), 0, 0, 1),
+				gef4shape(new Ellipse(0, 0, 60, 80), C2[0], C2[1], C2[2]),
+				shape(new Rectangle(0, 0, 100, 50), C3[0], C3[1], C3[2]),
 				new SwtFXButton("SwtFX 2"),
 				shape(new Rectangle(0, 0, 100, 100) {
 					{
 						setArcHeight(20);
 						setArcWidth(20);
 					}
-				}, 1, 0, 1), new Button("JavaFX 2"));
+				}, C4[0], C4[1], C4[2]), new Button("JavaFX 2"));
 
 		return new SwtFXScene(hbox, 400, 400);
 	}
