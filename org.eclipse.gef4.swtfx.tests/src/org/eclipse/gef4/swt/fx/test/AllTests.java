28523a389b7ae41046f1acd64c990883c16039b7
anyssen <alexander.nyssen@itemis.de>
2013-11-18 12:48:04 +0100

    [NONE] Remove reference to DummyTests.class (causing compilation problem).

--- org.eclipse.gef4.swtfx.tests/src/org/eclipse/gef4/swt/fx/test/AllTests.java
+++ org.eclipse.gef4.swtfx.tests/src/org/eclipse/gef4/swt/fx/test/AllTests.java
@@ -17,6 +17,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DummyTests.class })
+@SuiteClasses({})
 public class AllTests {
 }
