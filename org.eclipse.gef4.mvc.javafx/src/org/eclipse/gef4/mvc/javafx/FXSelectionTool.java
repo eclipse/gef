7482d847fff0514e9816c572b7165716cf91c34a
mwienand <matthias.wienand@itemis.de>
2013-12-05 13:22:54 +0100

    [NONE] Fix FXSelectionTool behavior.
    The FXSelectionTool uses event handlers now so that consumed events will not be
    handled by the tool. Therefore, scrolling does not deselect anymore.

--- org.eclipse.gef4.mvc.javafx/src/org/eclipse/gef4/mvc/javafx/FXSelectionTool.java
+++ org.eclipse.gef4.mvc.javafx/src/org/eclipse/gef4/mvc/javafx/FXSelectionTool.java
@@ -1,8 +1,12 @@
 package org.eclipse.gef4.mvc.javafx;
 
 import javafx.event.EventHandler;
+import javafx.event.EventTarget;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 import org.eclipse.gef4.mvc.aspects.selection.AbstractSelectionTool;
 import org.eclipse.gef4.mvc.parts.IContentPart;
@@ -17,31 +21,52 @@ public class FXSelectionTool extends AbstractSelectionTool<Node> {
 		@Override
 		public void handle(MouseEvent event) {
 			IVisualPart<Node> targetPart = getTargetPart(event);
-			if(targetPart == null || targetPart instanceof IRootVisualPart) {
-				select(null, false);
+			if (targetPart == null) {
+				return;
 			}
-			else if (targetPart instanceof IContentPart) {
-				if (select((IContentPart<Node>) targetPart, event.isControlDown())) {
-//					event.consume();
-				}
-			}
-			else if (targetPart instanceof IHandlePart){
-				IHandlePart<Node> handlePart = (IHandlePart<Node>)targetPart;
+
+			boolean append = event.isControlDown();
+
+			if (targetPart instanceof IRootVisualPart) {
+				select(null, append);
+			} else if (targetPart instanceof IContentPart) {
+				select((IContentPart<Node>) targetPart, append);
+			} else if (targetPart instanceof IHandlePart) {
+				IHandlePart<Node> handlePart = (IHandlePart<Node>) targetPart;
 				ITool<Node> handleTool = handlePart.getHandleTool();
 				getDomain().pushTool(handleTool);
-			}
-			else {
+			} else {
 				throw new IllegalArgumentException("Unsupported part type.");
 			}
 		}
 	};
 
 	protected IVisualPart<Node> getTargetPart(MouseEvent event) {
-		IVisualPart<Node> newSelection = getDomain().getViewer()
-				.getVisualPartMap().get(((Node) event.getTarget()));
-		if (newSelection instanceof IVisualPart) {
-			return (IVisualPart<Node>) newSelection;
+		EventTarget target = event.getTarget();
+		
+		if (target instanceof Node) {
+			Node targetNode = (Node) target;
+			
+			// look for the Node in the visual-part-map
+			IVisualPart<Node> newSelection = getDomain().getViewer()
+				.getVisualPartMap().get(targetNode);
+			if (newSelection instanceof IVisualPart) {
+				return (IVisualPart<Node>) newSelection;
+			}
+		
+			// try to find the root visual in the target node's parent hierarchy
+			FXRootVisualPart rootPart = (FXRootVisualPart) getDomain().getViewer()
+				.getRootPart();
+			Node rootVisual = rootPart.getVisual();
+			while (targetNode != null && targetNode != rootVisual) {
+				targetNode = targetNode.getParent();
+			}
+			
+			if (targetNode == rootVisual) {
+				return rootPart;
+			}
 		}
+		
 		return null;
 	}
 
@@ -49,13 +74,13 @@ public class FXSelectionTool extends AbstractSelectionTool<Node> {
 	public void activate() {
 		super.activate();
 		((FXViewer) getDomain().getViewer()).getCanvas().getScene()
-				.addEventFilter(MouseEvent.MOUSE_PRESSED, pressedFilter);
+				.addEventHandler(MouseEvent.MOUSE_PRESSED, pressedFilter);
 	}
 
 	@Override
 	public void deactivate() {
 		((FXViewer) getDomain().getViewer()).getCanvas().getScene()
-				.removeEventFilter(MouseEvent.MOUSE_PRESSED, pressedFilter);
+				.removeEventHandler(MouseEvent.MOUSE_PRESSED, pressedFilter);
 		super.deactivate();
 	}
 
