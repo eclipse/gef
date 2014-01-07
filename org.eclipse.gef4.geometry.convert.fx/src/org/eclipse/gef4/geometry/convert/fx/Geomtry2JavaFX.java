45d952e471c2201406baad1ece659e25457bc92d
anyssen <alexander.nyssen@itemis.de>
2014-01-07 16:51:20 +0100

    [NONE] Add GEF4 Geometry JavaFX conversion support.

---
+++ org.eclipse.gef4.geometry.convert.fx/src/org/eclipse/gef4/geometry/convert/fx/Geomtry2JavaFX.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Ny��en (itemis AG) - initial API and implementation
+ *     Matthias Wienand (itemis AG) - remove AffineTransform from resulting PathIterator
+ *     
+ *******************************************************************************/
+package org.eclipse.gef4.geometry.convert.fx;
+
+public class Geomtry2JavaFX {
+
+	private Geomtry2JavaFX() {
+		// this class should not be instantiated by clients
+	}
+}
---
+++ org.eclipse.gef4.geometry.convert.fx/src/org/eclipse/gef4/geometry/convert/fx/JavaFX2Geometry.java
@@ -0,0 +1,8 @@
+package org.eclipse.gef4.geometry.convert.fx;
+
+public class JavaFX2Geometry {
+
+	private JavaFX2Geometry() {
+		// this class should not be instantiated by clients
+	}
+}
