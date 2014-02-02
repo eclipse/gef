ce26d1eb4cd10acd4fcb8da86b74e57a3ff6afd8
anyssen <alexander.nyssen@itemis.de>
2014-02-02 19:25:02 +0100

    [NONE] Simplified implementation by using a single listener.

--- org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/parts/AbstractFXHandlePart.java
+++ org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/parts/AbstractFXHandlePart.java
@@ -2,7 +2,6 @@ package org.eclipse.gef4.mvc.fx.parts;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.geometry.Bounds;
 import javafx.scene.Node;
 
 import org.eclipse.gef4.mvc.anchors.IAnchor;
@@ -10,39 +9,27 @@ import org.eclipse.gef4.mvc.parts.AbstractHandlePart;
 
 abstract public class AbstractFXHandlePart extends AbstractHandlePart<Node> {
 
-	private ChangeListener<Number> positionChangeListener = new ChangeListener<Number>() {
+	private ChangeListener<Object> changeListener = new ChangeListener<Object>() {
 		@Override
-		public void changed(ObservableValue<? extends Number> observable,
-				Number oldValue, Number newValue) {
-			refreshVisual();
-		}
-	};
-
-	private ChangeListener<Bounds> boundsChangeListener = new ChangeListener<Bounds>() {
-		@Override
-		public void changed(ObservableValue<? extends Bounds> observable,
-				Bounds oldValue, Bounds newValue) {
+		public void changed(ObservableValue<?> observable, Object oldValue,
+				Object newValue) {
 			refreshVisual();
 		}
 	};
 
 	public void attachVisualToAnchorageVisual(Node anchorageVisual,
 			org.eclipse.gef4.mvc.anchors.IAnchor<Node> anchor) {
-		anchorageVisual.layoutXProperty().addListener(positionChangeListener);
-		anchorageVisual.layoutYProperty().addListener(positionChangeListener);
-		anchorageVisual.layoutBoundsProperty()
-				.addListener(boundsChangeListener);
+		anchorageVisual.layoutXProperty().addListener(changeListener);
+		anchorageVisual.layoutYProperty().addListener(changeListener);
+		anchorageVisual.layoutBoundsProperty().addListener(changeListener);
 	};
 
 	@Override
 	public void detachVisualFromAnchorageVisual(Node anchorageVisual,
 			IAnchor<Node> anchor) {
-		anchorageVisual.layoutXProperty()
-				.removeListener(positionChangeListener);
-		anchorageVisual.layoutYProperty()
-				.removeListener(positionChangeListener);
-		anchorageVisual.layoutBoundsProperty().removeListener(
-				boundsChangeListener);
+		anchorageVisual.layoutXProperty().removeListener(changeListener);
+		anchorageVisual.layoutYProperty().removeListener(changeListener);
+		anchorageVisual.layoutBoundsProperty().removeListener(changeListener);
 	}
 
 }
