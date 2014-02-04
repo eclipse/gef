dd039da36412843d91bb16abd72fc3e1a3d1949c
anyssen <alexander.nyssen@itemis.de>
2014-02-04 20:46:03 +0100

    [NONE] Open FXRootPart for subclassing (manipulating root figure).

--- org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/parts/FXRootPart.java
+++ org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/parts/FXRootPart.java
@@ -1,15 +1,19 @@
 package org.eclipse.gef4.mvc.fx.parts;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 import org.eclipse.gef4.mvc.fx.viewer.FXViewer;
 import org.eclipse.gef4.mvc.parts.AbstractRootPart;
 import org.eclipse.gef4.mvc.parts.IContentPart;
-import org.eclipse.gef4.mvc.parts.IHandlePart;
 import org.eclipse.gef4.mvc.parts.IVisualPart;
 import org.eclipse.gef4.mvc.viewer.IVisualPartViewer;
 
@@ -22,29 +26,64 @@ public class FXRootPart extends AbstractRootPart<Node> {
 	private static final String SCROLL_PANE_STYLE = "-fx-background-insets:0;-fx-padding:0;-fx-background-color:rgba(0,0,0,0);";
 
 	private ScrollPane scrollPane;
-	private StackPane layers;
+	private StackPane layersStackPane;
+	
 	private Pane contentLayer;
 	private Pane handleLayer;
 	private Pane feedbackLayer;
 
+	private Parent scrollPaneInput;
+
 	public FXRootPart() {
-		scrollPane = new ScrollPane();
-		scrollPane.setPannable(false);
-		scrollPane.setStyle(SCROLL_PANE_STYLE);
-
-		layers = new StackPane();
-		scrollPane.setContent(new Group(layers));
-
-		contentLayer = createLayer(false);
-		handleLayer = createLayer(false);
-		feedbackLayer = createLayer(true);
+		createRootVisual();
 	}
 
-	private Pane createLayer(boolean mouseTransparent) {
+	protected void createRootVisual() {
+		contentLayer = createContentLayer();
+		handleLayer = createHandleLayer();
+		feedbackLayer = createFeedbackLayer();
+				
+		layersStackPane = createLayersStackPane(Arrays.asList(new Pane[]{contentLayer, handleLayer, feedbackLayer}));
+		
+		scrollPaneInput = createScrollPaneInput(layersStackPane);
+
+		scrollPane = createScrollPane(scrollPaneInput);
+	}
+
+	protected ScrollPane createScrollPane(Parent scrollPaneInput) {
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setContent(scrollPaneInput);
+		scrollPane.setPannable(false);
+		scrollPane.setStyle(SCROLL_PANE_STYLE);
+		return scrollPane;
+	}
+
+	protected StackPane createLayersStackPane(List<Pane> layers) {
+		StackPane layersStackPane = new StackPane();
+		layersStackPane.getChildren().addAll(layers);
+		return layersStackPane;
+	}
+
+	protected Pane createContentLayer() {
+		return createLayer(false);
+	}
+	
+	protected Pane createHandleLayer() {
+		return createLayer(false);
+	}
+	
+	protected Pane createFeedbackLayer() {
+		return createLayer(true);
+	}
+
+	protected Parent createScrollPaneInput(StackPane layersStackPane) {
+		return new Group(layersStackPane);
+	}
+
+	protected Pane createLayer(boolean mouseTransparent) {
 		Pane layer = new Pane();
 		layer.setPickOnBounds(false);
 		layer.setMouseTransparent(mouseTransparent);
-		layers.getChildren().add(layer);
 		return layer;
 	}
 
@@ -60,7 +99,6 @@ public class FXRootPart extends AbstractRootPart<Node> {
 		return contentLayer;
 	}
 
-	// TODO: we need feedback parts if we want to differentiate this (otherwise remove this layer)
 	public Pane getFeedbackLayer() {
 		return feedbackLayer;
 	}
@@ -91,8 +129,8 @@ public class FXRootPart extends AbstractRootPart<Node> {
 
 	@Override
 	protected void registerAtVisualPartMap() {
-		getViewer().getVisualPartMap().put(layers, this);
-		for (Node child : layers.getChildren()) {
+		getViewer().getVisualPartMap().put(layersStackPane, this);
+		for (Node child : layersStackPane.getChildren()) {
 			// register root edit part also for the layers
 			getViewer().getVisualPartMap().put(child, this);
 		}
@@ -100,8 +138,8 @@ public class FXRootPart extends AbstractRootPart<Node> {
 
 	@Override
 	protected void unregisterFromVisualPartMap() {
-		getViewer().getVisualPartMap().remove(layers);
-		for (Node child : layers.getChildren()) {
+		getViewer().getVisualPartMap().remove(layersStackPane);
+		for (Node child : layersStackPane.getChildren()) {
 			// register root edit part also for the layers
 			getViewer().getVisualPartMap().remove(child);
 		}
