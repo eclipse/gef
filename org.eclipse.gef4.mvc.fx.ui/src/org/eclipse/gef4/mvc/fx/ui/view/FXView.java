bda960922a24054a191b820c351e4d3744c70bfd
anyssen <alexander.nyssen@itemis.de>
2014-05-05 18:14:11 +0200

    [NONE] Remove duplidate copyright header.

--- org.eclipse.gef4.mvc.fx.ui/src/org/eclipse/gef4/mvc/fx/ui/view/FXView.java
+++ org.eclipse.gef4.mvc.fx.ui/src/org/eclipse/gef4/mvc/fx/ui/view/FXView.java
@@ -9,17 +9,6 @@
  *     Alexander Nyßen (itemis AG) - initial API and implementation
  *     
  *******************************************************************************/
-/*******************************************************************************
- * Copyright (c) 2014 itemis AG and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Alexander Nyßen (itemis AG) - initial API and implementation
- *     
- *******************************************************************************/
 package org.eclipse.gef4.mvc.fx.ui.view;
 
 import java.beans.PropertyChangeEvent;
