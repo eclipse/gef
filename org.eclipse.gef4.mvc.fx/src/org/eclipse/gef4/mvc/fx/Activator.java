50b6bb190bd17911add41196b06342d1e64b67c4
anyssen <alexander.nyssen@itemis.de>
2014-05-26 16:00:23 +0200

    [NONE] Remove dependency to org.eclipse.ui.

--- org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/Activator.java
+++ org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/Activator.java
@@ -1,3 +1,4 @@
+package org.eclipse.gef4.mvc.fx;
 /*******************************************************************************
  * Copyright (c) 2014 itemis AG and others.
  * All rights reserved. This program and the accompanying materials
@@ -9,53 +10,44 @@
  *     Alexander Nyßen (itemis AG) - initial API and implementation
  *     
  *******************************************************************************/
-package org.eclipse.gef4.mvc.fx;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator implements BundleActivator {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.gef4.mvc.javafx"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.gef4.mvc.fx"; //$NON-NLS-1$
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
 	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
 	}
 
 }
