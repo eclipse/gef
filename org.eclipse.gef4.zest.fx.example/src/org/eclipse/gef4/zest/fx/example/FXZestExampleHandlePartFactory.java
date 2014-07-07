f39430304a077467b5ce238842d401f19336ba7c
mwienand <matthias.wienand@itemis.de>
2014-07-07 16:59:30 +0200

    [NONE] Disable handle parts as handle interactions are not yet implemented.

--- org.eclipse.gef4.zest.fx.example/src/org/eclipse/gef4/zest/fx/example/FXZestExampleHandlePartFactory.java
+++ org.eclipse.gef4.zest.fx.example/src/org/eclipse/gef4/zest/fx/example/FXZestExampleHandlePartFactory.java
@@ -1,8 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial API & implementation
+ *     
+ *******************************************************************************/
 package org.eclipse.gef4.zest.fx.example;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import javafx.scene.Node;
 
 import org.eclipse.gef4.geometry.planar.IGeometry;
+import org.eclipse.gef4.mvc.behaviors.IBehavior;
 import org.eclipse.gef4.mvc.fx.parts.FXDefaultHandlePartFactory;
 import org.eclipse.gef4.mvc.fx.policies.AbstractFXDragPolicy;
 import org.eclipse.gef4.mvc.fx.policies.FXResizeRelocateOnHandleDragPolicy;
@@ -13,34 +30,12 @@ import org.eclipse.gef4.mvc.parts.IHandlePart;
 import com.google.inject.Provider;
 
 public class FXZestExampleHandlePartFactory extends FXDefaultHandlePartFactory {
-
+	
 	@Override
-	public IHandlePart<Node> createShapeSelectionHandlePart(
-			IContentPart<Node> targetPart,
-			Provider<IGeometry> handleGeometryProvider, int vertexIndex) {
-		IHandlePart<Node> part = super.createShapeSelectionHandlePart(
-				targetPart, handleGeometryProvider, vertexIndex);
-		// TODO: the corresponding NodeContentPart does not provide resizing yet
-		part.setAdapter(AbstractFXDragPolicy.class,
-				new FXResizeRelocateOnHandleDragPolicy(
-						toReferencePoint(vertexIndex)));
-		return part;
+	public List<IHandlePart<Node>> createHandleParts(
+			List<IContentPart<Node>> targets, IBehavior<Node> contextBehavior,
+			Map<Object, Object> contextMap) {
+		return Collections.emptyList();
 	}
-
-	private ReferencePoint toReferencePoint(int vertexIndex) {
-		switch (vertexIndex) {
-		case 0:
-			return ReferencePoint.TOP_LEFT;
-		case 1:
-			return ReferencePoint.TOP_RIGHT;
-		case 2:
-			return ReferencePoint.BOTTOM_RIGHT;
-		case 3:
-			return ReferencePoint.BOTTOM_LEFT;
-		default:
-			throw new IllegalStateException("Unsupported vertex index ("
-					+ vertexIndex + "), expected 0 to 3.");
-		}
-	}
-
+	
 }
