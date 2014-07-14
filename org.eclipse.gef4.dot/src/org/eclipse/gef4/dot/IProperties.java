2e7d87ac693eb9d95cd7376eb3f72d6f94c08dfc
mwienand <matthias.wienand@itemis.de>
2014-07-14 16:56:15 +0200

    [439144] Add properties interface for Graph/DOT import/export.

---
+++ org.eclipse.gef4.dot/src/org/eclipse/gef4/dot/IProperties.java
@@ -0,0 +1,25 @@
+package org.eclipse.gef4.dot.internal.dot;
+
+public interface IProperties {
+
+	// graphs
+
+	public static final String NODE_STYLE_PROPERTY = "node-style";
+	public static final String EDGE_STYLE_PROPERTY = "edge-style";
+	public static final String LAYOUT_PROPERTY = "layout";
+
+	// edges
+
+	// public static final String EDGE_STYLE_PROPERTY = "edge-style";
+	public static final String STYLE_PROPERTY = "style";
+	public static final String LABEL_PROPERTY = "label";
+	public static final String ID_PROPERTY = "id";
+
+	// nodes
+
+	// public static final String STYLE_PROPERTY = "style";
+	// public static final String LABEL_PROPERTY = "label";
+	// public static final String ID_PROPERTY = "id";
+	public static final String IMAGE_PROPERTY = "image";
+
+}
