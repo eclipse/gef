78de89b3d959537efc87b926960f030182bad4f2
anyssen <alexander.nyssen@itemis.de>
2014-07-14 20:10:44 +0200

    [NONE] Polished javadoc.

--- org.eclipse.gef4.geometry/src/org/eclipse/gef4/geometry/planar/ICurve.java
+++ org.eclipse.gef4.geometry/src/org/eclipse/gef4/geometry/planar/ICurve.java
@@ -1,20 +1,19 @@
 /*******************************************************************************
  * Copyright (c) 2012 itemis AG and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Alexander Nyßen (itemis AG) - initial API and implementation
  *     Matthias Wienand (itemis AG) - contribution for Bugzilla #355997
- *     
+ *
  *******************************************************************************/
 package org.eclipse.gef4.geometry.planar;
 
 /**
- * <p>
  * The {@link ICurve} interface provides operations that allow the analysis of
  * linear geometric objects and the transfer to {@link BezierCurve} segments (
  * {@link #toBezier()}). The start and end {@link Point} of an {@link ICurve}
@@ -27,28 +26,27 @@ package org.eclipse.gef4.geometry.planar;
  * the {@link #overlaps(ICurve)} method. One may think that an overlap is a very
  * rare case. But in practical application, objects are usually aligned to a
  * grid, which extremely increases the probability of an overlap.
- * </p>
- * 
+ *
  * @author anyssen
  * @author mwienand
- * 
+ *
  */
 public interface ICurve extends IGeometry {
 
 	/**
 	 * Returns the points of intersection between this {@link ICurve} and the
 	 * given {@link ICurve}.
-	 * 
+	 *
 	 * @param c
-	 *            the {@link ICurve} to compute intersection points for
+	 *            The {@link ICurve} to compute intersection points with.
 	 * @return the points of intersection.
 	 */
 	public Point[] getIntersections(final ICurve c);
 
 	/**
 	 * Returns a {@link Point} representing the start point of this
-	 * {@link ICurve} .
-	 * 
+	 * {@link ICurve}.
+	 *
 	 * @return a new {@link Point} with the coordinates of the {@link ICurve}'s
 	 *         start point.
 	 */
@@ -57,7 +55,7 @@ public interface ICurve extends IGeometry {
 	/**
 	 * Returns a {@link Point} representing the end point of this {@link ICurve}
 	 * .
-	 * 
+	 *
 	 * @return a new {@link Point} with the coordinates of the {@link ICurve}'s
 	 *         end point.
 	 */
@@ -65,40 +63,40 @@ public interface ICurve extends IGeometry {
 
 	/**
 	 * Returns the start {@link Point}'s x coordinate.
-	 * 
-	 * @return the start {@link Point}'s x coordinate.
+	 *
+	 * @return the start {@link Point}'s x coordinate
 	 */
 	public double getX1();
 
 	/**
 	 * Returns the end {@link Point}'s x coordinate.
-	 * 
-	 * @return the end {@link Point}'s x coordinate.
+	 *
+	 * @return the end {@link Point}'s x coordinate
 	 */
 	public double getX2();
 
 	/**
 	 * Returns the start {@link Point}'s y coordinate.
-	 * 
-	 * @return the start {@link Point}'s y coordinate.
+	 *
+	 * @return the start {@link Point}'s y coordinate
 	 */
 	public double getY1();
 
 	/**
 	 * Returns the end {@link Point}'s y coordinate.
-	 * 
-	 * @return the end {@link Point}'s y coordinate.
+	 *
+	 * @return the end {@link Point}'s y coordinate
 	 */
 	public double getY2();
 
 	/**
 	 * Tests if this {@link ICurve} and the given {@link ICurve} intersect, i.e.
 	 * whether a final set of intersection points exists. Two curves intersect
-	 * if they touch (see {@link IGeometry#touches(IGeometry)}) but not overlap
-	 * (see {@link ICurve#overlaps(ICurve)}).
-	 * 
+	 * if they touch (see {@link IGeometry#touches(IGeometry)}) but do not
+	 * overlap (see {@link ICurve#overlaps(ICurve)}).
+	 *
 	 * @param c
-	 *            the {@link ICurve} to test for intersections
+	 *            The {@link ICurve} to test for intersections.
 	 * @return <code>true</code> if they intersect, <code>false</code> otherwise
 	 */
 	boolean intersects(final ICurve c);
@@ -108,20 +106,20 @@ public interface ICurve extends IGeometry {
 	 * whether an infinite set of intersection points exists. Two curves overlap
 	 * if they touch (see {@link IGeometry#touches(IGeometry)}) but not
 	 * intersect (see {@link ICurve#intersects(ICurve)}).
-	 * 
+	 *
 	 * @param c
-	 *            the {@link ICurve} to test for overlap
+	 *            The {@link ICurve} to test for overlap.
 	 * @return <code>true</code> if they overlap, <code>false</code> otherwise
 	 */
 	boolean overlaps(final ICurve c);
 
 	/**
-	 * A list of {@link BezierCurve}s that approximate the {@link ICurve}. For
-	 * example, a {@link Line} or a {@link BezierCurve} in general, could return
-	 * a list with the curve itself as its only element. But an {@link Ellipse}
-	 * or an {@link Arc} may return a list of consecutive {@link BezierCurve}s
-	 * which approximate the curve.
-	 * 
+	 * Computes a list of {@link BezierCurve}s that approximate the
+	 * {@link ICurve}. For example, a {@link Line} or a {@link BezierCurve} in
+	 * general could return a list with the curve itself as its only element.
+	 * But an {@link Ellipse} or an {@link Arc} may return a list of consecutive
+	 * {@link BezierCurve}s which approximate the {@link ICurve}.
+	 *
 	 * @return a list of {@link BezierCurve}s that approximate the
 	 *         {@link ICurve}
 	 */
--- org.eclipse.gef4.geometry/src/org/eclipse/gef4/geometry/planar/IShape.java
+++ org.eclipse.gef4.geometry/src/org/eclipse/gef4/geometry/planar/IShape.java
@@ -1,43 +1,41 @@
 /*******************************************************************************
- * Copyright (c) 2011 itemis AG and others.
- * 
+ * Copyright (c) 2011, 2014 itemis AG and others.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Alexander Nyßen (itemis AG) - initial API and implementation
  *     Matthias Wienand (itemis AG) - contribution for Bugzilla #355997
- *     
+ *
  *******************************************************************************/
 package org.eclipse.gef4.geometry.planar;
 
 /**
+ * An {@link IShape} is the representation of an areal geometric object. It is
+ * thus the counterpart of an {@link ICurve}.
  * <p>
- * The IShape interface is the counterpart of the ICurve interface, in that it
- * brings together operations to be used on areal geometric objects, such as
- * Polygons. An IShape allows to test if an IGeometry is fully contained by the
- * IShape using the contains(IGeometry) method. An IGeometry is considered to be
- * fully contained by an IShape if the IShape contains all the Points that
- * constitute the IGeometry in question. Moreover, the closed outline of an
- * IShape can be retrieved via the getOutline() method. It is defined to be an
- * IPolyCurve. If you want to process the outline segments individually, you can
- * use the getOutlineSegments() method, instead.
- * </p>
- * 
+ * An {@link IShape} allows to test if another {@link IGeometry} is fully
+ * contained by it ({@link #contains(IGeometry)}). Moreover its (closed) outline
+ * can be retrieved as an whole ({@link #getOutline()}) or in the form of
+ * individual segments ({@link #getOutlineSegments()}. As all other
+ * {@link IGeometry}s, an {@link IShape} may be transformed (
+ * {@link #getTransformed(AffineTransform)}) into another {@link IShape}.
+ *
  * @author anyssen
  * @author mwienand
- * 
+ *
  */
 public interface IShape extends IGeometry {
 
 	/**
 	 * Tests whether the given {@link IGeometry} is fully contained by this
 	 * {@link IShape}.
-	 * 
+	 *
 	 * @param g
-	 *            the {@link IGeometry} to test for containment
+	 *            The {@link IGeometry} to test for containment
 	 * @return <code>true</code> if the given {@link IGeometry} is fully
 	 *         contained by this {@link IShape}, <code>false</code> otherwise
 	 */
@@ -46,19 +44,25 @@ public interface IShape extends IGeometry {
 	/**
 	 * Returns an {@link ICurve} representing the outline of this {@link IShape}
 	 * .
-	 * 
-	 * @return an {@link ICurve} representing the outline
+	 *
+	 * @return an {@link ICurve} representing this {@link IShape}'s outline
 	 */
 	public ICurve getOutline();
 
 	/**
 	 * Returns the individual {@link ICurve} segments, which constitute the
 	 * outline of this {@link IShape}.
-	 * 
+	 *
 	 * @return the {@link ICurve} segments of this {@link IShape}'s outline
 	 */
 	public ICurve[] getOutlineSegments();
 
+	/**
+	 * Returns a new {@link IShape}, which represents the given {@link IShape}
+	 * after the application of the given {@link AffineTransform}.
+	 *
+	 * @see IGeometry#getTransformed(AffineTransform)
+	 */
 	@Override
 	public IShape getTransformed(AffineTransform t);
 
