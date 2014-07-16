25235a4389cfbb0cfa4f34122f580969f41eaad4
anyssen <alexander.nyssen@itemis.de>
2014-07-16 17:00:34 +0200

    [NONE] Clean up implementation.
    Performed some renamings and removed unnecessary casts. Externalized som
    local variables being used several times. Implementation however needs
    to be revised to ensure reordering is performed properly (if other
    children are involved). We should add test cases to ensure the behavior
    is correct.

--- org.eclipse.gef4.mvc/src/org/eclipse/gef4/mvc/behaviors/ContentBehavior.java
+++ org.eclipse.gef4.mvc/src/org/eclipse/gef4/mvc/behaviors/ContentBehavior.java
@@ -58,7 +58,8 @@ public class ContentBehavior<VR> extends AbstractBehavior<VR> implements
 	public void propertyChange(PropertyChangeEvent event) {
 		if (IContentModel.CONTENTS_PROPERTY.equals(event.getPropertyName())) {
 			synchronizeContentChildren((List<Object>) event.getNewValue());
-			// TODO: flushing of models should be done somewhere more appropriate
+			// TODO: flushing of models should be done somewhere more
+			// appropriate
 			getHost().getRoot().getViewer().getSelectionModel().deselectAll();
 			getHost().getRoot().getViewer().getHoverModel().clearHover();
 		} else if (IContentPart.CONTENT_PROPERTY
@@ -76,58 +77,63 @@ public class ContentBehavior<VR> extends AbstractBehavior<VR> implements
 	 * set of content children that is passed in.
 	 */
 	@SuppressWarnings("unchecked")
-	public void synchronizeContentChildren(List<Object> contentChildren) {
-		// only synchronize ContentPart children
-
+	public void synchronizeContentChildren(final List<Object> contentChildren) {
 		int i;
-		IContentPart<VR> editPart;
-		Object model;
 
-		List<IContentPart<VR>> contentPartChildren = PartUtils.filterParts(
+		// only synchronize IContentPart children
+		List<IContentPart<VR>> childContentParts = PartUtils.filterParts(
 				getHost().getChildren(), IContentPart.class);
-		int size = contentPartChildren.size();
-		Map<Object, IContentPart<VR>> modelToEditPart = Collections.emptyMap();
-		if (size > 0) {
-			modelToEditPart = new HashMap<Object, IContentPart<VR>>(size);
-			for (i = 0; i < size; i++) {
-				editPart = (IContentPart<VR>) contentPartChildren.get(i);
-				modelToEditPart.put(editPart.getContent(), editPart);
+		int contentChildrenSize = contentChildren.size();
+		int childContentPartsSize = childContentParts.size();
+
+		Map<Object, IContentPart<VR>> contentToContentPartMap = Collections
+				.emptyMap();
+		IContentPart<VR> contentPart;
+		if (childContentPartsSize > 0) {
+			contentToContentPartMap = new HashMap<Object, IContentPart<VR>>(
+					childContentPartsSize);
+			for (i = 0; i < childContentPartsSize; i++) {
+				contentPart = childContentParts.get(i);
+				contentToContentPartMap.put(contentPart.getContent(),
+						contentPart);
 			}
 		}
 
-		List<Object> modelObjects = contentChildren;
-		for (i = 0; i < modelObjects.size(); i++) {
-			model = modelObjects.get(i);
+		Object content;
+		for (i = 0; i < contentChildrenSize; i++) {
+			content = contentChildren.get(i);
 
 			// Do a quick check to see if editPart[i] == model[i]
-			if (i < size && contentPartChildren.get(i).getContent() == model)
+			if (i < childContentPartsSize
+					&& childContentParts.get(i).getContent() == content)
 				continue;
 
 			// Look to see if the EditPart is already around but in the
 			// wrong location
-			editPart = (IContentPart<VR>) modelToEditPart.get(model);
+			contentPart = contentToContentPartMap.get(content);
 
-			if (editPart != null) {
+			if (contentPart != null) {
 				// TODO: this is wrong, it has to take into consideration the
 				// visual parts in between
-				getHost().reorderChild(editPart, i);
+				getHost().reorderChild(contentPart, i);
 			} else {
 				// An EditPart for this model doesn't exist yet. Create and
 				// insert one.
-				editPart = findOrCreatePartFor(model);
-				getHost().addChild(editPart, i);
+				contentPart = findOrCreatePartFor(content);
+				getHost().addChild(contentPart, i);
 			}
 		}
 
 		// remove the remaining EditParts
-		contentPartChildren = PartUtils.filterParts(getHost().getChildren(),
+		childContentParts = PartUtils.filterParts(getHost().getChildren(),
 				IContentPart.class);
-		size = contentPartChildren.size();
-		if (i < size) {
-			List<IContentPart<VR>> trash = new ArrayList<IContentPart<VR>>(size
-					- i);
-			for (; i < size; i++)
-				trash.add(contentPartChildren.get(i));
+		childContentPartsSize = childContentParts.size();
+
+		if (i < childContentPartsSize) {
+			List<IContentPart<VR>> trash = new ArrayList<IContentPart<VR>>(
+					childContentPartsSize - i);
+			for (; i < childContentPartsSize; i++)
+				trash.add(childContentParts.get(i));
 			for (i = 0; i < trash.size(); i++) {
 				IContentPart<VR> ep = trash.get(i);
 				getHost().removeChild(ep);
@@ -157,56 +163,58 @@ public class ContentBehavior<VR> extends AbstractBehavior<VR> implements
 	 * set of content anchored that is passed in.
 	 */
 	@SuppressWarnings("unchecked")
-	public void synchronizeContentAnchored(List<Object> contentAnchored) {
+	public void synchronizeContentAnchored(List<Object> contentAnchoreds) {
 		int i;
-		IContentPart<VR> editPart;
-		Object model;
 
-		List<IContentPart<VR>> anchored = PartUtils.filterParts(getHost()
+		List<IContentPart<VR>> anchoredContentParts = PartUtils.filterParts(getHost()
 				.getAnchoreds(), IContentPart.class);
-		int size = anchored.size();
-		Map<Object, IContentPart<VR>> modelToEditPart = Collections.emptyMap();
-		if (size > 0) {
-			modelToEditPart = new HashMap<Object, IContentPart<VR>>(size);
-			for (i = 0; i < size; i++) {
-				editPart = (IContentPart<VR>) anchored.get(i);
-				modelToEditPart.put(editPart.getContent(), editPart);
+		int anchoredContentPartsSize = anchoredContentParts.size();
+		int contentAnchoredsSize = contentAnchoreds.size();
+
+		IContentPart<VR> contentPart;
+		Map<Object, IContentPart<VR>> contentToContentPartMap = Collections.emptyMap();
+		if (anchoredContentPartsSize > 0) {
+			contentToContentPartMap = new HashMap<Object, IContentPart<VR>>(anchoredContentPartsSize);
+			for (i = 0; i < anchoredContentPartsSize; i++) {
+				contentPart = anchoredContentParts.get(i);
+				contentToContentPartMap.put(contentPart.getContent(), contentPart);
 			}
 		}
 
-		for (i = 0; i < contentAnchored.size(); i++) {
-			model = contentAnchored.get(i);
+		Object content;
+		for (i = 0; i < contentAnchoredsSize; i++) {
+			content = contentAnchoreds.get(i);
 
 			// Do a quick check to see if editPart[i] == model[i]
-			if (i < anchored.size() && anchored.get(i).getContent() == model)
+			if (i < anchoredContentParts.size() && anchoredContentParts.get(i).getContent() == content)
 				continue;
 
 			// Look to see if the EditPart is already around but in the
 			// wrong location
-			editPart = (IContentPart<VR>) modelToEditPart.get(model);
+			contentPart = contentToContentPartMap.get(content);
 
-			if (editPart != null) {
+			if (contentPart != null) {
 				// TODO: this is wrong, it has to take into consideration the
 				// visual parts in between
 				// reorderChild(editPart, i);
 			} else {
 				// An EditPart for this model doesn't exist yet. Create and
 				// insert one.
-				editPart = findOrCreatePartFor(model);
+				contentPart = findOrCreatePartFor(content);
 				// what if it does not exist??
-				getHost().addAnchored(editPart);
+				getHost().addAnchored(contentPart);
 			}
 		}
 
 		// remove the remaining EditParts
-		anchored = PartUtils.filterParts(getHost().getAnchoreds(),
+		anchoredContentParts = PartUtils.filterParts(getHost().getAnchoreds(),
 				IContentPart.class);
-		size = anchored.size();
-		if (i < size) {
-			List<IContentPart<VR>> trash = new ArrayList<IContentPart<VR>>(size
+		anchoredContentPartsSize = anchoredContentParts.size();
+		if (i < anchoredContentPartsSize) {
+			List<IContentPart<VR>> trash = new ArrayList<IContentPart<VR>>(anchoredContentPartsSize
 					- i);
-			for (; i < size; i++)
-				trash.add(anchored.get(i));
+			for (; i < anchoredContentPartsSize; i++)
+				trash.add(anchoredContentParts.get(i));
 			for (i = 0; i < trash.size(); i++) {
 				IContentPart<VR> ep = trash.get(i);
 				getHost().removeAnchored(ep);
