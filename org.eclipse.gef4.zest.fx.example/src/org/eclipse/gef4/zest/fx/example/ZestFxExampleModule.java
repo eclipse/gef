29038b60d89e1ae99f3db76d8f806d48d922c2c4
anyssen <alexander.nyssen@itemis.de>
2014-07-19 11:03:36 +0200

    [439934] Fixed Zest.FX example.

--- org.eclipse.gef4.zest.fx.example/src/org/eclipse/gef4/zest/fx/example/ZestFxExampleModule.java
+++ org.eclipse.gef4.zest.fx.example/src/org/eclipse/gef4/zest/fx/example/ZestFxExampleModule.java
@@ -18,15 +18,13 @@ import org.eclipse.gef4.mvc.parts.IHandlePartFactory;
 import org.eclipse.gef4.zest.fx.ZestFxModule;
 
 import com.google.inject.TypeLiteral;
-import com.google.inject.name.Names;
 
 class ZestFxExampleModule extends ZestFxModule {
-	
+
 	@Override
 	protected void bindFXDefaultHandlePartFactory() {
 		binder().bind(new TypeLiteral<IHandlePartFactory<Node>>() {
-		}).annotatedWith(Names.named("AbstractViewer"))
-				.toInstance(new FXZestExampleHandlePartFactory());
+		}).toInstance(new FXZestExampleHandlePartFactory());
 	}
-	
+
 }
