5597a530441343bb8930711fec3d68ffa0569555
anyssen <alexander.nyssen@itemis.de>
2014-07-23 20:42:13 +0200

    [NONE] Fix compile problem in Zest.FX tests.

--- org.eclipse.gef4.zest.fx.tests/src/org/eclipse/gef4/zest/fx/tests/NodeLayoutPolicyTests.java
+++ org.eclipse.gef4.zest.fx.tests/src/org/eclipse/gef4/zest/fx/tests/NodeLayoutPolicyTests.java
@@ -16,13 +16,12 @@ import static org.junit.Assert.assertEquals;
 import javafx.scene.layout.Pane;
 
 import org.eclipse.core.commands.operations.IUndoableOperation;
-import org.eclipse.gef4.fx.nodes.FXGeometryNode;
 import org.eclipse.gef4.geometry.planar.Dimension;
 import org.eclipse.gef4.geometry.planar.Point;
-import org.eclipse.gef4.geometry.planar.Rectangle;
 import org.eclipse.gef4.graph.Graph;
 import org.eclipse.gef4.graph.Node;
 import org.eclipse.gef4.layout.PropertiesHelper;
+import org.eclipse.gef4.mvc.bindings.AdapterKey;
 import org.eclipse.gef4.mvc.fx.parts.AbstractFXContentPart;
 import org.eclipse.gef4.mvc.fx.policies.FXResizeRelocatePolicy;
 import org.eclipse.gef4.mvc.parts.IContentPart;
@@ -59,7 +58,7 @@ public class NodeLayoutPolicyTests {
 			final Dimension size) {
 		NodeLayoutPolicy policy = new NodeLayoutPolicy() {
 			private IContentPart<javafx.scene.Node> host;
-			
+
 			@Override
 			public IVisualPart<javafx.scene.Node> getHost() {
 				if (host == null)
@@ -67,14 +66,17 @@ public class NodeLayoutPolicyTests {
 						private Pane visual;
 
 						{
-							setAdapter(FXResizeRelocatePolicy.class, new FXResizeRelocatePolicy() {
-								@Override
-								public IUndoableOperation commit() {
-									return null;
-								}
-							});
+							setAdapter(
+									AdapterKey
+											.get(FXResizeRelocatePolicy.class),
+									new FXResizeRelocatePolicy() {
+										@Override
+										public IUndoableOperation commit() {
+											return null;
+										}
+									});
 						}
-						
+
 						@Override
 						public javafx.scene.Node getVisual() {
 							if (visual == null) {
@@ -107,7 +109,7 @@ public class NodeLayoutPolicyTests {
 		PropertiesHelper.setSize(nodeLayout, size.getWidth(), size.getHeight());
 
 		policy.adaptLayoutInformation(nodeLayout);
-		
+
 		javafx.scene.Node visual = policy.getHost().getVisual();
 		assertEquals(location,
 				new Point(visual.getLayoutX(), visual.getLayoutY()));
