5ae306fab886b1f1fe2dd9068871df5f7334ba39
mwienand <matthias.wienand@itemis.de>
2014-07-28 17:52:48 +0200

    [440554] Implement provisional "delete-on-key" in the example.

---
+++ org.eclipse.gef4.mvc.fx.example/src/org/eclipse/gef4/mvc/fx/example/parts/AbstractFXDeleteOperation.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial API and implementation
+ *     
+ *******************************************************************************/
+package org.eclipse.gef4.mvc.fx.example.parts;
+
+import javafx.scene.Node;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.AbstractOperation;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.gef4.mvc.behaviors.ContentBehavior;
+import org.eclipse.gef4.mvc.parts.IContentPart;
+
+public abstract class AbstractFXDeleteOperation extends AbstractOperation {
+
+	private Object contentChild;
+	private IContentPart<Node> parent;
+
+	public AbstractFXDeleteOperation(String label, IContentPart<Node> parent,
+			Object contentChild) {
+		super(label);
+		this.contentChild = contentChild;
+		this.parent = parent;
+	}
+	
+	public IContentPart<Node> getParent() {
+		return parent;
+	}
+	
+	public Object getContentChild() {
+		return contentChild;
+	}
+
+	@Override
+	public IStatus execute(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+		removeContentChild();
+		ContentBehavior contentBehavior = parent
+				.getAdapter(ContentBehavior.class);
+		contentBehavior.synchronizeContentChildren(parent.getContentChildren());
+		contentBehavior.synchronizeContentAnchorages(parent
+				.getContentAnchorages());
+		return Status.OK_STATUS;
+	}
+
+	public abstract void removeContentChild();
+
+	@Override
+	public IStatus redo(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+		return execute(monitor, info);
+	}
+
+	@Override
+	public IStatus undo(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+		addContentChild();
+		ContentBehavior contentBehavior = parent
+				.getAdapter(ContentBehavior.class);
+		contentBehavior.synchronizeContentChildren(parent.getContentChildren());
+		contentBehavior.synchronizeContentAnchorages(parent
+				.getContentAnchorages());
+		return Status.OK_STATUS;
+	}
+
+	public abstract void addContentChild();
+
+}
--- org.eclipse.gef4.mvc.fx.example/src/org/eclipse/gef4/mvc/fx/example/parts/FXGeometricShapePart.java
+++ org.eclipse.gef4.mvc.fx.example/src/org/eclipse/gef4/mvc/fx/example/parts/FXGeometricShapePart.java
@@ -12,9 +12,11 @@
 package org.eclipse.gef4.mvc.fx.example.parts;
 
 import java.awt.geom.NoninvertibleTransformException;
+import java.util.List;
 
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
+import javafx.scene.input.KeyEvent;
 
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.operations.AbstractOperation;
@@ -29,13 +31,18 @@ import org.eclipse.gef4.fx.anchors.IFXAnchor;
 import org.eclipse.gef4.fx.nodes.FXGeometryNode;
 import org.eclipse.gef4.geometry.convert.awt.AWT2Geometry;
 import org.eclipse.gef4.geometry.planar.AffineTransform;
+import org.eclipse.gef4.geometry.planar.IGeometry;
 import org.eclipse.gef4.geometry.planar.IShape;
 import org.eclipse.gef4.geometry.planar.Point;
+import org.eclipse.gef4.mvc.fx.example.model.AbstractFXGeometricElement;
 import org.eclipse.gef4.mvc.fx.example.model.FXGeometricShape;
+import org.eclipse.gef4.mvc.fx.policies.AbstractFXTypePolicy;
 import org.eclipse.gef4.mvc.fx.policies.FXRelocateOnDragPolicy;
 import org.eclipse.gef4.mvc.fx.policies.FXResizeRelocatePolicy;
 import org.eclipse.gef4.mvc.fx.tools.FXClickDragTool;
+import org.eclipse.gef4.mvc.fx.tools.FXTypeTool;
 import org.eclipse.gef4.mvc.operations.AbstractCompositeOperation;
+import org.eclipse.gef4.mvc.parts.IContentPart;
 import org.eclipse.gef4.mvc.parts.IVisualPart;
 
 public class FXGeometricShapePart extends AbstractFXGeometricElementPart {
@@ -73,62 +80,103 @@ public class FXGeometricShapePart extends AbstractFXGeometricElementPart {
 				new FXRelocateOnDragPolicy());
 
 		// transaction policies
-		setAdapter(AdapterKey.get(FXResizeRelocatePolicy.class), new FXResizeRelocatePolicy() {
-
-			@Override
-			public IUndoableOperation commit() {
-				final IUndoableOperation updateVisualOperation = super.commit();
-				if(updateVisualOperation == null){
-					return null;
-				}
-				
-				// commit changes to model
-				final FXGeometricShape shape = getContent();
-				IShape visualGeometry = visual.getGeometry();
-				if (shape.getTransform() != null) {
-					try {
-						visualGeometry = visual.getGeometry().getTransformed(AWT2Geometry.toAffineTransform(shape.getTransform().createInverse()));
-					} catch (NoninvertibleTransformException e) {
-						e.printStackTrace();
-					}
-				}
-				final IShape newGeometry = visualGeometry;
-				final IShape oldGeometry = shape.getGeometry();
-				final IUndoableOperation updateModelOperation = new AbstractOperation(
-						"Update Model") {
+		setAdapter(AdapterKey.get(FXResizeRelocatePolicy.class),
+				new FXResizeRelocatePolicy() {
 
 					@Override
-					public IStatus undo(IProgressMonitor monitor,
-							IAdaptable info) throws ExecutionException {
-						shape.setGeometry(oldGeometry);
-						return Status.OK_STATUS;
+					public IUndoableOperation commit() {
+						final IUndoableOperation updateVisualOperation = super
+								.commit();
+						if (updateVisualOperation == null) {
+							return null;
+						}
+
+						// commit changes to model
+						final FXGeometricShape shape = getContent();
+						IShape visualGeometry = visual.getGeometry();
+						if (shape.getTransform() != null) {
+							try {
+								visualGeometry = visual
+										.getGeometry()
+										.getTransformed(
+												AWT2Geometry
+														.toAffineTransform(shape
+																.getTransform()
+																.createInverse()));
+							} catch (NoninvertibleTransformException e) {
+								e.printStackTrace();
+							}
+						}
+						final IShape newGeometry = visualGeometry;
+						final IShape oldGeometry = shape.getGeometry();
+						final IUndoableOperation updateModelOperation = new AbstractOperation(
+								"Update Model") {
+
+							@Override
+							public IStatus undo(IProgressMonitor monitor,
+									IAdaptable info) throws ExecutionException {
+								shape.setGeometry(oldGeometry);
+								return Status.OK_STATUS;
+							}
+
+							@Override
+							public IStatus redo(IProgressMonitor monitor,
+									IAdaptable info) throws ExecutionException {
+								return execute(monitor, info);
+							}
+
+							@Override
+							public IStatus execute(IProgressMonitor monitor,
+									IAdaptable info) throws ExecutionException {
+								shape.setGeometry(newGeometry);
+								return Status.OK_STATUS;
+							}
+						};
+						// compose both operations
+						IUndoableOperation compositeOperation = new AbstractCompositeOperation(
+								updateVisualOperation.getLabel()) {
+							{
+								add(updateVisualOperation);
+								add(updateModelOperation);
+							}
+						};
+
+						return compositeOperation;
+					}
+				});
+
+		setAdapter(AdapterKey.get(FXTypeTool.TOOL_POLICY_KEY),
+				new AbstractFXTypePolicy() {
+					@Override
+					public void released(KeyEvent event) {
 					}
 
 					@Override
-					public IStatus redo(IProgressMonitor monitor,
-							IAdaptable info) throws ExecutionException {
-						return execute(monitor, info);
-					}
+					public void pressed(KeyEvent event) {
+						executeOperation(new AbstractFXDeleteOperation(
+								"delete", (IContentPart<Node>) getParent(),
+								getContent()) {
+							private List<AbstractFXGeometricElement<? extends IGeometry>> shapeVisuals;
 
-					@Override
-					public IStatus execute(IProgressMonitor monitor,
-							IAdaptable info) throws ExecutionException {
-						shape.setGeometry(newGeometry);
-						return Status.OK_STATUS;
-					}
-				};
-				// compose both operations
-				IUndoableOperation compositeOperation = new AbstractCompositeOperation(
-						updateVisualOperation.getLabel()) {
-					{
-						add(updateVisualOperation);
-						add(updateModelOperation);
-					}
-				};
+							{
+								shapeVisuals = ((FXGeometricModelPart) getParent())
+										.getContent().getShapeVisuals();
+							}
 
-				return compositeOperation;
-			}
-		});
+							@Override
+							public void removeContentChild() {
+								shapeVisuals.remove(getContentChild());
+							}
+
+							@SuppressWarnings("unchecked")
+							@Override
+							public void addContentChild() {
+								shapeVisuals
+										.add((AbstractFXGeometricElement<? extends IGeometry>) getContentChild());
+							}
+						});
+					}
+				});
 	}
 
 	@Override
