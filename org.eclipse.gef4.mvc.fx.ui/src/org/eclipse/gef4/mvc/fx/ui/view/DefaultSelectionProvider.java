c0162e09df8e46f2ada4a6a8b97c33ec9dbd631d
anyssen <alexander.nyssen@itemis.de>
2014-08-10 11:08:23 +0200

    [NONE] Add missing copyright header.

--- org.eclipse.gef4.mvc.fx.ui/src/org/eclipse/gef4/mvc/fx/ui/view/DefaultSelectionProvider.java
+++ org.eclipse.gef4.mvc.fx.ui/src/org/eclipse/gef4/mvc/fx/ui/view/DefaultSelectionProvider.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *     
+ *******************************************************************************/
 package org.eclipse.gef4.mvc.fx.ui.view;
 
 import java.util.ArrayList;
@@ -10,6 +21,7 @@ import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 
+// TOOD: move to Common.UI
 public class DefaultSelectionProvider implements ISelectionProvider {
 
 	private ISelection selection;
