7389f5b017dc5052f61e3278f1dd93e12d217de0
anyssen <alexander.nyssen@itemis.de>
2014-08-13 08:44:48 +0200

    [NONE] Fixed that all IFXAnchors provide positions in local coordinates.
    Up to now, FXChopBoxAnchor provided postions in the local coordinate systems of the AnchorKey's anchored nodes, while FXStaticAnchor provided scene coordinates. Ensured that local coordinates are always used. Changed javadoc of anchor and implementation within FXStaticAnchor to be compliant. Also adjusted FXBendPolicy.

--- org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/anchors/IFXAnchor.java
+++ org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/anchors/IFXAnchor.java
@@ -18,86 +18,79 @@ import javafx.scene.Node;
 import org.eclipse.gef4.geometry.planar.Point;
 
 /**
- * A visual anchor that can provide positions (in scene coordinates) for
- * attached {@link AnchorKey}s (i.e. anchored {@link Node}s). An
- * {@link IFXAnchor} may be bound to an anchorage {@link Node}. If so, it will
+ * A visual anchor that can provide positions for
+ * attached {@link AnchorKey}s (i.e. anchored {@link Node}s) in local coordinates of the {@link AnchorKey}'s anchored
+ * {@link Node}. An {@link IFXAnchor} may be bound to an anchorage {@link Node}. If this is the case, it will
  * recompute positions for all attached anchored {@link Node}s in case the
  * anchorage {@link Node} or any of its ancestors are changed in a way that will
- * have an effect on the position (in scene coordinates).
+ * have an effect on the position (in local coordinates of the anchored {@link Node}s).
  *
  * @author anyssen
- *
  */
 public interface IFXAnchor {
 
-	/**
-	 * Provides a read-only property with the anchorage {@link Node} this
-	 * {@link IFXAnchor} is attached to. The property value may be
-	 * <code>null</code> in case this {@link IFXAnchor} is not bound to an
-	 * anchorage {@link Node}.
-	 *
-	 * @return A read-only property storing the anchorage {@link Node}.
-	 */
-	ReadOnlyObjectProperty<Node> anchorageProperty();
+    /**
+     * Provides a read-only property with the anchorage {@link Node} this {@link IFXAnchor} is attached to. The property
+     * value may be <code>null</code> in case this {@link IFXAnchor} is not bound to an
+     * anchorage {@link Node}.
+     *
+     * @return A read-only property storing the anchorage {@link Node}.
+     */
+    ReadOnlyObjectProperty<Node> anchorageProperty();
 
-	/**
-	 * Attaches the given {@link AnchorKey} to this {@link IFXAnchor}.
-	 *
-	 * @param key
-	 *            The {@link AnchorKey} to be attached.
-	 */
-	void attach(AnchorKey key);
+    /**
+     * Attaches the given {@link AnchorKey} to this {@link IFXAnchor}.
+     *
+     * @param key
+     *            The {@link AnchorKey} to be attached.
+     */
+    void attach(AnchorKey key);
 
-	/**
-	 * Detaches the given {@link AnchorKey} from this {@link IFXAnchor}.
-	 *
-	 * @param key
-	 *            The {@link AnchorKey} to be detached.
-	 */
-	void detach(AnchorKey key);
+    /**
+     * Detaches the given {@link AnchorKey} from this {@link IFXAnchor}.
+     *
+     * @param key
+     *            The {@link AnchorKey} to be detached.
+     */
+    void detach(AnchorKey key);
 
-	/**
-	 * Provides the anchorage {@link Node} this {@link IFXAnchor} is bound to.
-	 * Will return the value of the {@link #anchorageProperty()}.
-	 *
-	 * @return The value of the {@link #anchorageProperty()}.
-	 */
-	Node getAnchorage();
+    /**
+     * Provides the anchorage {@link Node} this {@link IFXAnchor} is bound to.
+     * Will return the value of the {@link #anchorageProperty()}.
+     *
+     * @return The value of the {@link #anchorageProperty()}.
+     */
+    Node getAnchorage();
 
-	/**
-	 * Provides a position for the given {@link AnchorKey}. The provided
-	 * {@link AnchorKey} has to be attached to this {@link IFXAnchor} (see
-	 * {@link #attach(AnchorKey)}).
-	 *
-	 * @param key
-	 *            The {@link AnchorKey} to retrieve a position for. It has to be
-	 *            attached ({@link #attach(AnchorKey)}) to this
-	 *            {@link IFXAnchor} before.
-	 * @return The position for the given {@link AnchorKey} within scene
-	 *         coordinates.
-	 */
-	Point getPosition(AnchorKey key);
+    /**
+     * Provides a position for the given {@link AnchorKey}. The provided {@link AnchorKey} has to be attached to this
+     * {@link IFXAnchor} (see {@link #attach(AnchorKey)}).
+     *
+     * @param key
+     *            The {@link AnchorKey} to retrieve a position for. It has to be
+     *            attached ({@link #attach(AnchorKey)}) to this {@link IFXAnchor} before.
+     * @return The position for the given {@link AnchorKey} within local coordinates of the {@link AnchorKey}'s anchored
+     *         {@link Node}.
+     */
+    Point getPosition(AnchorKey key);
 
-	/**
-	 * Determines if the given {@link AnchorKey} is currently attached to this
-	 * {@link IFXAnchor}.
-	 *
-	 * @param key
-	 *            The {@link AnchorKey} to test.
-	 * @return <code>true</code> if the given key is attached, otherwise
-	 *         <code>false</code>.
-	 */
-	boolean isAttached(AnchorKey key);
+    /**
+     * Determines if the given {@link AnchorKey} is currently attached to this {@link IFXAnchor}.
+     *
+     * @param key
+     *            The {@link AnchorKey} to test.
+     * @return <code>true</code> if the given key is attached, otherwise <code>false</code>.
+     */
+    boolean isAttached(AnchorKey key);
 
-	/**
-	 * Provides a read-only (map) property with positions (in scene coordinates)
-	 * for all attached {@link AnchorKey}s. The positions will be updated for
-	 * all attached {@link AnchorKey}s if the anchorage {@link Node} or any of
-	 * its ancestors changes in a way that will effect them.
-	 *
-	 * @return A read-only (map) property storing positions for all attached
-	 *         {@link AnchorKey}s.
-	 */
-	ReadOnlyMapProperty<AnchorKey, Point> positionProperty();
+    /**
+     * Provides a read-only (map) property with positions (in local coordinates of the anchored)
+     * for all attached {@link AnchorKey}s. The positions will be updated for
+     * all attached {@link AnchorKey}s if the anchorage {@link Node} or any of
+     * its ancestors changes in a way that will effect them.
+     *
+     * @return A read-only (map) property storing positions for all attached {@link AnchorKey}s.
+     */
+    ReadOnlyMapProperty<AnchorKey, Point> positionProperty();
 
 }
--- org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/policies/FXBendPolicy.java
+++ org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/policies/FXBendPolicy.java
@@ -23,6 +23,7 @@ import org.eclipse.gef4.fx.anchors.AnchorLink;
 import org.eclipse.gef4.fx.anchors.FXStaticAnchor;
 import org.eclipse.gef4.fx.anchors.IFXAnchor;
 import org.eclipse.gef4.fx.nodes.IFXConnection;
+import org.eclipse.gef4.geometry.convert.fx.JavaFX2Geometry;
 import org.eclipse.gef4.geometry.planar.Point;
 import org.eclipse.gef4.mvc.fx.operations.FXBendOperation;
 import org.eclipse.gef4.mvc.fx.parts.AbstractFXContentPart;
@@ -32,207 +33,197 @@ import org.eclipse.gef4.mvc.parts.IVisualPart;
 import org.eclipse.gef4.mvc.policies.AbstractPolicy;
 
 /**
- * The {@link FXBendPolicy} can be used to manipulate the points constituting an
- * {@link IFXConnection}, i.e. its start point, way points, and end point. When
+ * The {@link FXBendPolicy} can be used to manipulate the points constituting an {@link IFXConnection}, i.e. its start
+ * point, way points, and end point. When
  * moving a point the policy takes care of:
  * <ul>
  * <li>Removing overlaid neighbor points.</li>
  * <li>Re-adding temporarily removed neighbor points.</li>
- * <li>Reconnecting points to the {@link IVisualPart} under mouse when
- * applicable.</li>
+ * <li>Reconnecting points to the {@link IVisualPart} under mouse when applicable.</li>
  * </ul>
  *
  * @author mwienand
  */
-public class FXBendPolicy extends AbstractPolicy<Node> implements
-ITransactional {
+public class FXBendPolicy extends AbstractPolicy<Node> implements ITransactional {
 
-	// constants (TODO: make configurable)
-	protected static final double REMOVE_THRESHOLD = 10;
+    // constants (TODO: make configurable)
+    protected static final double REMOVE_THRESHOLD = 10;
 
-	// interaction
-	private Point2D startPointInScene;
-	private Point startPoint;
-	private Point currentPoint;
-	private AnchorLink removedLink;
-	private int removedLinkIndex;
-	private int linkIndex;
-	private int oldLinkIndex;
+    // interaction
+    private Point2D startPointInScene;
+    private Point startPoint;
+    private Point currentPoint;
+    private AnchorLink removedLink;
+    private int removedLinkIndex;
+    private int linkIndex;
+    private int oldLinkIndex;
 
-	// operation
-	private FXBendOperation op;
+    // operation
+    private FXBendOperation op;
 
-	@Override
-	public IUndoableOperation commit() {
-		return op;
-	}
+    @Override
+    public IUndoableOperation commit() {
+        return op;
+    }
 
-	public void createWayPoint(int segmentIndex, Point mouseInScene) {
-		Node hostVisual = getHostVisual();
-		Point2D mouseInLocal = hostVisual.sceneToLocal(mouseInScene.x,
-				mouseInScene.y);
+    public void createWayPoint(int segmentIndex, Point mouseInScene) {
+        Node hostVisual = getHostVisual();
+        Point2D mouseInLocal = hostVisual.sceneToLocal(mouseInScene.x, mouseInScene.y);
 
-		// create new way point
-		segmentIndex++;
-		op.getNewLinks().add(
-				segmentIndex,
-				((IFXConnection) hostVisual)
-				.createWayPointAnchorLink(new Point(
-						mouseInLocal.getX(), mouseInLocal.getY())));
-		locallyExecuteOperation();
+        // create new way point
+        segmentIndex++;
+        op.getNewLinks().add(
+                segmentIndex,
+                ((IFXConnection) hostVisual).createWayPointAnchorLink(new Point(mouseInLocal.getX(), mouseInLocal
+                        .getY())));
+        locallyExecuteOperation();
 
-		// select newly created way point
-		selectPoint(segmentIndex, 0, mouseInScene);
-	}
+        // select newly created way point
+        selectPoint(segmentIndex, 0, mouseInScene);
+    }
 
-	protected AbstractFXContentPart getAnchorPart(
-			List<IContentPart<Node>> partsUnderMouse) {
-		for (IContentPart<Node> cp : partsUnderMouse) {
-			AbstractFXContentPart part = (AbstractFXContentPart) cp;
-			IFXAnchor anchor = part.getAnchor(getHost());
-			if (anchor != null) {
-				return part;
-			}
-		}
-		return null;
-	}
+    protected AbstractFXContentPart getAnchorPart(List<IContentPart<Node>> partsUnderMouse) {
+        for (IContentPart<Node> cp : partsUnderMouse) {
+            AbstractFXContentPart part = (AbstractFXContentPart) cp;
+            IFXAnchor anchor = part.getAnchor(getHost());
+            if (anchor != null) {
+                return part;
+            }
+        }
+        return null;
+    }
 
-	protected Node getHostVisual() {
-		return getHost().getVisual();
-	}
+    protected Node getHostVisual() {
+        return getHost().getVisual();
+    }
 
-	private Point getPosition(AnchorLink link) {
-		// TODO: Maybe this can be done nicer?
-		boolean attached = link.getAnchor().isAttached(link.getKey());
-		if (!attached) {
-			link.getAnchor().attach(link.getKey());
-		}
-		Point p = link.getPosition();
-		if (!attached) {
-			link.getAnchor().detach(link.getKey());
-		}
-		return p;
-	}
+    private Point getPosition(AnchorLink link) {
+        // TODO: Maybe this can be done nicer?
+        boolean attached = link.getAnchor().isAttached(link.getKey());
+        if (!attached) {
+            link.getAnchor().attach(link.getKey());
+        }
+        Point p = link.getPosition();
+        if (!attached) {
+            link.getAnchor().detach(link.getKey());
+        }
+        return p;
+    }
 
-	protected void hideShowOverlaid() {
-		// put removed back in
-		if (removedLink != null) {
-			linkIndex = oldLinkIndex;
-			op.getNewLinks().add(removedLinkIndex, removedLink);
-			removedLink = null;
-		}
+    protected void hideShowOverlaid() {
+        // put removed back in
+        if (removedLink != null) {
+            linkIndex = oldLinkIndex;
+            op.getNewLinks().add(removedLinkIndex, removedLink);
+            removedLink = null;
+        }
 
-		// do not remove overlaid if there are no way points
-		if (op.getNewLinks().size() <= 2) {
-			return;
-		}
+        // do not remove overlaid if there are no way points
+        if (op.getNewLinks().size() <= 2) {
+            return;
+        }
 
-		removedLinkIndex = -1;
-		oldLinkIndex = linkIndex;
+        removedLinkIndex = -1;
+        oldLinkIndex = linkIndex;
 
-		// determine overlaid neighbor
-		if (linkIndex > 0) {
-			int prevIndex = linkIndex - 1;
-			if (getPosition(op.getNewLinks().get(prevIndex)).getDistance(
-					currentPoint) < REMOVE_THRESHOLD) {
-				// remove previous
-				removedLinkIndex = prevIndex;
-				linkIndex--;
-			}
-		}
-		if (removedLinkIndex == -1 && linkIndex < op.getNewLinks().size() - 1) {
-			int nextIndex = linkIndex + 1;
-			if (getPosition(op.getNewLinks().get(nextIndex)).getDistance(
-					currentPoint) < REMOVE_THRESHOLD) {
-				// remove next
-				removedLinkIndex = nextIndex;
-			}
-		}
+        // determine overlaid neighbor
+        if (linkIndex > 0) {
+            int prevIndex = linkIndex - 1;
+            if (getPosition(op.getNewLinks().get(prevIndex)).getDistance(currentPoint) < REMOVE_THRESHOLD) {
+                // remove previous
+                removedLinkIndex = prevIndex;
+                linkIndex--;
+            }
+        }
+        if (removedLinkIndex == -1 && linkIndex < op.getNewLinks().size() - 1) {
+            int nextIndex = linkIndex + 1;
+            if (getPosition(op.getNewLinks().get(nextIndex)).getDistance(currentPoint) < REMOVE_THRESHOLD) {
+                // remove next
+                removedLinkIndex = nextIndex;
+            }
+        }
 
-		// remove neighbor if overlaid
-		if (removedLinkIndex != -1) {
-			removedLink = op.getNewLinks().remove(removedLinkIndex);
-		}
-	}
+        // remove neighbor if overlaid
+        if (removedLinkIndex != -1) {
+            removedLink = op.getNewLinks().remove(removedLinkIndex);
+        }
+    }
 
-	@Override
-	public void init() {
-		op = new FXBendOperation((IFXConnection) getHostVisual());
-		removedLink = null;
-	}
+    @Override
+    public void init() {
+        op = new FXBendOperation((IFXConnection) getHostVisual());
+        removedLink = null;
+    }
 
-	protected void locallyExecuteOperation() {
-		try {
-			op.execute(null, null);
-		} catch (ExecutionException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+    protected void locallyExecuteOperation() {
+        try {
+            op.execute(null, null);
+        } catch (ExecutionException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
-	public void movePoint(Point mouseInScene,
-			List<IContentPart<Node>> partsUnderMouse) {
-		// update position
-		Point2D mouseInLocal = getHostVisual().sceneToLocal(mouseInScene.x,
-				mouseInScene.y);
-		Point2D startPointInLocal = getHostVisual().sceneToLocal(
-				startPointInScene);
-		Point delta = new Point(mouseInLocal.getX() - startPointInLocal.getX(),
-				mouseInLocal.getY() - startPointInLocal.getY());
+    public void movePoint(Point mouseInScene, List<IContentPart<Node>> partsUnderMouse) {
+        // update position
+        Point2D mouseInLocal = getHostVisual().sceneToLocal(mouseInScene.x, mouseInScene.y);
+        Point2D startPointInLocal = getHostVisual().sceneToLocal(startPointInScene);
+        Point delta =
+                new Point(mouseInLocal.getX() - startPointInLocal.getX(), mouseInLocal.getY()
+                        - startPointInLocal.getY());
 
-		currentPoint.x = startPoint.x + delta.x;
-		currentPoint.y = startPoint.y + delta.y;
+        currentPoint.x = startPointInLocal.getX() + delta.x;
+        currentPoint.y = startPointInLocal.getY() + delta.y;
 
-		// update
-		hideShowOverlaid();
-		updateCurrentAnchorLink(mouseInScene, partsUnderMouse);
-		locallyExecuteOperation();
-	}
+        // update
+        hideShowOverlaid();
+        updateCurrentAnchorLink(mouseInScene, partsUnderMouse);
+        locallyExecuteOperation();
+    }
 
-	public void selectPoint(int segmentIndex, double segmentParameter,
-			Point mouseInScene) {
-		// store handle part information
-		if (segmentParameter == 1) {
-			linkIndex = segmentIndex + 1;
-		} else {
-			linkIndex = segmentIndex;
-		}
+    public void selectPoint(int segmentIndex, double segmentParameter, Point mouseInScene) {
+        // store handle part information
+        if (segmentParameter == 1) {
+            linkIndex = segmentIndex + 1;
+        } else {
+            linkIndex = segmentIndex;
+        }
 
-		// initialize position
-		startPointInScene = new Point2D(mouseInScene.x, mouseInScene.y);
-		startPoint = op.getNewLinks().get(linkIndex).getPosition();
-		currentPoint = startPoint.getCopy();
-	}
+        // initialize position
+        startPointInScene = new Point2D(mouseInScene.x, mouseInScene.y);
+        startPoint = op.getNewLinks().get(linkIndex).getPosition();
+        currentPoint = startPoint.getCopy();
+    }
 
-	@Override
-	public String toString() {
-		return "FXMoveBendPointPolicy[host=" + getHost() + "]";
-	}
+    @Override
+    public String toString() {
+        return "FXMoveBendPointPolicy[host=" + getHost() + "]";
+    }
 
-	protected void updateCurrentAnchorLink(Point mouseInScene,
-			List<IContentPart<Node>> partsUnderMouse) {
-		// snaps for start and end (TODO: configurable)
-		boolean snaps = linkIndex == 0
-				|| linkIndex == op.getNewLinks().size() - 1;
+    protected void updateCurrentAnchorLink(Point mouseInScene, List<IContentPart<Node>> partsUnderMouse) {
+        // snaps for start and end (TODO: configurable)
+        boolean snaps = linkIndex == 0 || linkIndex == op.getNewLinks().size() - 1;
 
-		AnchorLink link = null;
-		if (snaps) {
-			AbstractFXContentPart anchorPart = getAnchorPart(partsUnderMouse);
-			if (anchorPart != null) {
-				// use anchor returned by part
-				IFXAnchor anchor = anchorPart.getAnchor(getHost());
-				link = new AnchorLink(anchor, op.getOldLinks().get(linkIndex)
-						.getKey());
-			}
-		}
+        AnchorLink link = null;
+        if (snaps) {
+            AbstractFXContentPart anchorPart = getAnchorPart(partsUnderMouse);
+            if (anchorPart != null) {
+                // use anchor returned by part
+                IFXAnchor anchor = anchorPart.getAnchor(getHost());
+                link = new AnchorLink(anchor, op.getOldLinks().get(linkIndex).getKey());
+            }
+        }
 
-		if (link == null) {
-			// use static anchor, re-use key
-			AnchorKey key = op.getNewLinks().get(linkIndex).getKey();
-			FXStaticAnchor anchor = new FXStaticAnchor(currentPoint);
-			link = new AnchorLink(anchor, key);
-		}
+        if (link == null) {
+            // use static anchor, re-use key
+            AnchorKey key = op.getNewLinks().get(linkIndex).getKey();
 
-		op.getNewLinks().set(linkIndex, link);
-	}
+            FXStaticAnchor anchor =
+                    new FXStaticAnchor(JavaFX2Geometry.toPoint(getHostVisual().localToScene(currentPoint.x,
+                            currentPoint.y)));
+            link = new AnchorLink(anchor, key);
+        }
+
+        op.getNewLinks().set(linkIndex, link);
+    }
 
 }
