39b88c355d3ea8aa6ef3588561b7f52d1535f45c
anyssen <alexander.nyssen@itemis.de>
2014-08-17 21:50:23 +0200

    [NONE] Include org.eclipse.gef4.fx.tests bundle.

---
+++ org.eclipse.gef4.fx.tests/src/org/eclipse/gef4/fx/tests/FXCurveConnectionTests.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.gef4.fx.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.gef4.fx.nodes.FXCurveConnection;
+import org.eclipse.gef4.fx.nodes.IFXConnection;
+import org.eclipse.gef4.geometry.planar.Point;
+import org.junit.Test;
+
+public class FXCurveConnectionTests {
+
+	@Test
+	public void test_PointConversions() {
+		IFXConnection connection = new FXCurveConnection();
+		Point startPoint = new Point(123, 456);
+		Point wayPoint = new Point(789, 123);
+		Point endPoint = new Point(456, 789);
+		connection.setStartPoint(startPoint);
+		connection.setWayPoint(0, wayPoint);
+		connection.setEndPoint(endPoint);
+		assertEquals(startPoint, connection.getStartPoint());
+		assertEquals(wayPoint, connection.getWayPoint(0));
+		assertEquals(wayPoint, connection.getEndPoint());
+	}
+}
