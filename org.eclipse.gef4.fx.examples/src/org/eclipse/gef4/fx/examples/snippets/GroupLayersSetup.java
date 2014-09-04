384be0d0c9c6c709c45c33e3eb1d3c66952b3932
anyssen <alexander.nyssen@itemis.de>
2014-09-04 20:49:45 +0200

    [NONE] Add initial grid layer implementation.

---
+++ org.eclipse.gef4.fx.examples/src/org/eclipse/gef4/fx/examples/snippets/GroupLayersSetup.java
@@ -0,0 +1,213 @@
+package org.eclipse.gef4.fx.examples.snippets;
+
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.geometry.Bounds;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.transform.Scale;
+import javafx.scene.transform.Transform;
+import javafx.stage.Stage;
+
+import org.eclipse.gef4.fx.listeners.VisualChangeListener;
+import org.eclipse.gef4.fx.nodes.FXGeometryNode;
+import org.eclipse.gef4.fx.nodes.FXGridLayer;
+import org.eclipse.gef4.fx.nodes.FXUtils;
+import org.eclipse.gef4.geometry.planar.AffineTransform;
+import org.eclipse.gef4.geometry.planar.IGeometry;
+
+public class GroupLayersSetup extends Application {
+
+	public static void main(final String[] args) {
+		launch();
+	}
+
+	public StackPane layersStackPane;
+	public Group contentLayer;
+	public Group handleLayer;
+	public Group feedbackLayer;
+	public Scene scene;
+	private ScrollPane scrollPane;
+	private FXGridLayer gridLayer;
+	private Group bgLayer;
+
+	private final SimpleObjectProperty<Scale> scaleProperty = new SimpleObjectProperty<Scale>(
+			new Scale());
+
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		primaryStage.setScene(createScene());
+		primaryStage.sizeToScene();
+		primaryStage.show();
+	}
+
+	public Scene createScene() {
+		// scale
+		scaleProperty.get().setX(0.5);
+		scaleProperty.get().setY(0.5);
+
+		// layers
+		bgLayer = new Group();
+		final Rectangle bgContent = getBGRect(0.5, 0, 0, 0.5);
+		final Rectangle bgFeedback = getBGRect(0, 0.5, 0, 0.5);
+		final Rectangle bgHandle = getBGRect(0, 0, 0.5, 0.5);
+
+		gridLayer = new FXGridLayer();
+		// gridLayer.setStyle("-fx-background-color: rgba(127, 127, 0,
+		// 0.5);");
+
+		contentLayer = new Group();
+		showBg(contentLayer, bgContent);
+		contentLayer.setPickOnBounds(true);
+
+		feedbackLayer = new Group();
+		showBg(feedbackLayer, bgFeedback);
+		feedbackLayer.setPickOnBounds(false);
+		feedbackLayer.setMouseTransparent(true);
+
+		handleLayer = new Group();
+		showBg(handleLayer, bgHandle);
+		handleLayer.setPickOnBounds(false);
+
+		// scrolling
+		final String SCROLL_PANE_STYLE = "-fx-background-insets:0;-fx-padding:0;-fx-background-color:rgba(0,0,0,0);";
+		scrollPane = new ScrollPane();
+		final Group spi = new Group(bgLayer, gridLayer, contentLayer,
+				feedbackLayer, handleLayer);
+		scrollPane.setContent(spi);
+		scrollPane.setPannable(false);
+		scrollPane.setStyle(SCROLL_PANE_STYLE);
+
+		// scene
+		scene = new Scene(scrollPane, 800, 600);
+
+		// register the grid layer
+		gridLayer.bindToScale(scaleProperty);
+		gridLayer.bindMinSizeToBounds(scrollPane.viewportBoundsProperty());
+		gridLayer.bindPrefSizeToUnionedBounds(new ReadOnlyObjectProperty[] {
+				contentLayer.boundsInParentProperty(),
+				feedbackLayer.boundsInParentProperty(),
+				handleLayer.boundsInParentProperty() });
+
+		// create content and feedback
+		final FXGeometryNode red = new FXGeometryNode<org.eclipse.gef4.geometry.planar.Rectangle>(
+				new org.eclipse.gef4.geometry.planar.Rectangle(0, 0, 100, 100));
+		final FXGeometryNode blue = new FXGeometryNode<org.eclipse.gef4.geometry.planar.Rectangle>(
+				new org.eclipse.gef4.geometry.planar.Rectangle(0, 0, 100, 100));
+		red.setFill(Color.RED);
+		blue.setFill(Color.BLUE);
+		red.setStrokeType(StrokeType.OUTSIDE);
+		blue.setStrokeType(StrokeType.OUTSIDE);
+		red.setStrokeWidth(25);
+		blue.setStrokeWidth(25);
+
+		contentLayer.getChildren().addAll(red, q(50, Color.RED));
+		final Rectangle feedbackElement = q(50, Color.BLUE);
+		feedbackElement.setLayoutX(200);
+		feedbackElement.setLayoutY(0);
+		feedbackLayer.getChildren().addAll(blue, feedbackElement);
+
+		// scale content layer
+		bgContent.getTransforms().add(scaleProperty.get());
+		contentLayer.getTransforms().add(scaleProperty.get());
+
+		// move to initial positions
+		move(red, 100, 0);
+		move(blue, 200, 100);
+
+		new VisualChangeListener() {
+			@Override
+			protected void localToParentTransformChanged(final Node source,
+					final Transform oldTransform, final Transform newTransform) {
+				update();
+			}
+
+			private void update() {
+				final IGeometry geomInRed = red.getGeometry();
+				final IGeometry geomInScene = FXUtils.localToScene(red,
+						geomInRed);
+				final IGeometry geomInBlue = FXUtils.sceneToLocal(blue,
+						geomInScene);
+				final IGeometry transformedInBlue = geomInBlue
+						.getTransformed(new AffineTransform(1, 0, 0, 1, 100,
+								100));
+				blue.setGeometry(transformedInBlue);
+			}
+
+			@Override
+			protected void boundsInLocalChanged(final Bounds oldBounds,
+					final Bounds newBounds) {
+				update();
+			}
+		}.register(red, blue);
+
+		draggable(red);
+
+		return scene;
+	}
+
+	private void showBg(final Node layer, final Rectangle bg) {
+		layer.layoutBoundsProperty().addListener(new ChangeListener<Bounds>() {
+			@Override
+			public void changed(
+					final ObservableValue<? extends Bounds> observable,
+					final Bounds oldValue, final Bounds newValue) {
+				System.out.println("show bg for " + layer);
+				bg.setX(newValue.getMinX() + layer.getLayoutX());
+				bg.setY(newValue.getMinY() + layer.getLayoutY());
+				bg.setWidth(newValue.getWidth());
+				bg.setHeight(newValue.getHeight());
+			}
+		});
+	}
+
+	private Rectangle getBGRect(final double r, final double g, final double b,
+			final double a) {
+		final Rectangle bg = new Rectangle();
+		bg.setStrokeWidth(0);
+		bg.setFill(new Color(r, g, b, a));
+		bgLayer.getChildren().add(bg);
+		return bg;
+	}
+
+	private void draggable(final Node n) {
+		n.addEventHandler(MouseEvent.ANY, new EventHandler<MouseEvent>() {
+			double x = 0, y = 0;
+
+			@Override
+			public void handle(final MouseEvent event) {
+				if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+					move(n, event.getSceneX() - x, event.getSceneY() - y);
+				}
+				x = event.getSceneX();
+				y = event.getSceneY();
+			}
+		});
+	}
+
+	private <T extends Node> T move(final T n, final double byX,
+			final double byY) {
+		n.relocate(n.getLayoutX() + n.getLayoutBounds().getMinX() + byX,
+				n.getLayoutY() + n.getLayoutBounds().getMinY() + byY);
+		return n;
+	}
+
+	private Rectangle q(final double size, final Color fill) {
+		final Rectangle q = new Rectangle(size, size);
+		q.setFill(fill);
+		q.setStroke(Color.BLACK);
+		return q;
+	}
+
+}
---
+++ org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/nodes/FXGridLayer.java
@@ -0,0 +1,254 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.gef4.fx.nodes;
+
+import javafx.beans.Observable;
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Bounds;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Scale;
+
+public class FXGridLayer extends Pane {
+
+	private class GridCanvas extends Canvas {
+
+		public GridCanvas() {
+			// Redraw canvas when size changes.
+			widthProperty().addListener(new ChangeListener<Number>() {
+				@Override
+				public void changed(
+						final ObservableValue<? extends Number> observable,
+						final Number oldValue, final Number newValue) {
+					repaintGrid();
+				}
+			});
+			heightProperty().addListener(new ChangeListener<Number>() {
+				@Override
+				public void changed(
+						final ObservableValue<? extends Number> observable,
+						final Number oldValue, final Number newValue) {
+					repaintGrid();
+				}
+			});
+		}
+
+		@Override
+		public boolean isResizable() {
+			return true;
+		}
+
+		@Override
+		public double prefHeight(final double width) {
+			return getHeight();
+		}
+
+		@Override
+		public double prefWidth(final double height) {
+			return getWidth();
+		}
+
+		public void repaintGrid() {
+			final double width = getWidth();
+			final double height = getHeight();
+
+			final GraphicsContext gc = getGraphicsContext2D();
+			gc.setFill(Color.WHITE);
+			gc.fillRect(0, 0, width, height);
+
+			// don't paint grid if size is to large (TODO: remove canvas (make
+			// invisible)
+			if ((width > 2500) || (height > 2500)) {
+				return;
+			}
+
+			gc.setStroke(Color.GREY);
+			final Scale scale = scaleProperty.get();
+			for (double x = ((-getParent().getLayoutX()) / scale.getX()) % 10; x < width; x += 10) {
+				for (double y = ((-getParent().getLayoutY()) / scale.getY()) % 10; y < height; y += 10) {
+					// TODO: use circle
+					gc.strokeLine(x, y, x, y);
+				}
+			}
+		}
+	}
+
+	private final FXGridLayer.GridCanvas gridCanvas;
+	private final SimpleObjectProperty<Scale> scaleProperty = new SimpleObjectProperty<Scale>(
+			new Scale());
+
+	public FXGridLayer() {
+		final Scale scale = new Scale();
+		getTransforms().add(scale);
+		scaleProperty.addListener(new ChangeListener<Scale>() {
+			@Override
+			public void changed(
+					final ObservableValue<? extends Scale> observable,
+					final Scale oldValue, final Scale newValue) {
+				scale.setX(newValue.getX());
+				scale.setY(newValue.getY());
+			}
+		});
+		gridCanvas = new GridCanvas();
+		getChildren().add(gridCanvas);
+		setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		gridCanvas.widthProperty().bind(widthProperty());
+		gridCanvas.heightProperty().bind(heightProperty());
+		setPickOnBounds(false);
+		setMouseTransparent(true);
+
+		layoutXProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(
+					final ObservableValue<? extends Number> observable,
+					final Number oldValue, final Number newValue) {
+				gridCanvas.repaintGrid();
+			}
+		});
+		layoutYProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(
+					final ObservableValue<? extends Number> observable,
+					final Number oldValue, final Number newValue) {
+				gridCanvas.repaintGrid();
+			}
+		});
+	}
+
+	public void bindMinSizeToBounds(
+			final ReadOnlyObjectProperty<Bounds> unscaledMinSizeProperty) {
+		minWidthProperty().bind(new DoubleBinding() {
+			{
+				super.bind(unscaledMinSizeProperty, scaleProperty.get()
+						.xProperty());
+			}
+
+			@Override
+			protected double computeValue() {
+				if (unscaledMinSizeProperty.get() == null) {
+					return 0;
+				}
+				return Math
+						.ceil(((unscaledMinSizeProperty.get().getWidth() / scaleProperty
+								.get().getX()) - 1));
+			}
+		});
+
+		minHeightProperty().bind(new DoubleBinding() {
+			{
+				super.bind(unscaledMinSizeProperty, scaleProperty.get()
+						.yProperty());
+			}
+
+			@Override
+			protected double computeValue() {
+				if (unscaledMinSizeProperty.get() == null) {
+					return 0;
+				}
+				return Math
+						.ceil((unscaledMinSizeProperty.get().getHeight() / scaleProperty
+								.get().getY()) - 1);
+			}
+		});
+	}
+
+	public void bindPrefSizeToUnionedBounds(
+			@SuppressWarnings("unchecked") final ReadOnlyObjectProperty<Bounds>... unscaledBoundsProperties) {
+
+		layoutXProperty().bind(new DoubleBinding() {
+			{
+				super.bind(unscaledBoundsProperties);
+			}
+
+			@Override
+			protected double computeValue() {
+				double minX = 0;
+				for (final ReadOnlyObjectProperty<Bounds> b : unscaledBoundsProperties) {
+					minX = Math.min(minX, b.get().getMinX());
+				}
+				return minX;
+			}
+		});
+
+		layoutYProperty().bind(new DoubleBinding() {
+			{
+				super.bind(unscaledBoundsProperties);
+			}
+
+			@Override
+			protected double computeValue() {
+				double minY = 0;
+				for (final ReadOnlyObjectProperty<Bounds> b : unscaledBoundsProperties) {
+					minY = Math.min(minY, b.get().getMinY());
+				}
+				return minY;
+			}
+		});
+
+		prefWidthProperty().bind(new DoubleBinding() {
+			{
+				final Observable[] observables = new Observable[unscaledBoundsProperties.length + 2];
+				observables[0] = layoutXProperty();
+				observables[1] = scaleProperty.get().xProperty();
+				for (int i = 0; i < unscaledBoundsProperties.length; i++) {
+					observables[i + 2] = unscaledBoundsProperties[i];
+				}
+				super.bind(observables);
+			}
+
+			@Override
+			protected double computeValue() {
+				double maxX = 0;
+				for (final ReadOnlyObjectProperty<Bounds> b : unscaledBoundsProperties) {
+					maxX = Math.max(maxX, b.get().getMaxX());
+				}
+				return maxX
+						- (layoutXProperty().get() / scaleProperty.get().getX());
+			}
+		});
+
+		prefHeightProperty().bind(new DoubleBinding() {
+			{
+				final Observable[] observables = new Observable[unscaledBoundsProperties.length + 2];
+				observables[0] = layoutYProperty();
+				observables[1] = scaleProperty.get().yProperty();
+				for (int i = 0; i < unscaledBoundsProperties.length; i++) {
+					observables[i + 2] = unscaledBoundsProperties[i];
+				}
+				super.bind(observables);
+			}
+
+			@Override
+			protected double computeValue() {
+				double maxY = 0;
+				for (final ReadOnlyObjectProperty<Bounds> b : unscaledBoundsProperties) {
+					maxY = Math.max(maxY, b.get().getMaxY());
+				}
+				return maxY
+						- (layoutYProperty().get() / scaleProperty.get().getY());
+			}
+		});
+
+	}
+
+	public void bindToScale(ReadOnlyObjectProperty<Scale> scaleProperty) {
+		this.scaleProperty.bind(scaleProperty);
+	}
+
+}
