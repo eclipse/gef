82540ec24c92af1a552d7d921b70d5d9f6dce5b8
mwienand <matthias.wienand@itemis.de>
2015-01-08 14:02:53 +0100

    [NONE] Stop at content-bounds when scrolling/panning.

--- org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/policies/FXPanOnScrollPolicy.java
+++ org.eclipse.gef4.mvc.fx/src/org/eclipse/gef4/mvc/fx/policies/FXPanOnScrollPolicy.java
@@ -11,11 +11,18 @@
  *******************************************************************************/
 package org.eclipse.gef4.mvc.fx.policies;
 
+import javafx.geometry.Bounds;
 import javafx.scene.input.ScrollEvent;
 
+import org.eclipse.gef4.fx.nodes.ScrollPaneEx;
+import org.eclipse.gef4.mvc.fx.parts.FXRootPart;
 import org.eclipse.gef4.mvc.models.ViewportModel;
 
 public class FXPanOnScrollPolicy extends AbstractFXScrollPolicy {
+
+	private static final int SAME_SCROLL_MILLIS = 70;
+	private long lastMillis = 0;
+	private boolean stopped = false;
 
 	@Override
 	public void scroll(ScrollEvent event) {
@@ -25,13 +32,51 @@ public class FXPanOnScrollPolicy extends AbstractFXScrollPolicy {
 			return;
 		}
 
-		// TODO obtain policy to manipulate the viewport model
+		// do not scroll when this gesture was stopped at the content bounds
+		long millis = System.currentTimeMillis();
+		if (millis - lastMillis < SAME_SCROLL_MILLIS) {
+			if (stopped) {
+				return;
+			}
+		} else {
+			stopped = false;
+		}
+		lastMillis = millis;
+
 		double x = event.getDeltaX();
 		double y = event.getDeltaY();
+
+		// stop at content bounds
+		ScrollPaneEx scrollPane = ((FXRootPart) getHost().getRoot())
+				.getScrollPane();
+		Bounds contentBounds = scrollPane.getBoundsInViewport(scrollPane
+				.getContentGroup());
+		// horizontal
+		if (contentBounds.getMinX() < 0 && contentBounds.getMinX() + x > 0) {
+			x = -contentBounds.getMinX();
+			stopped = true;
+		}
+		if (contentBounds.getMaxX() > scrollPane.getWidth()
+				&& contentBounds.getMaxX() + x < scrollPane.getWidth()) {
+			x = scrollPane.getWidth() - contentBounds.getMaxX();
+			stopped = true;
+		}
+		// vertical
+		if (contentBounds.getMinY() < 0 && contentBounds.getMinY() + y > 0) {
+			y = -contentBounds.getMinY();
+			stopped = true;
+		}
+		if (contentBounds.getMaxY() > scrollPane.getHeight()
+				&& contentBounds.getMaxY() + y < scrollPane.getHeight()) {
+			y = scrollPane.getHeight() - contentBounds.getMaxY();
+			stopped = true;
+		}
+
+		// TODO obtain policy to manipulate the viewport model
 		ViewportModel viewportModel = getHost().getRoot().getViewer()
 				.getAdapter(ViewportModel.class);
 		viewportModel.setTranslateX(viewportModel.getTranslateX() + x);
-		viewportModel.setTranslateY(viewportModel.getTranslateY() - y);
+		viewportModel.setTranslateY(viewportModel.getTranslateY() + y);
 	}
 
 }
