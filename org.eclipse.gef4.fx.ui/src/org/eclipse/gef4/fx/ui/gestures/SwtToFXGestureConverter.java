9dd3af794054d3a36077e9974a5791afb7a0b618
anyssen <alexander.nyssen@itemis.de>
2015-01-08 17:14:04 +0100

    [NONE] Fix scroll event y-offset was interpreted differently in standalone and swt-embedded case.
    The sign value of the scroll event y-value was interpreted differently in the JavaFX standalone and the SWT embedded case. Fixed that by changing the behavior of the gesture converter.

--- org.eclipse.gef4.fx.ui/src/org/eclipse/gef4/fx/ui/gestures/SwtToFXGestureConverter.java
+++ org.eclipse.gef4.fx.ui/src/org/eclipse/gef4/fx/ui/gestures/SwtToFXGestureConverter.java
@@ -262,7 +262,7 @@ public class SwtToFXGestureConverter implements GestureListener {
 				0,
 				0, // totalScrollX, totalScrollY
 				-5.0,
-				5.0, // xMultiplier, yMultiplier
+				-5.0, // xMultiplier, yMultiplier
 				0, // touchCount
 				0,
 				0, // scrollTextX, scrollTextY
