b01f37b47ebb05e7f155f556e6fe42b4beb886a9
anyssen <alexander.nyssen@itemis.de>
2015-01-11 15:52:15 +0100

    [NONE] Extracted AbstractMvcExample.

---
+++ org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/MvcLogoExample.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.gef4.mvc.examples.logo;
+
+import java.util.Collections;
+import java.util.List;
+
+import javafx.application.Application;
+
+import org.eclipse.gef4.mvc.examples.AbstractMvcExample;
+import org.eclipse.gef4.mvc.examples.logo.model.FXGeometricModel;
+
+import com.google.inject.Module;
+
+public class MvcLogoExample extends AbstractMvcExample {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	public MvcLogoExample() {
+		super("GEF4 MVC Logo Example");
+	}
+
+	@Override
+	protected List<? extends Object> createContents() {
+		return Collections.singletonList(new FXGeometricModel());
+	}
+
+	@Override
+	protected Module createModule() {
+		return new MvcLogoExampleModule();
+	}
+}
---
+++ org.eclipse.gef4.mvc.examples/src/org/eclipse/gef4/mvc/examples/AbstractMvcExample.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.gef4.mvc.examples;
+
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import org.eclipse.gef4.mvc.fx.domain.FXDomain;
+import org.eclipse.gef4.mvc.fx.viewer.FXStageSceneContainer;
+import org.eclipse.gef4.mvc.fx.viewer.FXViewer;
+import org.eclipse.gef4.mvc.models.ContentModel;
+import org.eclipse.gef4.mvc.viewer.IViewer;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+
+public abstract class AbstractMvcExample extends Application {
+
+	protected final String title;
+
+	public AbstractMvcExample(String title) {
+		this.title = title;
+	}
+
+	protected abstract Module createModule();
+
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		Injector injector = Guice.createInjector(createModule());
+		FXDomain domain = new FXDomain();
+		injector.injectMembers(domain);
+
+		FXViewer viewer = domain.getAdapter(IViewer.class);
+		viewer.setSceneContainer(new FXStageSceneContainer(primaryStage));
+
+		// activate domain only after viewers have been hooked
+		domain.activate();
+
+		viewer.getAdapter(ContentModel.class).setContents(
+				createContents());
+
+		primaryStage.setResizable(true);
+		primaryStage.setWidth(640);
+		primaryStage.setHeight(480);
+		primaryStage.setTitle(title);
+		primaryStage.sizeToScene();
+		primaryStage.show();
+	}
+
+	protected abstract List<? extends Object> createContents();
+}
