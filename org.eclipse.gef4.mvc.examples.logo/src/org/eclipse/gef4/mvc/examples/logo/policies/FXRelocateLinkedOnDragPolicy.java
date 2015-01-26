276a15150395a5d616adf83fd1fb8341ed360a95
mwienand <matthias.wienand@itemis.de>
2015-01-26 17:56:04 +0100

    [458280] Move all parts "linked" to any selected part within FXRelocateLinkedOnDragPolicy.
    Previously, only the parts "linked" to the pressed part were relocated along
    with the selection.

--- org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/policies/FXRelocateLinkedOnDragPolicy.java
+++ org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/policies/FXRelocateLinkedOnDragPolicy.java
@@ -1,6 +1,7 @@
 package org.eclipse.gef4.mvc.examples.logo.policies;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javafx.scene.Node;
@@ -14,9 +15,16 @@ public class FXRelocateLinkedOnDragPolicy extends FXRelocateOnDragPolicy {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<IContentPart<Node, ? extends Node>> getTargetParts() {
-		// ensure that linked parts are moved with us during dragging
-		return new ArrayList<>(PartUtils.filterParts(
-				PartUtils.getAnchoreds(getHost(), "link"), IContentPart.class));
+		List<IContentPart<Node, ? extends Node>> selected = super
+				.getTargetParts();
+		List<IContentPart<Node, ? extends Node>> linked = new ArrayList<IContentPart<Node, ? extends Node>>();
+		for (IContentPart<Node, ? extends Node> cp : selected) {
+			// ensure that linked parts are moved with us during dragging
+			linked.addAll((Collection<? extends IContentPart<Node, ? extends Node>>) new ArrayList<>(
+					PartUtils.filterParts(PartUtils.getAnchoreds(cp, "link"),
+							IContentPart.class)));
+		}
+		return linked;
 	}
 
 }
