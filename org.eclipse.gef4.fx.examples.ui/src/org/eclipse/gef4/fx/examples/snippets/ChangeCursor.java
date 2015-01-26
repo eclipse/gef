544015dcdb75cbd63e702a8748e4edb3d4b7efe8
mwienand <matthias.wienand@itemis.de>
2015-01-26 16:56:24 +0100

    [458274] Change SWT Cursor according to JavaFX Cursor.

---
+++ org.eclipse.gef4.fx.examples.ui/src/org/eclipse/gef4/fx/examples/snippets/ChangeCursor.java
@@ -0,0 +1,39 @@
+package org.eclipse.gef4.fx.examples.snippets;
+
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
+public class ChangeCursor extends AbstractFXUIExamples {
+
+	public static void main(String[] args) {
+		new ChangeCursor();
+	}
+
+	public ChangeCursor() {
+	}
+
+	@Override
+	public Scene createScene() {
+		Rectangle rect = new Rectangle(100, 50, 100, 50);
+		Group root = new Group(rect);
+		final Scene scene = new Scene(root, 400, 300);
+		rect.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				scene.setCursor(Cursor.CROSSHAIR);
+			}
+		});
+		rect.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				scene.setCursor(null);
+			}
+		});
+		return scene;
+	}
+
+}
--- org.eclipse.gef4.fx.ui/src/org/eclipse/gef4/fx/ui/canvas/FXCanvasEx.java
+++ org.eclipse.gef4.fx.ui/src/org/eclipse/gef4/fx/ui/canvas/FXCanvasEx.java
@@ -14,21 +14,82 @@
 package org.eclipse.gef4.fx.ui.canvas;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swt.FXCanvas;
+import javafx.embed.swt.SWTFXUtils;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
+import javafx.scene.Scene;
 import javafx.stage.Window;
 
 import org.eclipse.gef4.common.reflect.ReflectionUtils;
 import org.eclipse.gef4.fx.ui.gestures.SwtToFXGestureConverter;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Composite;
 
 public class FXCanvasEx extends FXCanvas {
 
+	private static Map<Cursor, Integer> CURSOR_FROM_FX_TO_SWT = new HashMap<Cursor, Integer>() {
+		private static final long serialVersionUID = 1L;
+
+		{
+			// TODO: verify cursors marked with XXX
+			put(Cursor.CLOSED_HAND, SWT.CURSOR_HAND); // XXX
+			put(Cursor.CROSSHAIR, SWT.CURSOR_CROSS);
+			put(Cursor.DISAPPEAR, SWT.CURSOR_NO); // XXX
+			put(Cursor.E_RESIZE, SWT.CURSOR_SIZEE);
+			put(Cursor.H_RESIZE, SWT.CURSOR_SIZEWE);
+			put(Cursor.HAND, SWT.CURSOR_HAND);
+			put(Cursor.MOVE, SWT.CURSOR_CROSS); // XXX
+			put(Cursor.N_RESIZE, SWT.CURSOR_SIZEN);
+			put(Cursor.NE_RESIZE, SWT.CURSOR_SIZENE);
+			put(Cursor.NONE, SWT.CURSOR_NO);
+			put(Cursor.NW_RESIZE, SWT.CURSOR_SIZENW);
+			put(Cursor.OPEN_HAND, SWT.CURSOR_HAND);// XXX
+			put(Cursor.S_RESIZE, SWT.CURSOR_SIZES);
+			put(Cursor.SE_RESIZE, SWT.CURSOR_SIZESE);
+			put(Cursor.SW_RESIZE, SWT.CURSOR_SIZESW);
+			put(Cursor.TEXT, SWT.CURSOR_IBEAM);
+			put(Cursor.V_RESIZE, SWT.CURSOR_SIZENS);
+			put(Cursor.W_RESIZE, SWT.CURSOR_SIZEW);
+			put(Cursor.WAIT, SWT.CURSOR_WAIT);
+		}
+	};
+
 	private SwtToFXGestureConverter gestureConverter;
 	private List<FocusListener> focusListeners;
+	private ChangeListener<Cursor> cursorChangeListener = new ChangeListener<Cursor>() {
+		@Override
+		public void changed(ObservableValue<? extends Cursor> observable,
+				Cursor oldCursor, Cursor newCursor) {
+			if (newCursor instanceof ImageCursor) {
+				// custom cursor, convert image
+				ImageData imageData = SWTFXUtils.fromFXImage(
+						((ImageCursor) newCursor).getImage(), null);
+				double hotspotX = ((ImageCursor) newCursor).getHotspotX();
+				double hotspotY = ((ImageCursor) newCursor).getHotspotY();
+				org.eclipse.swt.graphics.Cursor swtCursor = new org.eclipse.swt.graphics.Cursor(
+						getDisplay(), imageData, (int) hotspotX, (int) hotspotY);
+				getShell().setCursor(swtCursor);
+			} else if (CURSOR_FROM_FX_TO_SWT.containsKey(newCursor)) {
+				// standard cursor, look up in map
+				getShell().setCursor(
+						new org.eclipse.swt.graphics.Cursor(getDisplay(),
+								CURSOR_FROM_FX_TO_SWT.get(newCursor)));
+			} else {
+				// unknown cursor, use default
+				getShell().setCursor(null);
+			}
+		}
+	};
 
 	public FXCanvasEx(Composite parent, int style) {
 		super(parent, style);
@@ -87,4 +148,22 @@ public class FXCanvasEx extends FXCanvas {
 			focusListeners.remove(listener);
 		}
 	}
+
+	@Override
+	public void setCursor(org.eclipse.swt.graphics.Cursor cursor) {
+		// Do not overwrite the JavaFX cursor.
+	}
+
+	@Override
+	public void setScene(Scene newScene) {
+		Scene oldScene = getScene();
+		if (oldScene != null) {
+			oldScene.cursorProperty().removeListener(cursorChangeListener);
+		}
+		super.setScene(newScene);
+		if (newScene != null) {
+			newScene.cursorProperty().addListener(cursorChangeListener);
+		}
+	}
+
 }
