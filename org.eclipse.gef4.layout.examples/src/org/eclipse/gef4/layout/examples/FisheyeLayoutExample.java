d1105e98433ba5b80721e3b3a39fca0f2a99ec75
mwienand <matthias.wienand@itemis.de>
2015-03-03 11:57:08 +0100

    [438734] Migrate FisheyeGraphSnippet to FisheyeLayoutExample.

---
+++ org.eclipse.gef4.layout.examples/src/org/eclipse/gef4/layout/examples/FisheyeLayoutExample.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial API & implementation
+ *
+ * Note: Parts of this class have been transferred from org.eclipse.gef4.zest.examples.layout.FisheyeGraphSnippet
+ *
+ *******************************************************************************/
+package org.eclipse.gef4.layout.examples;
+
+import javafx.application.Application;
+
+import org.eclipse.gef4.graph.Graph;
+import org.eclipse.gef4.graph.Node;
+import org.eclipse.gef4.layout.LayoutAlgorithm;
+import org.eclipse.gef4.layout.algorithms.GridLayoutAlgorithm;
+import org.eclipse.gef4.zest.examples.AbstractZestExample;
+
+public class FisheyeLayoutExample extends AbstractZestExample {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	public FisheyeLayoutExample() {
+		super("GEF4 Layout - Fisheye Example");
+	}
+
+	@Override
+	protected Graph createGraph() {
+		// TODO: images for info, warn, error
+		Graph g = new Graph();
+		for (int i = 0; i < 80; i++) {
+			Node n1 = n(g, LABEL, "Information", "fisheye", true);
+			Node n2 = n(g, LABEL, "Warning", "fisheye", true);
+			Node n3 = n(g, LABEL, "Error", "fisheye", true);
+			e(g, n1, n2, LABEL, "");
+			e(g, n2, n3, LABEL, "");
+		}
+		return g;
+	}
+
+	@Override
+	protected LayoutAlgorithm createLayoutAlgorithm() {
+		return new GridLayoutAlgorithm();
+	}
+
+	@Override
+	protected int getStageHeight() {
+		return 650;
+	}
+
+	@Override
+	protected int getStageWidth() {
+		return 550;
+	}
+
+}
