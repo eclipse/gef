fbd5b2a8711c67cdfcd1ea972356fa9b7f91ea84
mwienand <matthias.wienand@itemis.de>
2015-03-09 16:36:23 +0100

    [461421] Add LayoutModelTests to ensure that the mapping from Graph to LayoutContext works as intended.

--- org.eclipse.gef4.zest.fx.tests/src/org/eclipse/gef4/zest/fx/tests/AllTests.java
+++ org.eclipse.gef4.zest.fx.tests/src/org/eclipse/gef4/zest/fx/tests/AllTests.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (c) 2014 itemis AG and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Matthias Wienand (itemis AG) - initial API & implementation
- *     
+ *
  *******************************************************************************/
 package org.eclipse.gef4.zest.fx.tests;
 
@@ -17,6 +17,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GraphLayoutContextTests.class, NodeLayoutBehaviorTests.class })
+@SuiteClasses({ GraphLayoutContextTests.class, NodeLayoutBehaviorTests.class,
+		LayoutModelTests.class })
 public class AllTests {
 }
