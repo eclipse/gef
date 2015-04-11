eclipse/gef

commit 1e92f86657d7882e1c39645955638dafe7df502a

[337644] Update graph view in sync mode when DOT editor is selected

org.eclipse.gef4.dot.ui/src/org/eclipse/gef4/internal/dot/ui/DotGraphView.java
@@ -45,17 +45,22 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ResourceListSelectionDialog;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.xtext.ui.editor.XtextEditor;
 
 import com.google.inject.Guice;
 import com.google.inject.util.Modules;
@@ -299,6 +304,9 @@ public class DotGraphView extends ZestFxUiView {
 		/** If a *.dot file is visited, update the graph. */
 		private IResourceDeltaVisitor resourceVisitor;
 
+		/** Listen to selection changes and update graph in view. */
+		protected ISelectionListener selectionChangeListener = null;
+
 		Action action(final DotGraphView view) {
 
 			Action toggleUpdateModeAction = new Action(
@@ -312,15 +320,37 @@ public class DotGraphView extends ZestFxUiView {
 
 				private void toggleResourceListener() {
 					IWorkspace workspace = ResourcesPlugin.getWorkspace();
+					ISelectionService service = getSite().getWorkbenchWindow()
+							.getSelectionService();
 					if (view.listenToDotContent) {
 						workspace.addResourceChangeListener(
 								resourceChangeListener,
 								IResourceChangeEvent.POST_BUILD
 										| IResourceChangeEvent.POST_CHANGE);
+						service.addSelectionListener(selectionChangeListener);
 					} else {
 						workspace
 								.removeResourceChangeListener(resourceChangeListener);
+						service.removeSelectionListener(selectionChangeListener);
 					}
+				}
+
+			};
+
+			selectionChangeListener = new ISelectionListener() {
+				@Override
+				public void selectionChanged(IWorkbenchPart part,
+						ISelection selection) {
+					if (part instanceof XtextEditor) {
+						XtextEditor editor = (XtextEditor) part;
+						if ("org.eclipse.gef4.internal.dot.parser.Dot" //$NON-NLS-1$
+								.equals(editor.getLanguageName())
+								&& editor.getEditorInput() instanceof FileEditorInput) {
+							view.updateGraph(((FileEditorInput) editor
+									.getEditorInput()).getFile());
+						}
+					}
+
 				}
 			};
 

commit d304de6a4cd676f20d8aee225865159555977574

[337644] Fix issues exposed by image export on editor selection
This fixes issues that were exposed by functionality introduced in
1e92f86657d7882e1c39645955638dafe7df502a, resulting in the exported
PDF being set as the view's current DOT file, unwanted usage of
temp files, and unnecessary image reopening.

org.eclipse.gef4.dot.ui/src/org/eclipse/gef4/internal/dot/ui/DotGraphView.java
@@ -212,6 +212,7 @@ public class DotGraphView extends ZestFxUiView {
 		String dotString = currentDot;
 		dotString = DotFileUtils.read(file);
 		currentDot = dotString;
+		currentFile = file;
 		setGraphAsync(dotString, file);
 		return true;
 	}
@@ -399,10 +400,13 @@ public class DotGraphView extends ZestFxUiView {
 								.equals(editor.getLanguageName())
 								&& editor.getEditorInput() instanceof FileEditorInput) {
 							try {
-								view.updateGraph(DotFileUtils
+								File resolvedFile = DotFileUtils
 										.resolve(((FileEditorInput) editor
 												.getEditorInput()).getFile()
-												.getLocationURI().toURL()));
+												.getLocationURI().toURL());
+								if (!resolvedFile.equals(view.currentFile)) {
+									view.updateGraph(resolvedFile);
+								}
 							} catch (MalformedURLException e) {
 								e.printStackTrace();
 							}
@@ -432,7 +436,8 @@ public class DotGraphView extends ZestFxUiView {
 				@Override
 				public boolean visit(final IResourceDelta delta) {
 					IResource resource = delta.getResource();
-					if (resource.getType() == IResource.FILE) {
+					if (resource.getType() == IResource.FILE
+							&& ((IFile) resource).getName().endsWith(EXTENSION)) {
 						try {
 							final IFile f = (IFile) resource;
 							IWorkspaceRunnable workspaceRunnable = view