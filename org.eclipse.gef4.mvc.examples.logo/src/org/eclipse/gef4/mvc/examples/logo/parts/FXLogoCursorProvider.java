ac6a5f3c6427e82819b0f0eb54410cd11d32662a
anyssen <alexander.nyssen@itemis.de>
2015-04-12 13:34:24 +0200

    [NONE] Cleanup logo example.
    Remove unused rotate handle part. Changed rotate cursor image.

--- org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/parts/FXLogoCursorProvider.java
+++ org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/parts/FXLogoCursorProvider.java
@@ -14,21 +14,23 @@ package org.eclipse.gef4.mvc.examples.logo.parts;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.inject.Provider;
+
 import javafx.scene.Cursor;
 import javafx.scene.ImageCursor;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-
-import com.google.inject.Provider;
 
 public class FXLogoCursorProvider implements Provider<Map<KeyCode, Cursor>> {
 
 	@Override
 	public Map<KeyCode, Cursor> get() {
 		HashMap<KeyCode, Cursor> key2cursor = new HashMap<KeyCode, Cursor>();
-		key2cursor.put(KeyCode.CONTROL, new ImageCursor(new Image(
-				FXLogoCursorProvider.class.getResource("/undo_edit.gif")
-						.toExternalForm())));
+		key2cursor
+				.put(KeyCode.CONTROL,
+						new ImageCursor(new Image(FXLogoCursorProvider.class
+								.getResource("/rotate_obj.gif")
+								.toExternalForm())));
 		return key2cursor;
 	}
 
