f3a6907c4591efbfd03c460dddfa7f03289ffde5
anyssen <alexander.nyssen@itemis.de>
2015-05-15 20:07:51 +0200

    [457897] Created new o.e.gef4.mvc.examples.logo.web bundle.
    - Renamed the existing o.e.gef4.mvc.examples.logo.web repository into
    o.e.gef4.mvc.examples.logo.web.repository.
    - Created a new o.e.gef4.mvc.examples.logo.web bundle, whose MANIFEST.MF
    contains a Permissions attribute. Up to now the example is the same as
    the standalone logo example. Via this own bundle, we can later augment
    it to demonstrate some web integration aspects).

---
+++ org.eclipse.gef4.mvc.examples.logo.web/src/org/eclipse/gef4/mvc/examples/logo/web/MvcLogoWebExample.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.gef4.mvc.examples.logo.web;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.gef4.mvc.examples.AbstractMvcExample;
+import org.eclipse.gef4.mvc.examples.logo.MvcLogoExampleModule;
+import org.eclipse.gef4.mvc.examples.logo.model.FXGeometricModel;
+
+import com.google.inject.Module;
+
+import javafx.application.Application;
+
+public class MvcLogoWebExample extends AbstractMvcExample {
+
+	public static List<FXGeometricModel> createDefaultContents() {
+		return Collections.singletonList(new FXGeometricModel());
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	public MvcLogoWebExample() {
+		super("GEF4 MVC Logo Example (Web)");
+	}
+
+	@Override
+	protected List<? extends Object> createContents() {
+		return createDefaultContents();
+	}
+
+	@Override
+	protected Module createModule() {
+		return new MvcLogoExampleModule();
+	}
+}
