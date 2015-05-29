4a10b7a6e53c0bb4ebb0224d16af08f958d7d5b4
mwienand <matthias.wienand@itemis.de>
2015-05-29 18:42:03 +0200

    [466800] Added the missing package-info.java files to Cloudio.

---
+++ org.eclipse.gef4.cloudio.ui/src/org/eclipse/gef4/cloudio/ui/package-info.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial text
+ *
+ *******************************************************************************/
+/**
+ * This package provides a {@link org.eclipse.swt.widgets.Canvas} dedicated to visualizing
+ * word clouds ({@link org.eclipse.gef4.cloudio.ui.TagCloud}), as well as a related
+ * {@link org.eclipse.jface.viewers.ContentViewer} ({@link org.eclipse.gef4.cloudio.ui.TagCloudViewer}).
+ * <p>
+ * It also provides a {@link java.awt.Composite} ({@link org.eclipse.gef4.cloudio.ui.CloudOptionsComposite})
+ * which combines a {@link org.eclipse.gef4.cloudio.ui.TagCloud} with controls to adjust the layout
+ * options for rendering the tag cloud.
+ */
+package org.eclipse.gef4.cloudio.ui;
