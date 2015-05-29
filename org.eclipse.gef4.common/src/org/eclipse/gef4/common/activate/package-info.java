fe31875ffdeae065f74f28ff61f324525ba1258c
mwienand <matthias.wienand@itemis.de>
2015-05-29 11:57:12 +0200

    [468770] Replace existing package.html files with corresponding package-info.java files.

---
+++ org.eclipse.gef4.common/src/org/eclipse/gef4/common/activate/package-info.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial text
+ *
+ *******************************************************************************/
+/**
+ * This package provides an abstraction for {@link org.eclipse.gef4.common.activate.IActivatable} objects.
+ */
+package org.eclipse.gef4.common.activate;
---
+++ org.eclipse.gef4.common/src/org/eclipse/gef4/common/notify/package-info.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial text
+ *
+ *******************************************************************************/
+/**
+ * This package provides abstractions and implementations of observable collections.
+ */
+package org.eclipse.gef4.common.notify;
