17cd41b33cfe4d982ef249908f45943ef3b45383
mwienand <matthias.wienand@itemis.de>
2015-05-29 11:50:17 +0200

    [468770] Add missing copyright headers to all package-info.java files.

--- org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/anchors/package-info.java
+++ org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/anchors/package-info.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial text
+ *
+ *******************************************************************************/
 /**
  * This package provides a visual anchor abstraction ({@link org.eclipse.gef4.fx.anchors.IFXAnchor}),
  * a related abstract base implementation ({@link org.eclipse.gef4.fx.anchors.AbstractFXAnchor}),
--- org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/nodes/package-info.java
+++ org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/nodes/package-info.java
@@ -1,5 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial text
+ *
+ *******************************************************************************/
 /**
- * The nodes package provides:
+ * This package provides:
  * <ul>
  * <li>an adaptation of an {@link org.eclipse.gef4.geometry.planar.IGeometry} to {@link javafx.scene.Node}: {@link org.eclipse.gef4.fx.nodes.FXGeometryNode}</li>
  * <li>a connection abstraction that is based on {@link org.eclipse.gef4.fx.anchors.IFXAnchor}: {@link org.eclipse.gef4.fx.nodes.FXConnection}</li>
