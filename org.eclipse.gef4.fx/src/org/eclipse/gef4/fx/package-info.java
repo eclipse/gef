39ee301dc75eab630576a69a57c73b3a64f3372b
anyssen <alexander.nyssen@itemis.de>
2015-06-08 08:57:11 +0200

    [466467] Revise package-info for o.e.gef4.fx package

--- org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/package-info.java
+++ org.eclipse.gef4.fx/src/org/eclipse/gef4/fx/package-info.java
@@ -1,18 +1,16 @@
 /*******************************************************************************
  * Copyright (c) 2015 itemis AG and others.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Matthias Wienand (itemis AG) - initial text
+ * Contributors: Matthias Wienand (itemis AG) - initial text
  *
  *******************************************************************************/
 /**
- * This is the root package of the GEF4 FX component. The GEF4 FX component provides
- * useful additions around JavaFX. It is internally composed out of two modules,
- * namely FX and FX.UI. This package is also the root package of the FX module.
+ * This package provides the bundle activator (
+ * {@link org.eclipse.gef4.fx.FxBundle}).
  */
 package org.eclipse.gef4.fx;
