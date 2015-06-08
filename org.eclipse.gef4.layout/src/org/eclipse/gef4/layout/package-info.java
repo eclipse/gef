7a02093053cc1f20d5905fa2ebcb8218155d8546
anyssen <alexander.nyssen@itemis.de>
2015-06-08 08:59:37 +0200

    [466802] Revise package-info text of o.e.g4.layout package.

--- org.eclipse.gef4.layout/src/org/eclipse/gef4/layout/package-info.java
+++ org.eclipse.gef4.layout/src/org/eclipse/gef4/layout/package-info.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  * Copyright (c) 2015 itemis AG and others.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Matthias Wienand (itemis AG) - initial text
+ * Contributors: Matthias Wienand (itemis AG) - initial text
  *
  *******************************************************************************/
 /**
- * This is the root package of the GEF4 Layout component which provides an interface-based
- * facade to exchange layout information with layout algorithms (within this package),
- * concrete implementations of layout algorithms ({@link org.eclipse.gef4.layout.algorithms}), as well as
- * listeners to hook into the layout computation ({@link org.eclipse.gef4.layout.listeners}).
+ * This package provides an interface-based facade to exchange layout
+ * information with layout algorithms (within this package), concrete
+ * implementations of layout algorithms (
+ * {@link org.eclipse.gef4.layout.algorithms}), as well as listeners to hook
+ * into the layout computation ({@link org.eclipse.gef4.layout.listeners}).
  */
 package org.eclipse.gef4.layout;
