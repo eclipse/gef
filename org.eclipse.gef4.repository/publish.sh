d181c4b8be978d57860cc2fcafda79363675e59e
anyssen <alexander.nyssen@itemis.de>
2015-07-08 19:50:01 +0200

    [471151] Add echo statement, adjust formatting.

--- org.eclipse.gef4.repository/publish.sh
+++ org.eclipse.gef4.repository/publish.sh
@@ -7,41 +7,41 @@ jobName="gef4-master"
 # $2: Build type: i(ntegration), m(ilestone), r(elease)
 # $3: Whether to merge the site with an existing one: (y)es, (n)o
 # $4: The release label used to label the (nested) update site, e.g. 3.10.0 or 3.10.1M4
-# 
+ 
 if [ $# -eq 4 ];
-        then
-                buildId=$1
-                buildType=$2
-                merge=$3
-               	releaseLabel=$4
-        else
-                if [ $# -ne 0 ];
-                then
-                        exit 1
-                fi
+then
+	buildId=$1
+	buildType=$2
+	merge=$3
+	releaseLabel=$4
+else
+    if [ $# -ne 0 ];
+	then
+        exit 1
+    fi
 fi
 
 if [ -z "$buildId" ];
 then
-        for i in $( find ~/.hudson/jobs/$jobName/builds -type l | sed 's!.*/!!' | sort)
-        do
-                echo -n "$i, "
-        done
-        echo "lastStable, lastSuccessful"
-        echo -n "Please enter the id/label of the Hudson job you want to promote:"
-        read buildId
+    for i in $( find ~/.hudson/jobs/$jobName/builds -type l | sed 's!.*/!!' | sort)
+    do
+        echo -n "$i, "
+    done
+    echo "lastStable, lastSuccessful"
+    echo -n "Please enter the id/label of the Hudson job you want to promote:"
+    read buildId
 fi
 if [ -z "$buildId" ];
-        then
-                exit 0
+    then
+    exit 0
 fi
 
 # Determine the local update site we want to publish to
 if [ "$buildId" = "lastStable" -o "$buildId" = "lastSuccessful" ];
-        then
-                jobDir=$(readlink -f ~/.hudson/jobs/$jobName/$buildId)
-        else
-                jobDir=$(readlink -f ~/.hudson/jobs/$jobName/builds/$buildId)
+    then
+    jobDir=$(readlink -f ~/.hudson/jobs/$jobName/$buildId)
+else
+    jobDir=$(readlink -f ~/.hudson/jobs/$jobName/builds/$buildId)
 fi
 localUpdateSite=$jobDir/archive/update-site
 echo "Using local update-site: $localUpdateSite"
@@ -49,53 +49,53 @@ echo "Using local update-site: $localUpdateSite"
 # Reverse lookup the build id (in case lastSuccessful or lastStable was used)
 for i in $(find ~/.hudson/jobs/$jobName/builds/ -type l)
 do
-        if [ "$(readlink -f $i)" =  "$jobDir" ];
-                then
-                        buildId=${i##*/}
-        fi
+    if [ "$(readlink -f $i)" =  "$jobDir" ];
+    then
+        buildId=${i##*/}
+    fi
 done
 echo "Reverse lookup yielded build id: $buildId"
 
 # Select the build type
 if [ -z "$buildType" ];
 then
-        echo -n "Please select which type of build you want to publish to [i(integration), m(ilestone), r(elease)]: "
-        read buildType
+    echo -n "Please select which type of build you want to publish to [i(integration), m(ilestone), r(elease)]: "
+    read buildType
 fi
 echo "Publishing as $buildType build"
 
 # Determine remote update site we want to promote to
 case $buildType in
-        i|I) remoteSite=integration ;;
-        m|M) remoteSite=milestones ;;
-        r|R) remoteSite=releases ;;
-        *) exit 0 ;;
+    i|I) remoteSite=integration ;;
+    m|M) remoteSite=milestones ;;
+    r|R) remoteSite=releases ;;
+    *) exit 0 ;;
 esac
 remoteUpdateSiteBase="tools/gef/gef4/updates/$remoteSite"
 remoteUpdateSite="/home/data/httpd/download.eclipse.org/$remoteUpdateSiteBase"
 echo "Publishing to remote update-site: $remoteUpdateSite"
 
 if [ -d "$remoteUpdateSite" ];
-        then
-                if [ -z "$merge" ];
-                then
-                        echo -n "Do you want to merge with the existing update-site? [(y)es, (n)o]:"
-                        read merge
-                fi
-                if [ "$merge" != y -a "$merge" != n ];
-                        then
-                        exit 0
-                fi
-        else
-                merge=n
+then
+    if [ -z "$merge" ];
+    then
+        echo -n "Do you want to merge with the existing update-site? [(y)es, (n)o]:"
+        read merge
+    fi
+    if [ "$merge" != y -a "$merge" != n ];
+    then
+        exit 0
+    fi
+else
+    merge=n
 fi
 echo "Merging with existing site: $merge"
 
 # Determine releaseLabel
 if [ -z "$releaseLabel" ];
-        then
-                echo -n "Please enter release label (e.g. 3.10.0, 3.10.1M2):"
-                read releaseLabel
+then
+    echo -n "Please enter release label (e.g. 3.10.0, 3.10.1M2):"
+    read releaseLabel
 fi
 
 # Prepare a temp directory
@@ -112,9 +112,9 @@ cd eclipse
 chmod 700 eclipse
 cd ..
 if [ ! -d "eclipse" ];
-        then
-                echo "Failed to download an Eclipse SDK, being needed for provisioning."
-                exit
+then
+    echo "Failed to download an Eclipse SDK, being needed for provisioning."
+    exit
 fi
 # Prepare Eclipse SDK to provide WTP releng tools (used to postprocess repository, i.e set p2.mirrorsURL property)
 echo "Installing WTP Releng tools"
@@ -126,16 +126,16 @@ rm eclipse-SDK-4.4.2-linux-gtk-x86_64.tar.gz
 updateSiteLabel=${releaseLabel}_${jobName}_${buildId}
 # Prepare composite local update site (transfer into composite if needed)
 if [ "$merge" = y ];
+then
+    # check if the remote site is a composite update site
+    echo "Merging existing site into local one."
+    if [ -e "$remoteUpdateSite/compositeArtifacts.xml" ];
     then
-        # check if the remote site is a composite update site
-        echo "Merging existing site into local one."
-        if [ -e "$remoteUpdateSite/compositeArtifacts.xml" ];
-    	    then
-    	    cp -R $remoteUpdateSite/* update-site/
-    	else
-    		mkdir -p update-site/pre_${updateSiteLabel}
-    		cp -R $remoteUpdateSite/* update-site/pre_${updateSiteLabel}/
-    	fi
+	    cp -R $remoteUpdateSite/* update-site/
+	else
+		mkdir -p update-site/pre_${updateSiteLabel}
+		cp -R $remoteUpdateSite/* update-site/pre_${updateSiteLabel}/
+	fi
 else 
    	echo "Skipping merge operation."    
 fi
@@ -192,28 +192,29 @@ echo $artifact > compositeArtifacts.xml
 cd ..
 
 # Ensure p2.mirrorURLs property is used in update site
+echo "Updating p2.mirrorURLs property."
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.wtp.releng.tools.addRepoProperties -vmargs -DartifactRepoDirectory=$PWD/update-site/${updateSiteLabel} -Dp2MirrorsURL="http://www.eclipse.org/downloads/download.php?format=xml&file=/$remoteUpdateSiteBase/${updateSiteLabel}"
 
 # Create p2.index file
 if [ ! -e "update-site/p2.index" ];
-    then
-        echo "Creating p2.index file"
-        echo "version = 1" > update-site/p2.index
-        echo "metadata.repository.factory.order=compositeContent.xml,\!" >> update-site/p2.index
-        echo "artifact.repository.factory.order=compositeArtifacts.xml,\!" >> update-site/p2.index
+then
+    echo "Creating p2.index file."
+    echo "version = 1" > update-site/p2.index
+    echo "metadata.repository.factory.order=compositeContent.xml,\!" >> update-site/p2.index
+    echo "artifact.repository.factory.order=compositeArtifacts.xml,\!" >> update-site/p2.index
 fi
 
 # Backup then clean remote update site
 echo "Creating backup of remote update site."
 if [ -d "$remoteUpdateSite" ];
-        then
-                if [ -d BACKUP ];
-                        then
-                                rm -fr BACKUP
-                fi
-                mkdir BACKUP
-                cp -R $remoteUpdateSite/* BACKUP/
-                rm -fr $remoteUpdateSite
+then
+    if [ -d BACKUP ];
+	then
+        rm -fr BACKUP
+    fi
+    mkdir BACKUP
+    cp -R $remoteUpdateSite/* BACKUP/
+    rm -fr $remoteUpdateSite
 fi
 
 echo "Publishing contents of local update-site directory to remote update site $remoteUpdateSite"
