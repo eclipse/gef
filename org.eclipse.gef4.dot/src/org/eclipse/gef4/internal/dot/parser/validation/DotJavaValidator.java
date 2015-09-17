208938386496af9496ced56692021febe32ee99d
anyssen <alexander.nyssen@itemis.de>
2015-09-17 22:45:39 +0200

    [451098] Implement validation to check edgeOp matches graph type.
    - Implemented validation rule with severity ERROR to check that EdgeOp
    is specified according to GraphType.
    - Adjusted version of o.e.gef4.dot to 0.3.0.

--- org.eclipse.gef4.dot/src/org/eclipse/gef4/internal/dot/parser/validation/DotJavaValidator.java
+++ org.eclipse.gef4.dot/src/org/eclipse/gef4/internal/dot/parser/validation/DotJavaValidator.java
@@ -11,13 +11,70 @@
 
 package org.eclipse.gef4.internal.dot.parser.validation;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef4.internal.dot.parser.dot.DotGraph;
+import org.eclipse.gef4.internal.dot.parser.dot.DotPackage;
+import org.eclipse.gef4.internal.dot.parser.dot.EdgeOp;
+import org.eclipse.gef4.internal.dot.parser.dot.EdgeRhsNode;
+import org.eclipse.gef4.internal.dot.parser.dot.EdgeRhsSubgraph;
+import org.eclipse.gef4.internal.dot.parser.dot.GraphType;
+import org.eclipse.xtext.validation.Check;
+
+/**
+ * Provides DOT-specific validation rules.
+ * 
+ * @author anyssen
+ *
+ */
 public class DotJavaValidator extends AbstractDotJavaValidator {
 
-	// @Check
-	// public void checkGreetingStartsWithCapital(Greeting greeting) {
-	// if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-	// warning("Name should start with a capital", MyDslPackage.GREETING__NAME);
-	// }
-	// }
+	/**
+	 * Ensures that within {@link EdgeRhsNode}, '->' is used in directed graphs,
+	 * while '--' is used in undirected graphs.
+	 * 
+	 * @param edgeRhsNode
+	 *            The EdgeRhsNode to validate.
+	 */
+	@Check
+	public void checkEdgeOpCorrespondsToGraphType(EdgeRhsNode edgeRhsNode) {
+		checkEdgeOpCorrespondsToGraphType(edgeRhsNode.getOp(),
+				getDotGraph(edgeRhsNode).getType());
+	}
+
+	/**
+	 * Ensures that within {@link EdgeRhsSubgraph} '->' is used in directed
+	 * graphs, while '--' is used in undirected graphs.
+	 * 
+	 * @param edgeRhsSubgraph
+	 *            The EdgeRhsSubgraph to validate.
+	 */
+	@Check
+	public void checkEdgeOpCorrespondsToGraphType(
+			EdgeRhsSubgraph edgeRhsSubgraph) {
+		checkEdgeOpCorrespondsToGraphType(edgeRhsSubgraph.getOp(),
+				getDotGraph(edgeRhsSubgraph).getType());
+	}
+
+	private void checkEdgeOpCorrespondsToGraphType(EdgeOp edgeOp,
+			GraphType graphType) {
+		boolean edgeDirected = edgeOp.equals(EdgeOp.DIRECTED);
+		boolean graphDirected = graphType.equals(GraphType.DIGRAPH);
+		if (graphDirected && !edgeDirected) {
+			error("EdgeOp '--' may only be used in undirected graphs.",
+					DotPackage.eINSTANCE.getEdgeRhs_Op());
+
+		} else if (!graphDirected && edgeDirected) {
+			error("EdgeOp '->' may only be used in directed graphs.",
+					DotPackage.eINSTANCE.getEdgeRhs_Op());
+		}
+	}
+
+	private DotGraph getDotGraph(EObject eObject) {
+		EObject container = eObject.eContainer();
+		while (container != null && !(container instanceof DotGraph)) {
+			container = container.eContainer();
+		}
+		return (DotGraph) container;
+	}
 
 }
