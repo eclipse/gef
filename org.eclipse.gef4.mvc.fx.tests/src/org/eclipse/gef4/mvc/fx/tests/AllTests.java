4e348bdacf7a0f958bfa844ac385897ee94fba1a
mwienand <matthias.wienand@itemis.de>
2015-09-17 13:04:49 +0200

    [477352] Add tests for FXTransformPolicy.

--- org.eclipse.gef4.mvc.fx.tests/src/org/eclipse/gef4/mvc/fx/tests/AllTests.java
+++ org.eclipse.gef4.mvc.fx.tests/src/org/eclipse/gef4/mvc/fx/tests/AllTests.java
@@ -17,7 +17,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AbstractFXHandlePartTests.class, FXClickDragToolTests.class, FXTypeToolTests.class })
+@SuiteClasses({ AbstractFXHandlePartTests.class, FXClickDragToolTests.class, FXTypeToolTests.class,
+		FXTransformPolicyTests.class })
 public class AllTests {
 
 }
