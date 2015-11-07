19a5ebab3ae6d8daa3ed0119568bc5737fc59a4c
anyssen <alexander.nyssen@itemis.de>
2015-11-07 15:06:47 +0100

    [457147] Rename test bundles and packages to follow naming conventions.
    - Renamed o.e.g4.cloudio.tests to o.e.g4.cloudio.tests.ui
    - Renamed o.e.g4.geometry.convert.swt.tests to
    o.e.g4.geometry.tests.convert.swt
    - Renamed o.e.g4.geometry.convert.fx.tests to
    o.e.g4.geometry.tests.convert.fx
    - Renamed o.e.g4.mvc.fx.tests to o.e.g4.mvc.tests.fx
    - Renamed o.e.g4.zest.fx.tests to o.e.g4.zest.tests.fx
    - Renamed o.e.g4.zest.fx.jface.tests to o.e.g4.zest.tests.fx.jface

---
+++ org.eclipse.gef4.geometry.tests.convert.fx/src/org/eclipse/gef4/geometry/tests/convert/fx/FXConversionTests.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gef4.geometry.tests.convert.fx;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.gef4.geometry.convert.fx.Geometry2JavaFX;
+import org.eclipse.gef4.geometry.convert.fx.JavaFX2Geometry;
+import org.eclipse.gef4.geometry.planar.Path;
+import org.junit.Test;
+
+public class FXConversionTests {
+
+	@Test
+	public void test_PathConversion() {
+		// create a simple path and convert it from geometry to javafx and back
+		// again!
+		Path p = new Path().moveTo(50, 50).lineTo(100, 100).quadTo(100, 150, 50, 150).cubicTo(20, 120, 20, 80, 50, 50)
+				.close();
+		assertEquals(p, JavaFX2Geometry.toPath(Geometry2JavaFX.toPath(p)));
+	}
+
+}
---
+++ org.eclipse.gef4.mvc.tests.fx/src/org/eclipse/gef4/mvc/tests/fx/FxApplicationThreadRule.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.gef4.mvc.tests.fx;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.swing.SwingUtilities;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+
+/**
+ * A {@link TestRule} to ensure tests are executed on the JavaFX Application
+ * Thread using {@link Platform#runLater(Runnable)}, ensuring that the JavaFX
+ * Toolkit is properly initialized before execution.
+ *
+ * @author anyssen
+ *
+ */
+public class FxApplicationThreadRule implements TestRule {
+
+	private static boolean initializedJavaFxToolkit = false;
+
+	@Override
+	public Statement apply(final Statement base, Description description) {
+		return new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				if (!initializedJavaFxToolkit) {
+					final CountDownLatch latch = new CountDownLatch(1);
+					SwingUtilities.invokeLater(new Runnable() {
+						@Override
+						public void run() {
+							new JFXPanel(); // initializes JavaFX environment
+							initializedJavaFxToolkit = true;
+							latch.countDown();
+						}
+					});
+					latch.await();
+				}
+
+				final CountDownLatch countDownLatch = new CountDownLatch(1);
+				final AtomicReference<Throwable> throwableRef = new AtomicReference<>();
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							base.evaluate();
+						} catch (Throwable throwable) {
+							throwableRef.set(throwable);
+						}
+						countDownLatch.countDown();
+					}
+				});
+				countDownLatch.await();
+				Throwable thrown = throwableRef.get();
+				if (thrown != null) {
+					throw thrown;
+				}
+			}
+		};
+	}
+}
