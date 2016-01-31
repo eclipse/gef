ac840297cf6ae4f6dd56047637cd772526a5d21a
anyssen <alexander.nyssen@itemis.de>
2016-01-31 13:34:53 +0100

    [458439] Correct fix for missing conversion of image cursor.
    - Fixed that all cursors set on FXCanvas were ignored by FXCanvasEx.
    - Ensured image cursor is preserved by creating a respective platform
    cursor for it.

--- org.eclipse.gef4.fx.swt/src/org/eclipse/gef4/fx/swt/canvas/FXCanvasEx.java
+++ org.eclipse.gef4.fx.swt/src/org/eclipse/gef4/fx/swt/canvas/FXCanvasEx.java
@@ -14,10 +14,16 @@
  *******************************************************************************/
 package org.eclipse.gef4.fx.swt.canvas;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Method;
+
+import org.eclipse.gef4.common.reflect.ReflectionUtils;
+import org.eclipse.gef4.fx.swt.gestures.SwtToFxEventConverter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+import com.sun.javafx.cursor.CursorFrame;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -28,28 +34,14 @@ import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.stage.Window;
 
-import org.eclipse.gef4.common.reflect.ReflectionUtils;
-import org.eclipse.gef4.fx.swt.gestures.SwtToFxEventConverter;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.events.FocusListener;
-import org.eclipse.swt.events.GestureEvent;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.widgets.Canvas;
-import org.eclipse.swt.widgets.Composite;
-
 /**
- * A replacement of {@link FXCanvas} that offers the following additional
- * capabilities:
+ * A replacement of {@link FXCanvas} that fixes the following issues:
  * <ul>
- * <li>Forwarding of SWT {@link GestureEvent}s to JavaFX (the original FXCanvas
- * simply ignores all those events)</li>
- * <li>Support for notifying SWT {@link FocusListener}s (the original FXCanvas
- * will forward all focus events to the embedded JavaFX stage), so SWT
- * {@link FocusListener} will not be notified</li>
- * <li>Support for setting cursors via JavaFX (i.e. the cursor of the embedded
- * JavaFX stage, its transferred into an SWT cursor on this {@link FXCanvasEx})
- * </li>
+ * <li>https://bugs.openjdk.java.net/browse/JDK-8143596 (gesture events not
+ * forwarded) and horizontal mouse events not forwarded: fixed by forwarding of
+ * missing SWT events to JavaFX through an {@link SwtToFxEventConverter}.</li>
+ * <li>https://bugs.openjdk.java.net/browse/JDK-8088147 (image cursors not
+ * supported): fixed by adding support for image cursors.</li>
  * </ul>
  *
  * @author anyssen
@@ -57,39 +49,16 @@ import org.eclipse.swt.widgets.Composite;
  */
 public class FXCanvasEx extends FXCanvas {
 
-	private static Map<Cursor, Integer> CURSOR_FROM_FX_TO_SWT = new HashMap<Cursor, Integer>() {
-		private static final long serialVersionUID = 1L;
-
-		{
-			// TODO: verify cursors marked with XXX
-			put(Cursor.CLOSED_HAND, SWT.CURSOR_HAND); // XXX
-			put(Cursor.CROSSHAIR, SWT.CURSOR_CROSS);
-			put(Cursor.DISAPPEAR, SWT.CURSOR_NO); // XXX
-			put(Cursor.E_RESIZE, SWT.CURSOR_SIZEE);
-			put(Cursor.H_RESIZE, SWT.CURSOR_SIZEWE);
-			put(Cursor.HAND, SWT.CURSOR_HAND);
-			put(Cursor.MOVE, SWT.CURSOR_CROSS); // XXX
-			put(Cursor.N_RESIZE, SWT.CURSOR_SIZEN);
-			put(Cursor.NE_RESIZE, SWT.CURSOR_SIZENE);
-			put(Cursor.NONE, SWT.CURSOR_NO);
-			put(Cursor.NW_RESIZE, SWT.CURSOR_SIZENW);
-			put(Cursor.OPEN_HAND, SWT.CURSOR_HAND);// XXX
-			put(Cursor.S_RESIZE, SWT.CURSOR_SIZES);
-			put(Cursor.SE_RESIZE, SWT.CURSOR_SIZESE);
-			put(Cursor.SW_RESIZE, SWT.CURSOR_SIZESW);
-			put(Cursor.TEXT, SWT.CURSOR_IBEAM);
-			put(Cursor.V_RESIZE, SWT.CURSOR_SIZENS);
-			put(Cursor.W_RESIZE, SWT.CURSOR_SIZEW);
-			put(Cursor.WAIT, SWT.CURSOR_WAIT);
-		}
-	};
-
 	private SwtToFxEventConverter gestureConverter;
-	private List<FocusListener> focusListeners;
+
 	private ChangeListener<Cursor> cursorChangeListener = new ChangeListener<Cursor>() {
+
 		@Override
 		public void changed(ObservableValue<? extends Cursor> observable,
 				Cursor oldCursor, Cursor newCursor) {
+			// XXX: SWTCursors does support image cursors yet
+			// (https://bugs.openjdk.java.net/browse/JDK-8088147); we compensate
+			// this here
 			if (newCursor instanceof ImageCursor) {
 				// custom cursor, convert image
 				ImageData imageData = SWTFXUtils.fromFXImage(
@@ -99,14 +68,21 @@ public class FXCanvasEx extends FXCanvas {
 				org.eclipse.swt.graphics.Cursor swtCursor = new org.eclipse.swt.graphics.Cursor(
 						getDisplay(), imageData, (int) hotspotX,
 						(int) hotspotY);
-				getShell().setCursor(swtCursor);
-			} else if (CURSOR_FROM_FX_TO_SWT.containsKey(newCursor)) {
-				// standard cursor, look up in map
-				getShell().setCursor(new org.eclipse.swt.graphics.Cursor(
-						getDisplay(), CURSOR_FROM_FX_TO_SWT.get(newCursor)));
-			} else {
-				// unknown cursor, use default
-				getShell().setCursor(null);
+				// XXX: Set platform cursor on CursorFrame so that it can be
+				// retrieved by FXCanvas' HostContainer (which ultimately sets
+				// the cursor on the FXCanvas)
+				try {
+					Method currentCursorFrameAccessor = Cursor.class
+							.getDeclaredMethod("getCurrentFrame",
+									new Class[] {});
+					currentCursorFrameAccessor.setAccessible(true);
+					CursorFrame currentCursorFrame = (CursorFrame) currentCursorFrameAccessor
+							.invoke(newCursor, new Object[] {});
+					currentCursorFrame.setPlatforCursor(
+							org.eclipse.swt.graphics.Cursor.class, swtCursor);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	};
@@ -126,45 +102,11 @@ public class FXCanvasEx extends FXCanvas {
 	public FXCanvasEx(Composite parent, int style) {
 		super(parent, style);
 		gestureConverter = new SwtToFxEventConverter(this);
-
-		// add a focus listener to propagate focus events also to FocusListeners
-		// registered on this canvas. FXCanvas will not notify them, as all
-		// focus events are only forwarded to the embedded stage.
-		addFocusListener(new FocusListener() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				for (FocusListener l : focusListeners) {
-					l.focusGained(e);
-				}
-			}
-
-			@Override
-			public void focusLost(FocusEvent e) {
-				for (FocusListener l : focusListeners) {
-					l.focusLost(e);
-				}
-			}
-		});
-		focusListeners = new ArrayList<>();
-	}
-
-	@Override
-	public void addFocusListener(FocusListener listener) {
-		if (focusListeners == null) {
-			// if we have not created our local field yet, we are within
-			// initialization, where focus listeners have to be registered via
-			// the super implementation.
-			super.addFocusListener(listener);
-		} else {
-			focusListeners.add(listener);
-		}
 	}
 
 	@Override
 	public void dispose() {
 		gestureConverter.dispose();
-		focusListeners.clear();
-		focusListeners = null;
 		super.dispose();
 	}
 
@@ -178,20 +120,6 @@ public class FXCanvasEx extends FXCanvas {
 	}
 
 	@Override
-	public void removeFocusListener(FocusListener listener) {
-		if (focusListeners == null) {
-			super.removeFocusListener(listener);
-		} else {
-			focusListeners.remove(listener);
-		}
-	}
-
-	@Override
-	public void setCursor(org.eclipse.swt.graphics.Cursor cursor) {
-		// Do not overwrite the JavaFX cursor.
-	}
-
-	@Override
 	public void setScene(Scene newScene) {
 		Scene oldScene = getScene();
 		if (oldScene != null) {
@@ -202,5 +130,4 @@ public class FXCanvasEx extends FXCanvas {
 			newScene.cursorProperty().addListener(cursorChangeListener);
 		}
 	}
-
 }
