97cd9c11ec08fea1e306e54b9b5168018cb51339
mwienand <matthias.wienand@itemis.de>
2016-02-05 10:29:50 +0100

    [NONE] Fix copyright header.

--- org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/parts/FXGeometricModelPart.java
+++ org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/parts/FXGeometricModelPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 itemis AG and others.
+ * Copyright (c) 2014, 2016 itemis AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *     Matthias Wienand (itemis AG) - contributions for Bugzillas #450285 & #487070
  *
  *******************************************************************************/
 package org.eclipse.gef4.mvc.examples.logo.parts;
