28cf9837d778403938913ad2bd602803f2adf922
mwienand <matthias.wienand@itemis.de>
2016-02-05 16:17:06 +0100

    [NONE] Re-enable layouting in DotGraphView.

--- org.eclipse.gef4.dot.ui/src/org/eclipse/gef4/dot/internal/ui/DotGraphViewModule.java
+++ org.eclipse.gef4.dot.ui/src/org/eclipse/gef4/dot/internal/ui/DotGraphViewModule.java
@@ -14,7 +14,6 @@ package org.eclipse.gef4.dot.internal.ui;
 
 import org.eclipse.gef4.common.adapt.AdapterKey;
 import org.eclipse.gef4.zest.fx.ZestFxModule;
-import org.eclipse.gef4.zest.fx.behaviors.LayoutContextBehavior;
 
 import com.google.inject.multibindings.MapBinder;
 
@@ -27,45 +26,47 @@ import com.google.inject.multibindings.MapBinder;
  */
 public class DotGraphViewModule extends ZestFxModule {
 
-	// TODO: Guard calls to LayoutContextBehaivor and trigger layout
-	// computations from within LayoutContextBehavior so that a replacement is
-	// not needed, but the binding can just be removed.
-	public static final class NullLayoutContextBehavior
-			extends LayoutContextBehavior {
-		@Override
-		protected void doActivate() {
-			// super.doActivate();
-		}
-
-		@Override
-		protected void doDeactivate() {
-			// super.doDeactivate();
-		}
-
-		@Override
-		public void applyLayout(boolean clean) {
-			// super.applyLayout(clean);
-		}
-	}
+	// // TODO: Guard calls to LayoutContextBehaivor and trigger layout
+	// // computations from within LayoutContextBehavior so that a replacement
+	// is
+	// // not needed, but the binding can just be removed.
+	// public static final class NullLayoutContextBehavior
+	// extends LayoutContextBehavior {
+	// @Override
+	// protected void doActivate() {
+	// // super.doActivate();
+	// }
+	//
+	// @Override
+	// protected void doDeactivate() {
+	// // super.doDeactivate();
+	// }
+	//
+	// @Override
+	// public void applyLayout(boolean clean) {
+	// // super.applyLayout(clean);
+	// }
+	// }
 
 	@Override
 	protected void bindNodeLayoutBehaviorAsNodeContentPartAdapter(
 			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
-		// super.bindNodeLayoutBehaviorAsNodeContentPartAdapter(adapterMapBinder);
+		super.bindNodeLayoutBehaviorAsNodeContentPartAdapter(adapterMapBinder);
 	}
 
 	@Override
 	protected void bindLayoutContextBehaviorAsGraphContentPartAdapter(
 			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
-		// super.bindLayoutContextBehaviorAsGraphContentPartAdapter(adapterMapBinder);
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(NullLayoutContextBehavior.class);
+		super.bindLayoutContextBehaviorAsGraphContentPartAdapter(
+				adapterMapBinder);
+		// adapterMapBinder.addBinding(AdapterKey.defaultRole())
+		// .to(NullLayoutContextBehavior.class);
 	}
 
 	@Override
 	protected void bindEdgeLayoutBehaviorAsEdgeContentPartAdapter(
 			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
-		// super.bindEdgeLayoutBehaviorAsEdgeContentPartAdapter(adapterMapBinder);
+		super.bindEdgeLayoutBehaviorAsEdgeContentPartAdapter(adapterMapBinder);
 	}
 
 }
