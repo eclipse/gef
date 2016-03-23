21b9177d23c1d8326f6218dff637aa4dec963d87
anyssen <nyssen@itemis.de>
2016-03-23 20:19:23 +0100

    [NONE] Format copyright header.

--- org.eclipse.gef4.dot/src/org/eclipse/gef4/dot/internal/parser/formatting/DotFormatter.java
+++ org.eclipse.gef4.dot/src/org/eclipse/gef4/dot/internal/parser/formatting/DotFormatter.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2009, 2014 Fabian Steeg and others.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +8,7 @@
  *
  * Contributors:
  *     Fabian Steeg - initial API and implementation (see bug #277380)
+ *     
  *******************************************************************************/
 package org.eclipse.gef4.dot.internal.parser.formatting;
 
