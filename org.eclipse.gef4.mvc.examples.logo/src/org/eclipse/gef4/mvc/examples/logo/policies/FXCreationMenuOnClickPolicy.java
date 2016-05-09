58e13309af96e541942fe7bea97dbab12c04237a
anyssen <nyssen@itemis.de>
2016-05-09 15:19:34 +0200

    [493180] Ensure color schema for feedback/handles can be adjusted.
    - Adjusted FXCreationMenuOnClickPolicy with respect to changed color
    schema.

--- org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/policies/FXCreationMenuOnClickPolicy.java
+++ org.eclipse.gef4.mvc.examples.logo/src/org/eclipse/gef4/mvc/examples/logo/policies/FXCreationMenuOnClickPolicy.java
@@ -18,7 +18,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.gef4.common.adapt.AdapterKey;
 import org.eclipse.gef4.geometry.planar.AffineTransform;
 import org.eclipse.gef4.geometry.planar.Point;
-import org.eclipse.gef4.mvc.fx.parts.FXHoverFeedbackPart;
+import org.eclipse.gef4.mvc.fx.parts.FXDefaultHoverFeedbackPartFactory;
 import org.eclipse.gef4.mvc.fx.parts.FXRootPart;
 import org.eclipse.gef4.mvc.fx.policies.FXTransformPolicy;
 import org.eclipse.gef4.mvc.fx.policies.IFXOnClickPolicy;
@@ -31,7 +31,9 @@ import org.eclipse.gef4.mvc.policies.CreationPolicy;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.reflect.TypeToken;
+import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.name.Named;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -57,8 +59,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Affine;
 
 // TODO: only applicable for FXRootPart and FXViewer
-public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
-		implements IFXOnClickPolicy {
+public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node> implements IFXOnClickPolicy {
 
 	/**
 	 * The adapter role for the
@@ -84,8 +85,7 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 	/**
 	 * Set of points used for the left (smaller as, <code>&lt;</code>) arrow.
 	 */
-	private static final Double[] LEFT_ARROW_POINTS = new Double[] { 10d, 0d,
-			0d, 5d, 10d, 10d };
+	private static final Double[] LEFT_ARROW_POINTS = new Double[] { 10d, 0d, 0d, 5d, 10d, 10d };
 
 	/**
 	 * Radius of the drop shadow effects.
@@ -95,16 +95,9 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 	/**
 	 * Set of points used for the right (greater than, <code>&gt;</code>) arrow.
 	 */
-	private static final Double[] RIGHT_ARROW_POINTS = new Double[] { 0d, 0d,
-			10d, 5d, 0d, 10d };
+	private static final Double[] RIGHT_ARROW_POINTS = new Double[] { 0d, 0d, 10d, 5d, 0d, 10d };
 
-	/**
-	 * The {@link Color} used for highlighting visuals.
-	 */
-	private static final Color HIGHLIGHT_COLOR = FXHoverFeedbackPart.DEFAULT_STROKE;
-
-	private static Reflection createDropShadowReflectionEffect(
-			double effectRadius, Color color) {
+	private static Reflection createDropShadowReflectionEffect(double effectRadius, Color color) {
 		DropShadow dropShadow = new DropShadow(effectRadius, color);
 		Reflection reflection = new Reflection();
 		reflection.setInput(dropShadow);
@@ -117,6 +110,10 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 		}
 		return node == parent;
 	}
+
+	@Inject
+	@Named(FXDefaultHoverFeedbackPartFactory.HOVER_FEEDBACK_COLOR)
+	private Color hightlightColor;
 
 	/**
 	 * List of {@link IFXCreationMenuItem}s which can be constructed.
@@ -148,8 +145,7 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 	 * Stores the padding around visuals used to circumvent translation issues
 	 * when applying a drop shadow effect.
 	 */
-	private final double padding = DROP_SHADOW_RADIUS + 1
-			+ ARROW_STROKE_WIDTH * 2 + 1;
+	private final double padding = DROP_SHADOW_RADIUS + 1 + ARROW_STROKE_WIDTH * 2 + 1;
 
 	/**
 	 * The {@link HBox} in which all menu visuals are layed out.
@@ -171,8 +167,7 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 			}
 			EventTarget target = e.getTarget();
 			if (target instanceof Node) {
-				initialMousePositionInScene = new Point(e.getSceneX(),
-						e.getSceneY());
+				initialMousePositionInScene = new Point(e.getSceneX(), e.getSceneY());
 				openMenu(e);
 			}
 		} else if (MouseButton.PRIMARY.equals(e.getButton())) {
@@ -191,8 +186,7 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 
 	private void closeMenu() {
 		// remove menu visuals
-		getViewer().getCanvas().getScrolledOverlayGroup().getChildren()
-				.remove(hbox);
+		getViewer().getCanvas().getScrolledOverlayGroup().getChildren().remove(hbox);
 	}
 
 	private Node createArrow(final boolean left) {
@@ -204,8 +198,7 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 		arrow.setStroke(ARROW_STROKE);
 		arrow.setFill(ARROW_FILL);
 		// effect
-		effectOnHover(arrow,
-				new DropShadow(DROP_SHADOW_RADIUS, HIGHLIGHT_COLOR));
+		effectOnHover(arrow, new DropShadow(DROP_SHADOW_RADIUS, hightlightColor));
 		// action
 		arrow.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
@@ -222,10 +215,8 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 		refreshMenuItem();
 
 		// highlighting
-		templateGroup.setEffect(createDropShadowReflectionEffect(
-				DROP_SHADOW_RADIUS, Color.TRANSPARENT));
-		effectOnHover(templateGroup, createDropShadowReflectionEffect(
-				DROP_SHADOW_RADIUS, HIGHLIGHT_COLOR));
+		templateGroup.setEffect(createDropShadowReflectionEffect(DROP_SHADOW_RADIUS, Color.TRANSPARENT));
+		effectOnHover(templateGroup, createDropShadowReflectionEffect(DROP_SHADOW_RADIUS, hightlightColor));
 
 		// register click action
 		templateGroup.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -299,12 +290,9 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 
 		// compute translation based on the bounds, scaling, and width/height
 		// deltas
-		Affine contentsTransform = getViewer().getCanvas()
-				.contentTransformProperty().get();
-		double x = boundsInContent.getMinX()
-				- bounds.getMinX() / contentsTransform.getMxx() - dx / 2;
-		double y = boundsInContent.getMinY()
-				- bounds.getMinY() / contentsTransform.getMyy() - dy / 2;
+		Affine contentsTransform = getViewer().getCanvas().contentTransformProperty().get();
+		double x = boundsInContent.getMinX() - bounds.getMinX() / contentsTransform.getMxx() - dx / 2;
+		double y = boundsInContent.getMinY() - bounds.getMinY() / contentsTransform.getMyy() - dy / 2;
 
 		// close the creation menu
 		closeMenu();
@@ -315,23 +303,19 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 
 		// build create operation
 		IRootPart<Node, ? extends Node> root = getHost().getRoot();
-		CreationPolicy<Node> creationPolicy = root
-				.getAdapter(new TypeToken<CreationPolicy<Node>>() {
-				});
+		CreationPolicy<Node> creationPolicy = root.getAdapter(new TypeToken<CreationPolicy<Node>>() {
+		});
 		creationPolicy.init();
-		IContentPart<Node, ? extends Node> contentPart = creationPolicy
-				.create(toCreate, item.findContentParent(root), HashMultimap
-						.<IContentPart<Node, ? extends Node>, String> create());
+		IContentPart<Node, ? extends Node> contentPart = creationPolicy.create(toCreate, item.findContentParent(root),
+				HashMultimap.<IContentPart<Node, ? extends Node>, String> create());
 
 		// relocate to final position
-		FXTransformPolicy txPolicy = contentPart
-				.getAdapter(FXTransformPolicy.class);
+		FXTransformPolicy txPolicy = contentPart.getAdapter(FXTransformPolicy.class);
 		txPolicy.init();
 		txPolicy.setTransform(new AffineTransform(1, 0, 0, 1, x, y));
 
 		// assemble operations
-		ReverseUndoCompositeOperation rev = new ReverseUndoCompositeOperation(
-				"CreateOnClick");
+		ReverseUndoCompositeOperation rev = new ReverseUndoCompositeOperation("CreateOnClick");
 		rev.add(creationPolicy.commit());
 		rev.add(txPolicy.commit());
 
@@ -353,27 +337,21 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 
 		hbox = new HBox();
 		hbox.getChildren().addAll(wrapWithPadding(leftArrow, padding),
-				wrapWithPadding(menuItem, padding, maxWidth, maxHeight),
-				wrapWithPadding(rightArrow, padding));
+				wrapWithPadding(menuItem, padding, maxWidth, maxHeight), wrapWithPadding(rightArrow, padding));
 
 		// place into overlay group
-		final Group overlayGroup = getViewer().getCanvas()
-				.getScrolledOverlayGroup();
+		final Group overlayGroup = getViewer().getCanvas().getScrolledOverlayGroup();
 		overlayGroup.getChildren().add(hbox);
 
 		hbox.layoutBoundsProperty().addListener(new ChangeListener<Bounds>() {
 			@Override
-			public void changed(ObservableValue<? extends Bounds> observable,
-					Bounds oldBounds, Bounds newBounds) {
-				Affine contentTransform = getViewer().getCanvas()
-						.getContentTransform();
+			public void changed(ObservableValue<? extends Bounds> observable, Bounds oldBounds, Bounds newBounds) {
+				Affine contentTransform = getViewer().getCanvas().getContentTransform();
 				hbox.setTranslateX(-newBounds.getWidth() / 2);
 				hbox.setTranslateY(-newBounds.getHeight() / 2);
 				hbox.setScaleX(contentTransform.getMxx());
 				hbox.setScaleY(contentTransform.getMyy());
-				Point2D pos = overlayGroup.sceneToLocal(
-						initialMousePositionInScene.x,
-						initialMousePositionInScene.y);
+				Point2D pos = overlayGroup.sceneToLocal(initialMousePositionInScene.x, initialMousePositionInScene.y);
 				hbox.setLayoutX(pos.getX());
 				hbox.setLayoutY(pos.getY());
 			}
@@ -383,17 +361,14 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 	private void refreshMenuItem() {
 		// exchange template visual
 		templateGroup.getChildren().clear();
-		templateGroup.getChildren()
-				.add(items.get(currentItemIndex).createVisual());
+		templateGroup.getChildren().add(items.get(currentItemIndex).createVisual());
 	}
 
 	private void refreshMenuItems() {
 		@SuppressWarnings("serial")
-		List<IFXCreationMenuItem> menuItems = getHost()
-				.<Provider<List<IFXCreationMenuItem>>> getAdapter(AdapterKey
-						.get(new TypeToken<Provider<List<IFXCreationMenuItem>>>() {
-						}, MENU_ITEM_PROVIDER_ROLE))
-				.get();
+		List<IFXCreationMenuItem> menuItems = getHost().<Provider<List<IFXCreationMenuItem>>> getAdapter(
+				AdapterKey.get(new TypeToken<Provider<List<IFXCreationMenuItem>>>() {
+				}, MENU_ITEM_PROVIDER_ROLE)).get();
 		this.items.clear();
 		this.items.addAll(menuItems);
 		// compute max width and height
@@ -415,15 +390,13 @@ public class FXCreationMenuOnClickPolicy extends AbstractInteractionPolicy<Node>
 	}
 
 	private StackPane wrapWithPadding(Node node, double padding) {
-		return wrapWithPadding(node, padding, node.getLayoutBounds().getWidth(),
-				node.getLayoutBounds().getHeight());
+		return wrapWithPadding(node, padding, node.getLayoutBounds().getWidth(), node.getLayoutBounds().getHeight());
 	}
 
-	private StackPane wrapWithPadding(Node node, double padding, double width,
-			double height) {
+	private StackPane wrapWithPadding(Node node, double padding, double width, double height) {
 		StackPane stack = new StackPane();
-		stack.getChildren().addAll(new Rectangle(width + padding + padding,
-				height + padding + padding, Color.TRANSPARENT), node);
+		stack.getChildren()
+				.addAll(new Rectangle(width + padding + padding, height + padding + padding, Color.TRANSPARENT), node);
 		return stack;
 	}
 
