508b00c18c650cda2f032c58966bcfc1fc132dcf
anyssen <nyssen@itemis.de>
2016-05-21 09:46:39 +0200

    [494193] Add link between test case and bug.

--- org.eclipse.gef4.geometry.tests/src/org/eclipse/gef4/geometry/tests/BezierCurveTests.java
+++ org.eclipse.gef4.geometry.tests/src/org/eclipse/gef4/geometry/tests/BezierCurveTests.java
@@ -141,6 +141,7 @@ public class BezierCurveTests {
 		BezierCurve c0 = new BezierCurve(1, 1, 1, 10, 10, 1, 10, 10);
 		assertEquals(new Rectangle(1, 1, 9, 9), c0.getBounds());
 
+		// test bounds computation reported in bug #494193
 		BezierCurve c1 = new BezierCurve(399.05999755859375, 96.6969985961914,
 				484.6500244140625, 209.1699981689453, 456.27001953125,
 				302.8699951171875, 438.55999755859375, 348.239990234375);
