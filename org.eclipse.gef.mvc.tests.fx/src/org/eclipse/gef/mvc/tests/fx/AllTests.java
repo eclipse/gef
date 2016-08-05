ee318caca92df8c858a88fc2337e894cf02cb0a1
mwienand <matthias.wienand@itemis.de>
2016-08-05 17:38:17 +0200

    [498150] Disable all but one failing test.

--- org.eclipse.gef.mvc.tests.fx/src/org/eclipse/gef/mvc/tests/fx/AllTests.java
+++ org.eclipse.gef.mvc.tests.fx/src/org/eclipse/gef/mvc/tests/fx/AllTests.java
@@ -17,8 +17,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AbstractFXHandlePartTests.class, FXBendConnectionPolicyTests.class, FXClickDragToolTests.class,
-		FXTypeToolTests.class, FXTransformPolicyTests.class, FXViewerTests.class, FocusTraversalPolicyTests.class })
+@SuiteClasses({ FXClickDragToolTests.class })
 public class AllTests {
 
 }
