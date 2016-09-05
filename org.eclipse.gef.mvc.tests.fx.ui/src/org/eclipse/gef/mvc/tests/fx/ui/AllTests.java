a67d574a131f622314305ff016ae3ba9e02825fe
mwienand <matthias.wienand@itemis.de>
2016-09-05 16:00:59 +0200

    [500205] Add missing copyright headers.

--- org.eclipse.gef.mvc.tests.fx.ui/src/org/eclipse/gef/mvc/tests/fx/ui/AllTests.java
+++ org.eclipse.gef.mvc.tests.fx.ui/src/org/eclipse/gef/mvc/tests/fx/ui/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2016 itemis AG and others.
+ * Copyright (c) 2016 itemis AG and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *     Matthias Wienand (itemis AG) - initial API and implementation
  *
  *******************************************************************************/
 package org.eclipse.gef.mvc.tests.fx.ui;
