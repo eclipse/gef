a6735707aed02dd405589eb81edd090bc129fc72
Tamas Miklossy <miklossy@itemis.de>
2016-11-26 23:41:30 +0100

    [NONE] Clean up the Dot Extractor test cases.
    - Move the test graphs used by the Dot Extractor test cases into the
    DotTestGraphs class.
    - Convert each test case into a separate junit test case to avoid that a
    failing test prevents the execution of the remaining tests.

--- org.eclipse.gef.dot.tests/src/org/eclipse/gef/dot/tests/DotExtractorTests.java
+++ org.eclipse.gef.dot.tests/src/org/eclipse/gef/dot/tests/DotExtractorTests.java
@@ -1,17 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2016 Fabian Steeg. All rights reserved. This program and
- * the accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
+ * Copyright (c) 2009, 2016 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * <p/>
+ *
  * Contributors: 
  *     Fabian Steeg - initial API and implementation; see bug 277380
  *     Alexander Nyßen (itemis AG) - rename refactoring
+ *     Tamas Miklossy  (itemis AG) - minor refactoring
+ *
  *******************************************************************************/
 package org.eclipse.gef.dot.tests;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import org.eclipse.gef.dot.internal.ui.DotGraphView;
 import org.eclipse.gef.dot.internal.ui.DotGraphView.DotExtractor;
@@ -25,38 +26,59 @@ import org.junit.Test;
  */
 public class DotExtractorTests {
 
-	/* Testing input and output values: */
-	@SuppressWarnings("serial")
-	Map<String, String> values = new HashMap<String, String>() {
-		{
-			put("/** Javadoc stuff graph name{a;b;a->b} and more */", //$NON-NLS-1$
-					"graph name{a;b;a->b}"); //$NON-NLS-1$
-			put("/** Javadoc stuff graph long_name{a;b;a->b} and more */", //$NON-NLS-1$
-					"graph long_name{a;b;a->b}"); //$NON-NLS-1$
-			put("/* Java block comment \n stuff digraph{a;b;a->b} and more */", //$NON-NLS-1$
-					"digraph{a;b;a->b}"); //$NON-NLS-1$
-			put("Stuff about a graph and then graph{a;b;a->b} and more ", //$NON-NLS-1$
-					"graph{a;b;a->b}"); //$NON-NLS-1$
-			put("Stuff about a graph and then with breaks graph{a\nb\na->b} and more ", //$NON-NLS-1$
-					"graph{a\nb\na->b}"); //$NON-NLS-1$
-			put("Stuff about a graph and then digraph{a;b;a->b} and more ", //$NON-NLS-1$
-					"digraph{a;b;a->b}"); //$NON-NLS-1$
-			put("Stuff about a graph and then digraph{subgraph cluster_0{1->2}; 1->3} and more ", //$NON-NLS-1$
-					"digraph{subgraph cluster_0{1->2}; 1->3}"); //$NON-NLS-1$
-			put("Stuff about a graph then graph{node[shape=record];1[label=\"{Text|Text}\"]} and more", //$NON-NLS-1$
-					"graph{node[shape=record];1[label=\"{Text|Text}\"]}"); //$NON-NLS-1$
-		}
-	};
+	@Test
+	public void testDotExtraction01() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_01,
+				DotTestGraphs.EXTRACTED_01);
+	}
 
 	@Test
-	public void extractDot() {
-		for (String input : values.keySet()) {
-			String expected = values.get(input);
-			String output = new DotGraphView.DotExtractor(input).getDotString();
-			Assert.assertEquals(/*
-								 * String.format(
-								 * "Incorrect DOT extraction for '%s';", input),
-								 */expected, output);
-		}
+	public void testDotExtraction02() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_02,
+				DotTestGraphs.EXTRACTED_02);
+	}
+
+	@Test
+	public void testDotExtraction03() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_03,
+				DotTestGraphs.EXTRACTED_03);
+	}
+
+	@Test
+	public void testDotExtraction04() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_04,
+				DotTestGraphs.EXTRACTED_04);
+	}
+
+	@Test
+	public void testDotExtraction05() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_05,
+				DotTestGraphs.EXTRACTED_05);
+	}
+
+	@Test
+	public void testDotExtraction06() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_06,
+				DotTestGraphs.EXTRACTED_06);
+	}
+
+	@Test
+	public void testDotExtraction07() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_07,
+				DotTestGraphs.EXTRACTED_07);
+	}
+
+	@Test
+	public void testDotExtraction08() {
+		testDotExtraction(DotTestGraphs.EMBEDDED_08,
+				DotTestGraphs.EXTRACTED_08);
+	}
+
+	private void testDotExtraction(String embedded, String expected) {
+		String extracted = new DotGraphView.DotExtractor(embedded)
+				.getDotString();
+		Assert.assertEquals(
+				String.format("Incorrect DOT extraction for '%s';", embedded),
+				expected.trim(), extracted);
 	}
 }
