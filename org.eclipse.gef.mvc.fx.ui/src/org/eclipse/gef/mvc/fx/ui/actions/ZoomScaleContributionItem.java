1c250a8ef8b0074c62fddf2c2e1f6814b53e5355
mwienand <matthias.wienand@itemis.de>
2017-01-09 13:59:43 +0100

    [482139] Use exponential function to compute scale/zoom values.

--- org.eclipse.gef.mvc.fx.ui/src/org/eclipse/gef/mvc/fx/ui/actions/ZoomScaleContributionItem.java
+++ org.eclipse.gef.mvc.fx.ui/src/org/eclipse/gef/mvc/fx/ui/actions/ZoomScaleContributionItem.java
@@ -12,9 +12,6 @@
  *******************************************************************************/
 package org.eclipse.gef.mvc.fx.ui.actions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.gef.fx.nodes.InfiniteCanvas;
 import org.eclipse.gef.mvc.fx.viewer.IViewer;
 import org.eclipse.swt.SWT;
@@ -38,163 +35,23 @@ import javafx.scene.Parent;
 public class ZoomScaleContributionItem extends AbstractViewerContributionItem {
 
 	/**
-	 */
-	protected static class Ranges {
-		private int minSource = 0;
-		private int maxSource = 1000;
-		private double minTarget = 0.03125;
-		private double maxTarget = 32.0;
-		private List<Integer> stopsSource = new ArrayList<>();
-		private List<Double> stopsTarget = new ArrayList<>();
-
-		/**
-		 */
-		public Ranges() {
-		}
-
-		/**
-		 * @param minSource
-		 *            a
-		 * @param maxSource
-		 *            a
-		 * @param minTarget
-		 *            a
-		 * @param maxTarget
-		 *            a
-		 */
-		public Ranges(int minSource, int maxSource, double minTarget,
-				double maxTarget) {
-			this.minSource = minSource;
-			this.maxSource = maxSource;
-			this.minTarget = minTarget;
-			this.maxTarget = maxTarget;
-		}
-
-		/**
-		 * @param source
-		 *            a
-		 * @param target
-		 *            a
-		 * @return a
-		 */
-		public Ranges addStop(int source, double target) {
-			stopsSource.add(source);
-			stopsTarget.add(target);
-			return this;
-		}
-
-		/**
-		 * @param target
-		 *            a
-		 * @return a
-		 */
-		public int toSource(double target) {
-			int mins = minSource;
-			int maxs = maxSource;
-			double mind = minTarget;
-			double maxd = maxTarget;
-
-			// iterate stops to find nearest min/max
-			for (int i = 0; i < stopsTarget.size(); i++) {
-				Double stopTarget = stopsTarget.get(i);
-				Integer stopSource = stopsSource.get(i);
-				if (target > stopTarget) {
-					mind = stopTarget;
-					mins = stopSource;
-				}
-				if (target < stopTarget) {
-					if (stopTarget < maxd) {
-						maxd = stopTarget;
-					}
-					if (stopSource < maxs) {
-						maxs = stopSource;
-					}
-				}
-			}
-
-			// map from double range to int range
-			return (int) (mins
-					+ (maxs - mins) * (target - mind) / (maxd - mind));
-		}
-
-		/**
-		 * @param source
-		 *            a
-		 * @return a
-		 */
-		public double toTarget(int source) {
-			// System.out.println("toTarget(" + source + ")");
-
-			int mins = minSource;
-			int maxs = maxSource;
-			double mind = minTarget;
-			double maxd = maxTarget;
-
-			// System.out.println(" Min/Max: " + mins + " : " + mind + " -> "
-			// + maxs + " : " + maxd);
-
-			// iterate stops to find nearest min/max
-			for (int i = 0; i < stopsSource.size(); i++) {
-				Integer stopSource = stopsSource.get(i);
-				Double stopTarget = stopsTarget.get(i);
-
-				// System.out.println(
-				// " Stop " + i + ": " + stopSource + " : " + stopTarget);
-
-				if (source > stopSource) {
-					mins = stopSource;
-					mind = stopTarget;
-					// System.out.println(" -> Min: " + mins + " : " + mind);
-				}
-				if (source < stopSource) {
-					if (stopSource < maxs) {
-						maxs = stopSource;
-					}
-					if (stopTarget < maxd) {
-						maxd = stopTarget;
-					}
-					// System.out.println(" -> Max: " + maxs + " : " + maxd);
-				}
-			}
-
-			// System.out.println(" Result: " + mind + " + (" + maxd + " - " +
-			// mind
-			// + ") * (" + source + " - " + mins + ") / (" + maxs + " - "
-			// + mins + ")");
-
-			// map from int range to double range
-			double target = mind
-					+ (maxd - mind) * (source - mins) / (double) (maxs - mins);
-
-			// System.out.println(" -> " + target);
-
-			return target;
-		}
-	}
-
-	/**
 	 *
 	 */
 	public static final String ZOOM_SCALE_CONTRIBUTION_ITEM_ID = "ZoomScaleContributionItem";
 
-	private static final double coeffBase = 0.474357;
-	private static final double coeffExpo = 0.000421363;
-	// double zoomFactor = (coeffBase * (Math.e ** (coeffExpo * selection)))
-
-	private static final double invCoeffBase = 2373.25;
-	private static final double invCoeffExpo = 2.10812;
-	// int selection = Math.round(invCoeffBase * log(invCoeffExpo * zoomFactor))
+	private static final int SCALE_MINIMUM = 1; // 0.03125 zoom
+	private static final int SCALE_MAXIMUM = 10000; // 32.0 zoom
+	private static final double SCALE_TO_ZOOM_COEFF_BASE = 0.0312283;// 0.474357;
+	private static final double SCALE_TO_ZOOM_COEFF_EXPO = 0.000693217; // 0.000421363;
+	private static final double ZOOM_TO_SCALE_COEFF_BASE = 1442.55; // 2373.25;
+	private static final double ZOOM_TO_SCALE_COEFF_EXPO = 32.0222; // 2.10812;
 
 	// controls
 	private ToolItem toolItem;
-
 	private Scale zoomScale;
 
 	// listeners
 	private ChangeListener<? super Number> zoomListener;
-
-	// helpers
-	private Ranges ranges;
 
 	// zoom action
 	private AbstractZoomAction zoomAction = new AbstractZoomAction("Zoom") {
@@ -218,11 +75,6 @@ public class ZoomScaleContributionItem extends AbstractViewerContributionItem {
 					"Zoom listener is already registered.");
 		}
 
-		ranges = new Ranges(0, getMaximumScaleValue(), getMinimumZoomFactor(),
-				getMaximumZoomFactor());
-		ranges.addStop(2500, 1d).addStop(4000, 2.5d).addStop(6000, 6d)
-				.addStop(8000, 14d);
-
 		Parent canvas = getViewer().getCanvas();
 		if (canvas instanceof InfiniteCanvas) {
 			InfiniteCanvas infiniteCanvas = (InfiniteCanvas) canvas;
@@ -233,6 +85,30 @@ public class ZoomScaleContributionItem extends AbstractViewerContributionItem {
 					.addListener(zoomListener);
 			updateScaleValue(infiniteCanvas.getContentTransform().getMxx());
 		}
+	}
+
+	/**
+	 * a
+	 *
+	 * @param zoomFactor
+	 *            a
+	 * @return a
+	 */
+	protected int computeScaleValue(double zoomFactor) {
+		return (int) Math.round(ZOOM_TO_SCALE_COEFF_BASE
+				* Math.log(ZOOM_TO_SCALE_COEFF_EXPO * zoomFactor));
+	}
+
+	/**
+	 * a
+	 *
+	 * @param scaleValue
+	 *            a
+	 * @return a
+	 */
+	protected double computeZoomFactor(int scaleValue) {
+		return SCALE_TO_ZOOM_COEFF_BASE
+				* Math.pow(Math.E, SCALE_TO_ZOOM_COEFF_EXPO * scaleValue);
 	}
 
 	@Override
@@ -279,9 +155,9 @@ public class ZoomScaleContributionItem extends AbstractViewerContributionItem {
 		toolItem = new ToolItem(tb, SWT.SEPARATOR, index);
 
 		zoomScale = new Scale(tb, SWT.HORIZONTAL);
-		zoomScale.setMinimum(0);
-		zoomScale.setMaximum(getMaximumScaleValue());
-		zoomScale.setSelection(getMaximumScaleValue() / 2);
+		zoomScale.setMinimum(SCALE_MINIMUM);
+		zoomScale.setMaximum(SCALE_MAXIMUM);
+		zoomScale.setSelection(SCALE_MAXIMUM / 2 + SCALE_MINIMUM / 2);
 
 		zoomScale.setSize(75, 100);
 		toolItem.setWidth(zoomScale.getSize().x);
@@ -296,7 +172,7 @@ public class ZoomScaleContributionItem extends AbstractViewerContributionItem {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				int sel = zoomScale.getSelection();
-				double zoom = ranges.toTarget(sel);
+				double zoom = computeZoomFactor(sel);
 				if (zoom > 0) {
 					Event event = new Event();
 					event.data = zoom;
@@ -312,27 +188,6 @@ public class ZoomScaleContributionItem extends AbstractViewerContributionItem {
 		updateScaleValue(mxx);
 	}
 
-	/**
-	 * @return a
-	 */
-	protected int getMaximumScaleValue() {
-		return 10000;
-	}
-
-	/**
-	 * @return a
-	 */
-	protected double getMaximumZoomFactor() {
-		return 32d;
-	}
-
-	/**
-	 * @return a
-	 */
-	protected double getMinimumZoomFactor() {
-		return 1d / 32d;
-	}
-
 	@Override
 	public void init(IViewer viewer) {
 		super.init(viewer);
@@ -345,7 +200,7 @@ public class ZoomScaleContributionItem extends AbstractViewerContributionItem {
 	 */
 	protected void updateScaleValue(double n) {
 		if (zoomScale != null) {
-			zoomScale.setSelection(ranges.toSource(n));
+			zoomScale.setSelection(computeScaleValue(n));
 		}
 	}
 }
