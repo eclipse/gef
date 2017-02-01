fb9160e9386af9e26bcb3078ddedaf2b9facd094
mwienand <matthias.wienand@itemis.de>
2017-02-01 14:37:07 +0100

    [508830] Fix javadoc errors.

--- org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/formatting/DotEscStringFormatter.java
+++ org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/formatting/DotEscStringFormatter.java
@@ -3,30 +3,21 @@
  */
 package org.eclipse.gef.dot.internal.language.formatting;
 
-import com.google.inject.Inject;
+import org.eclipse.gef.dot.internal.language.services.DotEscStringGrammarAccess;
 import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
-import org.eclipse.gef.dot.internal.language.services.DotEscStringGrammarAccess;
+
+import com.google.inject.Inject;
 
 /**
  * This class contains custom formatting declarations.
- * 
- * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#formatting
- * on how and when to use it.
- * 
- * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
 public class DotEscStringFormatter extends AbstractDeclarativeFormatter {
-	
+
 	@Inject
-	private DotEscStringGrammarAccess grammarAccess; 
-	
+	private DotEscStringGrammarAccess grammarAccess;
+
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
-// It's usually a good idea to activate the following three statements.
-// They will add and preserve newlines around comments
-//		c.setLinewrap(0, 1, 2).before(grammarAccess.getSL_COMMENTRule());
-//		c.setLinewrap(0, 1, 2).before(grammarAccess.getML_COMMENTRule());
-//		c.setLinewrap(0, 1, 1).after(grammarAccess.getML_COMMENTRule());
 	}
 }
--- org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/formatting/DotHtmlLabelFormatter.java
+++ org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/formatting/DotHtmlLabelFormatter.java
@@ -3,25 +3,23 @@
  */
 package org.eclipse.gef.dot.internal.language.formatting;
 
-import com.google.inject.Inject;
+import org.eclipse.gef.dot.internal.language.services.DotHtmlLabelGrammarAccess;
 import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
-import org.eclipse.gef.dot.internal.language.services.DotHtmlLabelGrammarAccess;
+
+import com.google.inject.Inject;
 
 /**
  * This class contains custom formatting declarations.
  */
 public class DotHtmlLabelFormatter extends AbstractDeclarativeFormatter {
-	
+
 	@Inject
-	private DotHtmlLabelGrammarAccess grammarAccess; 
-	
+	private DotHtmlLabelGrammarAccess grammarAccess;
+
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
-// It's usually a good idea to activate the following three statements.
-// They will add and preserve newlines around comments
-//		c.setLinewrap(0, 1, 2).before(grammarAccess.getSL_COMMENTRule());
-//		c.setLinewrap(0, 1, 2).before(grammarAccess.getML_COMMENTRule());
-//		c.setLinewrap(0, 1, 1).after(grammarAccess.getML_COMMENTRule());
+		// TODO: preserve newlines around comments
+		// c.setLinewrap(0, 1, 2).before(grammarAccess.getHTML_COMMENTRule());
 	}
 }
