58f3d87737f236e74fa49b8213370beec981f971
anyssen <nyssen@itemis.de>
2017-03-14 09:54:44 +0100

    [NONE] Remove interaction capabilites from DotGraphView.
    Removed all but selection and hover interaction from DotGraphView, as
    moving/rotating/hiding nodes within the DotGraph view is meaningless.
    Selection will be needed if we want to synchronize with the Xtext
    editor.

--- org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/DotGraphViewModule.java
+++ org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/DotGraphViewModule.java
@@ -12,81 +12,215 @@
 package org.eclipse.gef.dot.internal.ui;
 
 import org.eclipse.gef.common.adapt.AdapterKey;
+import org.eclipse.gef.common.adapt.inject.AdaptableScopes;
+import org.eclipse.gef.common.adapt.inject.AdapterMaps;
+import org.eclipse.gef.mvc.fx.MvcFxModule;
+import org.eclipse.gef.mvc.fx.behaviors.ConnectionClickableAreaBehavior;
+import org.eclipse.gef.mvc.fx.behaviors.HoverBehavior;
+import org.eclipse.gef.mvc.fx.domain.IDomain;
 import org.eclipse.gef.mvc.fx.parts.DefaultFocusFeedbackPartFactory;
 import org.eclipse.gef.mvc.fx.parts.DefaultHoverFeedbackPartFactory;
 import org.eclipse.gef.mvc.fx.parts.DefaultSelectionFeedbackPartFactory;
-import org.eclipse.gef.mvc.fx.parts.DefaultSelectionHandlePartFactory;
+import org.eclipse.gef.mvc.fx.parts.IContentPartFactory;
+import org.eclipse.gef.mvc.fx.policies.FocusAndSelectOnClickPolicy;
 import org.eclipse.gef.mvc.fx.policies.HoverOnHoverPolicy;
-import org.eclipse.gef.mvc.fx.policies.NormalizeConnectedOnDragPolicy;
-import org.eclipse.gef.mvc.fx.policies.ResizePolicy;
-import org.eclipse.gef.mvc.fx.policies.TransformPolicy;
+import org.eclipse.gef.mvc.fx.policies.SelectFocusedOnTypePolicy;
+import org.eclipse.gef.mvc.fx.policies.TraverseFocusOnTypePolicy;
+import org.eclipse.gef.mvc.fx.providers.GeometricOutlineProvider;
 import org.eclipse.gef.mvc.fx.providers.ShapeBoundsProvider;
-import org.eclipse.gef.zest.fx.ZestFxModule;
-import org.eclipse.gef.zest.fx.behaviors.NodeHidingBehavior;
-import org.eclipse.gef.zest.fx.behaviors.NodeLayoutBehavior;
-import org.eclipse.gef.zest.fx.policies.HideOnTypePolicy;
-import org.eclipse.gef.zest.fx.policies.HidePolicy;
-import org.eclipse.gef.zest.fx.policies.OpenNestedGraphOnDoubleClickPolicy;
-import org.eclipse.gef.zest.fx.policies.ShowHiddenNeighborsOnTypePolicy;
-import org.eclipse.gef.zest.fx.policies.ShowHiddenNeighborsPolicy;
-import org.eclipse.gef.zest.fx.policies.TranslateSelectedAndRelocateLabelsOnDragPolicy;
+import org.eclipse.gef.mvc.fx.viewer.IViewer;
+import org.eclipse.gef.zest.fx.parts.EdgeLabelPart;
+import org.eclipse.gef.zest.fx.parts.EdgePart;
+import org.eclipse.gef.zest.fx.parts.NodeLabelPart;
+import org.eclipse.gef.zest.fx.parts.NodePart;
+import org.eclipse.gef.zest.fx.parts.ZestFxContentPartFactory;
+import org.eclipse.gef.zest.fx.parts.ZestFxRootPart;
 
+import com.google.inject.Binder;
 import com.google.inject.Provider;
 import com.google.inject.multibindings.MapBinder;
 
-public class DotGraphViewModule extends ZestFxModule {
+public class DotGraphViewModule extends MvcFxModule {
 
 	@Override
+	protected void bindAbstractContentPartAdapters(
+			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		super.bindAbstractContentPartAdapters(adapterMapBinder);
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(FocusAndSelectOnClickPolicy.class);
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(SelectFocusedOnTypePolicy.class);
+	}
+
+	/**
+	 * Adds (default) adapter map bindings for {@link EdgeLabelPart} and all
+	 * sub-classes. May be overwritten by sub-classes to change the default
+	 * bindings.
+	 *
+	 * @param adapterMapBinder
+	 *            The {@link MapBinder} to be used for the binding registration.
+	 *            In this case, will be obtained from
+	 *            {@link AdapterMaps#getAdapterMapBinder(Binder, Class)} using
+	 *            {@link EdgeLabelPart} as a key.
+	 *
+	 * @see AdapterMaps#getAdapterMapBinder(Binder, Class)
+	 */
+	protected void bindEdgeLabelPartAdapters(
+			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		// selection link feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultSelectionFeedbackPartFactory.SELECTION_LINK_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(ShapeBoundsProvider.class);
+
+		// selection feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultSelectionFeedbackPartFactory.SELECTION_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(ShapeBoundsProvider.class);
+
+		// hover feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultHoverFeedbackPartFactory.HOVER_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(ShapeBoundsProvider.class);
+
+		// hover on-hover policy
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(HoverOnHoverPolicy.class);
+	}
+
+	/**
+	 * Adds (default) adapter map bindings for {@link EdgePart} and all
+	 * sub-classes. May be overwritten by sub-classes to change the default
+	 * bindings.
+	 *
+	 * @param adapterMapBinder
+	 *            The {@link MapBinder} to be used for the binding registration.
+	 *            In this case, will be obtained from
+	 *            {@link AdapterMaps#getAdapterMapBinder(Binder, Class)} using
+	 *            {@link EdgePart} as a key.
+	 *
+	 * @see AdapterMaps#getAdapterMapBinder(Binder, Class)
+	 */
+	protected void bindEdgePartAdapters(
+			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		// selection link feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultSelectionFeedbackPartFactory.SELECTION_LINK_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(GeometricOutlineProvider.class);
+
+		// selection feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultSelectionFeedbackPartFactory.SELECTION_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(GeometricOutlineProvider.class);
+
+		// hover feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultHoverFeedbackPartFactory.HOVER_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(GeometricOutlineProvider.class);
+
+		// geometry provider for focus feedback
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultFocusFeedbackPartFactory.FOCUS_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(GeometricOutlineProvider.class);
+
+		// clickable area behavior
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(ConnectionClickableAreaBehavior.class);
+
+		// hover on-hover policy
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(HoverOnHoverPolicy.class);
+	}
+
+	/**
+	 * Binds {@link IContentPartFactory} to {@link ZestFxContentPartFactory}.
+	 */
+	protected void bindIContentPartFactory() {
+		binder().bind(IContentPartFactory.class)
+				.to(ZestFxContentPartFactory.class)
+				.in(AdaptableScopes.typed(IViewer.class));
+	}
+
+	@Override
+	protected void bindIRootPartAdaptersForContentViewer(
+			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		super.bindIRootPartAdaptersForContentViewer(adapterMapBinder);
+
+		// keyboard focus traversal
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(TraverseFocusOnTypePolicy.class);
+
+		// select focused on type
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(SelectFocusedOnTypePolicy.class);
+
+		// hover behavior
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(HoverBehavior.class);
+	}
+
+	/**
+	 * Adds (default) adapter map bindings for {@link NodeLabelPart} and all
+	 * sub-classes. May be overwritten by sub-classes to change the default
+	 * bindings.
+	 *
+	 * @param adapterMapBinder
+	 *            The {@link MapBinder} to be used for the binding registration.
+	 *            In this case, will be obtained from
+	 *            {@link AdapterMaps#getAdapterMapBinder(Binder, Class)} using
+	 *            {@link NodeLabelPart} as a key.
+	 *
+	 * @see AdapterMaps#getAdapterMapBinder(Binder, Class)
+	 */
+	protected void bindNodeLabelPartAdapters(
+			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		// selection link feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultSelectionFeedbackPartFactory.SELECTION_LINK_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(ShapeBoundsProvider.class);
+
+		// selection feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultSelectionFeedbackPartFactory.SELECTION_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(ShapeBoundsProvider.class);
+
+		// hover feedback provider
+		adapterMapBinder
+				.addBinding(AdapterKey
+						.role(DefaultHoverFeedbackPartFactory.HOVER_FEEDBACK_GEOMETRY_PROVIDER))
+				.to(ShapeBoundsProvider.class);
+
+		// hover on-hover policy
+		adapterMapBinder.addBinding(AdapterKey.defaultRole())
+				.to(HoverOnHoverPolicy.class);
+	}
+
+	/**
+	 * Adds (default) adapter map bindings for {@link NodePart} and all
+	 * sub-classes. May be overwritten by sub-classes to change the default
+	 * bindings.
+	 *
+	 * @param adapterMapBinder
+	 *            The {@link MapBinder} to be used for the binding registration.
+	 *            In this case, will be obtained from
+	 *            {@link AdapterMaps#getAdapterMapBinder(Binder, Class)} using
+	 *            {@link NodePart} as a key.
+	 *
+	 * @see AdapterMaps#getAdapterMapBinder(Binder, Class)
+	 */
 	protected void bindNodePartAdapters(
 			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
-		// layout
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(NodeLayoutBehavior.class);
-		// pruning
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(HidePolicy.class);
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(ShowHiddenNeighborsPolicy.class);
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(NodeHidingBehavior.class);
-
-		// translate on-drag
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(TranslateSelectedAndRelocateLabelsOnDragPolicy.class);
-
-		// show hidden neighbors on-type
-		adapterMapBinder.addBinding(AdapterKey.role("show-hidden-neighbors")) //$NON-NLS-1$
-				.to(ShowHiddenNeighborsOnTypePolicy.class);
-
-		// hide on-type
-		adapterMapBinder.addBinding(AdapterKey.role("hide")) //$NON-NLS-1$
-				.to(HideOnTypePolicy.class);
-
-		adapterMapBinder.addBinding(AdapterKey.role("open-nested-graph")) //$NON-NLS-1$
-				.to(OpenNestedGraphOnDoubleClickPolicy.class);
-
-		// transform policy for relocation
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(TransformPolicy.class);
-
-		// resize policy to resize nesting nodes
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(ResizePolicy.class);
-
 		// anchor provider
 		adapterMapBinder.addBinding(AdapterKey.defaultRole())
 				.to(DotAnchorProvider.class);
-
-		// feedback and handles
-		adapterMapBinder
-				.addBinding(AdapterKey
-						.role(DefaultSelectionHandlePartFactory.SELECTION_HANDLES_GEOMETRY_PROVIDER))
-				.toProvider(new Provider<ShapeBoundsProvider>() {
-					@Override
-					public ShapeBoundsProvider get() {
-						return new ShapeBoundsProvider(1);
-					}
-				});
 
 		// selection feedback provider
 		adapterMapBinder
@@ -130,9 +264,30 @@ public class DotGraphViewModule extends ZestFxModule {
 		// hover on-hover
 		adapterMapBinder.addBinding(AdapterKey.defaultRole())
 				.to(HoverOnHoverPolicy.class);
+	}
 
-		// normalize on drag
-		adapterMapBinder.addBinding(AdapterKey.defaultRole())
-				.to(NormalizeConnectedOnDragPolicy.class);
+	@Override
+	protected void bindRootPartAsContentViewerAdapter(
+			MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		adapterMapBinder
+				.addBinding(AdapterKey.role(IDomain.CONTENT_VIEWER_ROLE))
+				.to(ZestFxRootPart.class)
+				.in(AdaptableScopes.typed(IViewer.class));
+	}
+
+	@Override
+	protected void configure() {
+		super.configure();
+
+		bindIContentPartFactory();
+
+		bindNodePartAdapters(
+				AdapterMaps.getAdapterMapBinder(binder(), NodePart.class));
+		bindEdgePartAdapters(
+				AdapterMaps.getAdapterMapBinder(binder(), EdgePart.class));
+		bindEdgeLabelPartAdapters(
+				AdapterMaps.getAdapterMapBinder(binder(), EdgeLabelPart.class));
+		bindNodeLabelPartAdapters(
+				AdapterMaps.getAdapterMapBinder(binder(), NodeLabelPart.class));
 	}
 }
