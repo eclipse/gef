92b0134715dace6295cc8bc95a46b2cf3a1e850d
anyssen <nyssen@itemis.de>
2017-05-18 14:55:14 +0200

    [NONE] Ensure resolutions ends when handler have been found.
    Ensured handler resolver stops navigating the hierarchy as soon as a
    matching handler is found.

--- org.eclipse.gef.mvc.fx/src/org/eclipse/gef/mvc/fx/gestures/DefaultHandlerResolver.java
+++ org.eclipse.gef.mvc.fx/src/org/eclipse/gef/mvc/fx/gestures/DefaultHandlerResolver.java
@@ -13,15 +13,12 @@ package org.eclipse.gef.mvc.fx.gestures;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
-import org.eclipse.gef.common.adapt.AdapterKey;
 import org.eclipse.gef.common.adapt.IAdaptable;
 import org.eclipse.gef.mvc.fx.domain.IDomain;
 import org.eclipse.gef.mvc.fx.handlers.IHandler;
+import org.eclipse.gef.mvc.fx.parts.IRootPart;
 import org.eclipse.gef.mvc.fx.parts.IVisualPart;
 import org.eclipse.gef.mvc.fx.parts.PartUtils;
 import org.eclipse.gef.mvc.fx.viewer.IViewer;
@@ -31,23 +28,20 @@ import com.google.common.reflect.TypeToken;
 import javafx.scene.Node;
 
 /**
- * The {@link DefaultHandlerResolver} is an {@link IHandlerResolver} that works
- * in two stages:
- * <ol>
- * <li>Examining the active handlers of other gestures to find "multi-gesture"
- * handlers that implement or extend the given target handler type. If any
- * "multi-gesture" handlers are found, the target resolution finishes and these
- * are returned as the target handlers. Otherwise, the target resolution
- * continues with the next stage.
- * <li>Examining the handlers of the visual parts that are contained within the
- * root-to-target path in the visual part hierarchy. All handlers that implement
- * or extend the given target handler type are returned as target handlers. The
- * handlers that are registered on the root part have highest precedence, i.e.
- * they will be executed first, and the handlers that are registered on the
- * target part have lowest precedence, i.e. they will be executed last.
- * </ol>
- * For details, take a look at the
- * {@link #resolve(IGesture, Node, IViewer, Class)} method.
+ * The {@link DefaultHandlerResolver} is the default implementation of
+ * {@link IHandlerResolver}.
+ *
+ * It works in two stages. First, the active handlers of other gestures are
+ * examined to find "multi-gesture" handlers that implement or extend the given
+ * target type. If any "multi-gesture" handlers are found, the target resolution
+ * finishes and these active handlers are returned as the target handlers.
+ * Otherwise, the visual part hierarchy is searched for respective handlers.
+ * Beginning with the visual part that controls the given {@link Node}, the
+ * hierarchy is walked up. The resolution finishes as soon as handlers of the
+ * desired type can be obtained from a respective visual part. If no handlers
+ * have been located before, the {@link IRootPart} is queried last. The
+ * retrieved handlers (if more than one) are (lexicographically) sorted by their
+ * role, so that the target handler selection is deterministic.
  *
  * @author mwienand
  *
@@ -55,74 +49,6 @@ import javafx.scene.Node;
 public class DefaultHandlerResolver extends IAdaptable.Bound.Impl<IDomain>
 		implements IHandlerResolver {
 
-	private final static class AdapterKeyComparator
-			implements Comparator<AdapterKey<?>> {
-		private boolean descending;
-
-		public AdapterKeyComparator(boolean descending) {
-			this.descending = descending;
-		}
-
-		@Override
-		public int compare(AdapterKey<?> lhs, AdapterKey<?> rhs) {
-			int cmp = lhs.getRole().compareTo(rhs.getRole());
-			return descending ? -cmp : cmp;
-		}
-	}
-
-	/**
-	 * The comparator that is used to sort drag adapters.
-	 */
-	private final static AdapterKeyComparator ADAPTER_KEY_COMPARATOR = new AdapterKeyComparator(
-			true);
-
-	/**
-	 * {@inheritDoc}
-	 * <p>
-	 * This strategy works in two stages:
-	 * <ol>
-	 * <li>Examining the active handlers of other gestures to find
-	 * "multi-gesture" handlers that implement or extend the given target type.
-	 * If any "multi-gesture" handlers are found, the target resolution finishes
-	 * and these are returned as the target handlers. Otherwise, the target
-	 * resolution continues with the next stage.
-	 * <li>Examining the handlers of the visual parts that are contained within
-	 * the root-to-target path in the visual part hierarchy. All handlers that
-	 * implement or extend the given target handler type are returned as target
-	 * handlers. The handlers that are registered on the root part have highest
-	 * precedence, i.e. they will be executed first, and the handlers that are
-	 * registered on the target part have lowest precedence, i.e. they will be
-	 * executed last.
-	 * </ol>
-	 * The second stage is structured in two parts:
-	 * <ol>
-	 * <li>Determination of the target part.
-	 * <li>Determination of the target handler based on the target part.
-	 * </ol>
-	 * The first {@link IVisualPart} that controls a {@link Node} within the
-	 * parent hierarchy of the given target {@link Node} is used as the target
-	 * part. If no target part can be found, the root part is used as the target
-	 * part.
-	 * <p>
-	 * Beginning at the root part, and walking down the visual part hierarchy,
-	 * all handlers of the specified type are collected. The handlers that are
-	 * registered on one part are (lexicographically) sorted by their role, so
-	 * that the target handler selection is deterministic.
-	 * <p>
-	 * For example, when you have 3 parts, the root part, an intermediate part,
-	 * and a leaf part, the target handler selection for a handler of type X
-	 * could yield the following results:
-	 * <ol>
-	 * <li>LayeredRootPart.X with role "0"
-	 * <li>LayeredRootPart.X with role "1"
-	 * <li>IntermediatePart.X with role "a"
-	 * <li>IntermediatePart.X with role "b"
-	 * <li>LeafPart.X with role "x"
-	 * <li>LeafPart.X with role "y"
-	 * </ol>
-	 * These handlers would then all be executed/notified about an input event
-	 * by the calling gesture.
-	 */
 	@Override
 	@SuppressWarnings({ "serial", "unchecked" })
 	public <T extends IHandler> List<? extends T> resolve(IGesture gesture,
@@ -174,34 +100,17 @@ public class DefaultHandlerResolver extends IAdaptable.Bound.Impl<IDomain>
 
 		// System.out.println("target part = " + targetPart);
 
-		// collect all handlers on the way from the target part to the
-		// root part
+		// search handlers up the hierarchy
 		IVisualPart<? extends Node> part = targetPart;
 		List<T> handlers = new ArrayList<>();
-		while (part != null) {
+		while (part != null && handlers.isEmpty()) {
 			// System.out.println("[find handlers for " + part + "]");
-			// determine on-drag-handlers
-			Map<AdapterKey<? extends T>, T> partHandlers = part
-					.<T> getAdapters(handlerType);
-
-			// sort descending by role (converted to integer)
-			List<AdapterKey<? extends T>> descendinglySortedKeys = new ArrayList<>(
-					partHandlers.keySet());
-			Collections.sort(descendinglySortedKeys, ADAPTER_KEY_COMPARATOR);
-
-			// add to the list of handlers
-			for (AdapterKey<? extends T> key : descendinglySortedKeys) {
-				// System.out.println("add handler " + key);
-				handlers.add(partHandlers.get(key));
-			}
+			// determine handlers
+			handlers.addAll(part.getAdapters(handlerType).values());
 
 			// go one level up in the hierarchy
 			part = part.getParent();
 		}
-
-		// reverse order in which handlers are returned so that parent handlers
-		// are called before child handlers
-		Collections.reverse(handlers);
 
 		// System.out.println("RETURN in reverse order:");
 		// for (T p : handlers) {
