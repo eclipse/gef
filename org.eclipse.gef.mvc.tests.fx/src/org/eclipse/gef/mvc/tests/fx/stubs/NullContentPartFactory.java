cd11490800832504f6eb29aaa5aca9dd6237b2cd
mwienand <matthias.wienand@itemis.de>
2017-05-23 14:03:15 +0200

    [NONE] Add NullContentPartFactory.

---
+++ org.eclipse.gef.mvc.tests.fx/src/org/eclipse/gef/mvc/tests/fx/stubs/NullContentPartFactory.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2017 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthias Wienand (itemis AG) - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.gef.mvc.tests.fx.stubs;
+
+import java.util.Map;
+
+import org.eclipse.gef.mvc.fx.parts.IContentPart;
+import org.eclipse.gef.mvc.fx.parts.IContentPartFactory;
+
+import javafx.scene.Node;
+
+public class NullContentPartFactory implements IContentPartFactory {
+	@Override
+	public IContentPart<? extends Node> createContentPart(Object content, Map<Object, Object> contextMap) {
+		return null;
+	}
+}
