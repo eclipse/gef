c9550d41ebfda668aee3d66894bae1512964c61d
anyssen <nyssen@itemis.de>
2017-05-29 14:27:21 +0200

    [NONE] Add package-info to actions package.

---
+++ org.eclipse.gef.mvc.fx.ui/src/org/eclipse/gef/mvc/fx/ui/actions/package-info.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG and others.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Matthias Wienand (itemis AG) - initial text
+ *
+ *******************************************************************************/
+/**
+ * This package contains specific {@link org.eclipse.jface.action.Action} and
+ * {@link org.eclipse.jface.action.ContributionItem} implementations related to
+ * zooming, scrolling, selecting, and deleting, as well as
+ * {@link org.eclipse.ui.actions.ActionGroup} that bundle related actions.
+ */
+package org.eclipse.gef.mvc.fx.ui.actions;
