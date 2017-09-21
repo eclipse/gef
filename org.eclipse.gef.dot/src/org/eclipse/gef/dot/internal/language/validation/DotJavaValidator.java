88ce9205d13e30f6242f61995c68bb04ee85ee14
Zoey Gerrit Prigge <zoey.prigge@uni-duesseldorf.de>
2017-09-21 17:06:27 +0200

    [454629] Record label validation support
    - Implemented validation methods for the recordLabel in
    DotJavaValidator.java
    - Introduced ConvertingValidationMessageAcceptor to accept Issues in the
    scope of the subgrammar and register them in the scope of the Dot
    grammar.
    - Handling calculating offset in ConvertingValidationMessageAcceptor
    - Implemented further validation test cases:
    - Tests for other semantic issue (no node returned case) and Syntax
    issues added.
    
    
    Signed-off-by: Zoey Gerrit Prigge <zoey.prigge@uni-duesseldorf.de>
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=454629

--- org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/validation/DotJavaValidator.java
+++ org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/validation/DotJavaValidator.java
@@ -12,18 +12,26 @@
  *     Tamas Miklossy  - Add support for arrowType edge decorations (bug #477980)
  *                     - Add support for polygon-based node shapes (bug #441352)
  *                     - Add support for all dot attributes (bug #461506)
+ *     Zoey Gerrit Prigge - Add support for record label attributes (bug #454629)
  *
  *******************************************************************************/
 
 package org.eclipse.gef.dot.internal.language.validation;
 
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.common.reflect.ReflectionUtils;
 import org.eclipse.gef.dot.internal.DotAttributes;
 import org.eclipse.gef.dot.internal.DotAttributes.Context;
 import org.eclipse.gef.dot.internal.language.DotAstHelper;
+import org.eclipse.gef.dot.internal.language.DotRecordLabelStandaloneSetup;
 import org.eclipse.gef.dot.internal.language.dot.AttrList;
 import org.eclipse.gef.dot.internal.language.dot.AttrStmt;
 import org.eclipse.gef.dot.internal.language.dot.Attribute;
@@ -35,13 +43,19 @@ import org.eclipse.gef.dot.internal.language.dot.EdgeRhsSubgraph;
 import org.eclipse.gef.dot.internal.language.dot.GraphType;
 import org.eclipse.gef.dot.internal.language.dot.NodeStmt;
 import org.eclipse.gef.dot.internal.language.shape.PolygonBasedNodeShape;
+import org.eclipse.gef.dot.internal.language.shape.RecordBasedNodeShape;
 import org.eclipse.gef.dot.internal.language.style.NodeStyle;
 import org.eclipse.gef.dot.internal.language.terminals.ID;
 import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.parser.IParseResult;
+import org.eclipse.xtext.parser.IParser;
+import org.eclipse.xtext.validation.AbstractInjectableValidator;
 import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.RangeBasedDiagnostic;
+
+import com.google.inject.Injector;
 
 /**
  * Provides DOT-specific validation rules.
@@ -237,5 +251,60 @@ public class DotJavaValidator extends AbstractDotJavaValidator {
 			error("EdgeOp '->' may only be used in directed graphs.",
 					DotPackage.eINSTANCE.getEdgeRhs_Op());
 		}
+	}
+
+	/**
+	 * Use to run the recordLabel subgrammar validation on relevant labels
+	 * (label attributes on Nodes where a recordBased label shape attribute
+	 * exists).
+	 * 
+	 * @param attribute
+	 *            The attribute to validate.
+	 */
+	@Check
+	public void checkRecordBasedNodeShapeValue(Attribute attribute) {
+		if (DotAttributes.getContext(attribute).equals(Context.NODE)
+				&& attribute.getName().toValue()
+						.equals(DotAttributes.LABEL__GCNE)) {
+			String shapeValue = DotAstHelper.getDependedOnAttributeValue(
+					attribute, DotAttributes.SHAPE__N);
+			if (RecordBasedNodeShape.get(shapeValue) != null) {
+				doRecordLabelValidation(attribute);
+			}
+		}
+	}
+
+	private void doRecordLabelValidation(Attribute attribute) {
+		Injector recordLabelInjector = new DotRecordLabelStandaloneSetup()
+				.createInjectorAndDoEMFRegistration();
+		DotRecordLabelJavaValidator validator = recordLabelInjector
+				.getInstance(DotRecordLabelJavaValidator.class);
+		IParser parser = recordLabelInjector.getInstance(IParser.class);
+
+		ConvertingValidationMessageAcceptor messageAcceptor = new ConvertingValidationMessageAcceptor(
+				attribute, DotPackage.Literals.ATTRIBUTE__VALUE,
+				attribute.getName().toString(), getMessageAcceptor(),
+				"\"".length());
+
+		validator.setMessageAcceptor(messageAcceptor);
+
+		IParseResult result = parser
+				.parse(new StringReader(attribute.getValue().toValue()));
+
+		for (INode error : result.getSyntaxErrors())
+			messageAcceptor.acceptSyntaxError(error);
+
+		Map<Object, Object> validationContext = new HashMap<Object, Object>();
+		validationContext.put(AbstractInjectableValidator.CURRENT_LANGUAGE_NAME,
+				ReflectionUtils.getPrivateFieldValue(validator,
+						"languageName"));
+
+		// validate both the children (loop) and root element
+		Iterator<EObject> iterator = result.getRootASTElement().eAllContents();
+		while (iterator.hasNext())
+			validator.validate(iterator.next(), null/* diagnostic chain */,
+					validationContext);
+
+		validator.validate(result.getRootASTElement(), null, validationContext);
 	}
 }
