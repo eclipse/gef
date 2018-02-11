10c65d7b719fe40a0fadf4c65d6bffe9c0526967
Tamas Miklossy <miklossy@itemis.de>
2018-02-11 10:44:19 +0100

    [NONE] Adapt Copyright header in the Dot2ZestGraphCopier.java file.

--- org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/Dot2ZestGraphCopier.java
+++ org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/Dot2ZestGraphCopier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 itemis AG and others.
+ * Copyright (c) 2017, 2018 itemis AG and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *    Alexander Nyßen (itemis AG) - initial API and implementation
+ *    Tamas Miklossy  (itemis AG) - minor improvements
  *
  *******************************************************************************/
 package org.eclipse.gef.dot.internal.ui;
