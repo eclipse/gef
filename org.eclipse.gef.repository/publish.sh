9bd676c6be0bf2d582d5cbdbb012c060a244a510
mwienand <matthias.wienand@itemis.de>
2018-07-17 09:55:51 +0200

    NONE: Update eclipse release referred to in publish.sh.

--- org.eclipse.gef.repository/publish.sh
+++ org.eclipse.gef.repository/publish.sh
@@ -169,8 +169,8 @@ cd $tmpDir
 
 # Download and prepare Eclipse SDK, which is needed to merge update site and postprocess repository 
 echo "Downloading eclipse to $PWD"
-cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.7.1-201709061700/eclipse-SDK-4.7.1-linux-gtk-x86_64.tar.gz .
-tar -xvzf eclipse-SDK-4.7.1-linux-gtk-x86_64.tar.gz
+cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.8-201806110500/eclipse-SDK-4.8-linux-gtk-x86_64.tar.gz .
+tar -xvzf eclipse-SDK-4.8-linux-gtk-x86_64.tar.gz
 cd eclipse
 chmod 700 eclipse
 cd ..
@@ -184,7 +184,7 @@ echo "Installing WTP Releng tools"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/webtools/releng/repository/ -installIUs org.eclipse.wtp.releng.tools.feature.feature.group
 # Clean up
 echo "Cleaning up"
-rm eclipse-SDK-4.7.1-linux-gtk-x86_64.tar.gz
+rm eclipse-SDK-4.8-linux-gtk-x86_64.tar.gz
 
 # Generate drop files
 if [ "$dropFiles" = y ];
