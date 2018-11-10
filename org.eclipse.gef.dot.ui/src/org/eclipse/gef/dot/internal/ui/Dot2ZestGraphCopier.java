e00cb9d9884f6b92646254da27db14d4a7023931
Tamas Miklossy <miklossy@itemis.de>
2018-11-10 16:10:56 +0100

    [NONE] Refactoring the copy method of the Dot2ZestGraphCopier class.
    - Split up the implementation of the Dot2ZestGraphCopier copy method
    into several smaller ones to improve readability.

--- org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/Dot2ZestGraphCopier.java
+++ org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/Dot2ZestGraphCopier.java
@@ -8,13 +8,14 @@
  *
  * Contributors:
  *    Alexander Nyßen (itemis AG) - initial API and implementation
- *    Tamas Miklossy  (itemis AG) - minor improvements
+ *    Tamas Miklossy  (itemis AG) - minor improvements, refactoring
  *
  *******************************************************************************/
 package org.eclipse.gef.dot.internal.ui;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.gef.dot.internal.DotAttributes;
 import org.eclipse.gef.dot.internal.language.rect.Rect;
@@ -45,95 +46,117 @@ public class Dot2ZestGraphCopier extends GraphCopier {
 	}
 
 	@Override
-	public Graph copy(Graph graph) {
-		Graph copiedGraph = super.copy(graph);
-		// post-process graph to 'flatten' non-cluster subgraphs
-		List<org.eclipse.gef.graph.Node> replacementNodes = new ArrayList<>();
-		for (Node node : copiedGraph.getNodes()) {
-			if (node.getNestedGraph() == null) {
-				replacementNodes.add(node);
-			} else {
-				// retrieve copied dot node to decide whether we have a cluster
-				// (as the name of the nested graph will not be copied)
-				Node dotNode = null;
-				for (Node n : getInputToOutputNodeMap().keySet()) {
-					if (getInputToOutputNodeMap().get(n) == node) {
-						dotNode = n;
-						break;
-					}
-				}
-				// only support clusters in native mode
-				if (DotAttributes.isCluster(dotNode)
-						&& !getAttributeCopier().options().emulateLayout) {
-					// ensure cluster nodes get lowest z-order
-					replacementNodes.add(0, node);
+	public Graph copy(Graph dotGraph) {
+		Graph zestGraph = super.copy(dotGraph);
 
-					// initialize a rectangle shape
-					ZestProperties.setShape(node,
-							new GeometryNode<>(new Rectangle()));
+		// post-process graph to handle nested graphs
+		List<Node> zestNodeReplacements = new ArrayList<>();
+		for (Node zestNode : zestGraph.getNodes()) {
+			processZestNode(zestNode, zestGraph, zestNodeReplacements);
+		}
 
-					// for cluster nodes position and size are determined using
-					// the bounding box (specified as attribute of nested graph)
-					Rect bb = DotAttributes
-							.getBbParsed(dotNode.getNestedGraph());
+		// add cluster nodes as lowest in z-order
+		return new Graph(zestGraph.attributesProperty(), zestNodeReplacements,
+				zestGraph.edgesProperty());
+	}
 
-					Point zestPosition = null;
-					Dimension zestSize = null;
-					if (bb != null) {
-						zestPosition = new Point(bb.getLlx(), bb.getUry());
-						zestSize = new Dimension(bb.getUrx() - bb.getLlx(),
-								bb.getLly() - bb.getUry());
-						ZestProperties.setPosition(node, zestPosition);
-						ZestProperties.setSize(node, zestSize);
-					}
+	protected void processZestNode(Node zestNode, Graph zestGraph,
+			List<Node> zestNodeReplacements) {
+		if (zestNode.getNestedGraph() == null) {
+			zestNodeReplacements.add(zestNode);
+		} else {
+			processNestedGraph(zestNode, zestGraph, zestNodeReplacements);
+		}
+	}
 
-					// determine label for cluster
-					String dotLabel = DotAttributes
-							.getLabel(dotNode.getNestedGraph());
-					if ("\\G".equals(dotLabel)) { //$NON-NLS-1$
-						dotLabel = DotAttributes
-								._getName(dotNode.getNestedGraph());
-					}
-					if (dotLabel != null) {
-						ZestProperties.setExternalLabel(node, dotLabel);
-						// XXX: We use an external label and position it inside
-						// the node, as Zest will always center the label.
-						// TODO: change this as soon as different label
-						// positions are supported by Zest
-						if (zestPosition != null && zestSize != null) {
-							double xOffset = 0.5 * (zestSize.width
-									- Dot2ZestAttributesConverter
-											.computeZestLabelSize(
-													dotLabel).width);
-							double yOffset = 4;
-							ZestProperties.setExternalLabelPosition(node,
-									zestPosition.getTranslated(xOffset,
-											yOffset));
-						}
-					}
-				}
-				Graph subgraph = node.getNestedGraph();
-				for (Node n : subgraph.getNodes()) {
-					// 'unfold' all incoming and outgoing edges (they have
-					// to refer to the nested nodes)
-					for (Edge e : node.getIncomingEdges()) {
-						Edge edgeCopy = copyEdge(e);
-						edgeCopy.setTarget(n);
-						copiedGraph.getEdges().add(edgeCopy);
-					}
-					for (Edge e : node.getOutgoingEdges()) {
-						Edge edgeCopy = copyEdge(e);
-						edgeCopy.setSource(n);
-						copiedGraph.getEdges().add(edgeCopy);
-					}
-				}
-				replacementNodes.addAll(subgraph.getNodes());
-				copiedGraph.getEdges().addAll(subgraph.getEdges());
-				node.setNestedGraph(null);
+	protected void processNestedGraph(Node zestNode, Graph zestGraph,
+			List<Node> zestNodeReplacements) {
+		// retrieve copied dot node to decide whether we have a cluster
+		// (as the name of the nested graph will not be copied)
+		Node dotNode = getDotNode(zestNode);
+
+		// only support clusters in native mode
+		if (DotAttributes.isCluster(dotNode)
+				&& !getAttributeCopier().options().emulateLayout) {
+			processCluster(dotNode, zestNode, zestNodeReplacements);
+		}
+		processSubgraph(zestNode, zestGraph, zestNodeReplacements);
+
+		zestNode.setNestedGraph(null);
+	}
+
+	protected void processCluster(Node dotNode, Node zestNode,
+			List<Node> zestNodeReplacements) {
+		zestNodeReplacements.add(0, zestNode);
+
+		// initialize a rectangle shape
+		ZestProperties.setShape(zestNode, new GeometryNode<>(new Rectangle()));
+
+		// for cluster nodes position and size are determined using
+		// the bounding box (specified as attribute of nested graph)
+		Rect bb = DotAttributes.getBbParsed(dotNode.getNestedGraph());
+
+		Point zestPosition = null;
+		Dimension zestSize = null;
+		if (bb != null) {
+			zestPosition = new Point(bb.getLlx(), bb.getUry());
+			zestSize = new Dimension(bb.getUrx() - bb.getLlx(),
+					bb.getLly() - bb.getUry());
+			ZestProperties.setPosition(zestNode, zestPosition);
+			ZestProperties.setSize(zestNode, zestSize);
+		}
+
+		// determine label for cluster
+		String dotLabel = DotAttributes.getLabel(dotNode.getNestedGraph());
+		if ("\\G".equals(dotLabel)) { //$NON-NLS-1$
+			dotLabel = DotAttributes._getName(dotNode.getNestedGraph());
+		}
+		if (dotLabel != null) {
+			ZestProperties.setExternalLabel(zestNode, dotLabel);
+			// XXX: We use an external label and position it inside
+			// the node, as Zest will always center the label.
+			// TODO: change this as soon as different label
+			// positions are supported by Zest
+			if (zestPosition != null && zestSize != null) {
+				double xOffset = 0.5
+						* (zestSize.width - Dot2ZestAttributesConverter
+								.computeZestLabelSize(dotLabel).width);
+				double yOffset = 4;
+				ZestProperties.setExternalLabelPosition(zestNode,
+						zestPosition.getTranslated(xOffset, yOffset));
 			}
 		}
-		// add cluster nodes as lowest in z-order
-		return new Graph(copiedGraph.attributesProperty(), replacementNodes,
-				copiedGraph.edgesProperty());
+	}
+
+	protected void processSubgraph(Node zestNode, Graph zestGraph,
+			List<Node> zestNodeReplacements) {
+		Graph subgraph = zestNode.getNestedGraph();
+		for (Node n : subgraph.getNodes()) {
+			// 'unfold' all incoming and outgoing edges (they have
+			// to refer to the nested nodes)
+			for (Edge e : zestNode.getIncomingEdges()) {
+				Edge edgeCopy = copyEdge(e);
+				edgeCopy.setTarget(n);
+				zestGraph.getEdges().add(edgeCopy);
+			}
+			for (Edge e : zestNode.getOutgoingEdges()) {
+				Edge edgeCopy = copyEdge(e);
+				edgeCopy.setSource(n);
+				zestGraph.getEdges().add(edgeCopy);
+			}
+		}
+		zestNodeReplacements.addAll(subgraph.getNodes());
+		zestGraph.getEdges().addAll(subgraph.getEdges());
+	}
+
+	protected Node getDotNode(Node zestNode) {
+		Map<Node, Node> inputToOutputMap = getInputToOutputNodeMap();
+		for (Node input : inputToOutputMap.keySet()) {
+			Node output = inputToOutputMap.get(input);
+			if (output == zestNode) {
+				return input;
+			}
+		}
+		return null;
 	}
 }
