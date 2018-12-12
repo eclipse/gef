fa659d3218c860aa7f6336a27753259e94c67d8a
Kyle Girard <kyle@kdmanalytics.com>
2018-12-12 12:17:37 -0500

    [533872]: Implement DragDropGesture to support JavaFX drag and drop events.
        - Create DragDropGesture class to handle JavaFX drag drop gestures
        - Create IOnDragDropHandler interface to allow objects to be notified of drag and drop events
    
        Signed-off-by: Kyle Girard <kyle@kdmanalytics.com>
        Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=533872

---
+++ org.eclipse.gef.mvc.fx/src/org/eclipse/gef/mvc/fx/handlers/IOnDragDropHandler.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2018 KDM Analytics Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Kyle Girard (KDM Analytics Inc.) - initial API and implementation
+ *
+ *******************************************************************************/
+
+package org.eclipse.gef.mvc.fx.handlers;
+
+import org.eclipse.gef.mvc.fx.handlers.IHandler;
+import org.eclipse.gef.mvc.fx.gestures.DragDropGesture;
+import javafx.scene.input.DragEvent;
+
+/**
+ * An interaction handler that implements the {@link IOnDragDropHandler} interface will be notified
+ * about dragOver and dragDrop events by the {@link DragDropGesture} .
+ *
+ * @author kgirard
+ *
+ */
+public interface IOnDragDropHandler extends IHandler {
+
+	/**
+	 * This callback method is invoked when the user performs a drag that enters a hosts boundaries.
+	 * 
+	 * @param event
+	 */
+	void dragEntered(DragEvent event);
+
+	/**
+	 * This callback method is invoked when the user performs a drag that exits a hosts boundaries.
+	 * 
+	 * @param event
+	 */
+	void dragExited(DragEvent event);
+
+	/**
+	 * This callback method is invoked when the user performs a dragOver on the host
+	 *
+	 * @param event
+	 */
+	void dragOver(DragEvent event);
+
+	/**
+	 * This callback method is invoked when the user performs a dragDropped on the host
+	 *
+	 * @param event
+	 */
+	void dragDropped(DragEvent event);
+
+}
