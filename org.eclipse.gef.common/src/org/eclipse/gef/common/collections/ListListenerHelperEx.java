d5dd1b9a595f404d27a0956375754638a39d2e81
mwienand <matthias.wienand@itemis.de>
2019-03-15 16:52:21 +0100

    [518221] Update copyright header.

--- org.eclipse.gef.common/src/org/eclipse/gef/common/collections/ListListenerHelperEx.java
+++ org.eclipse.gef.common/src/org/eclipse/gef/common/collections/ListListenerHelperEx.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2016 itemis AG and others.
+ * Copyright (c) 2016, 2019 itemis AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Alexander Nyßen (itemis AG) - initial API and implementation
+ *     Matthias Wienand (itemis AG) - remove checkCursor()
  *
  *******************************************************************************/
 package org.eclipse.gef.common.collections;
