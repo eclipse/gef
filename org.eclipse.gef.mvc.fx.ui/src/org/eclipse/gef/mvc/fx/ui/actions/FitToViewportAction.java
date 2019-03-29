1f034898a844acd3ac0f044717b36ba856b6baff
Tamas Miklossy <miklossy@itemis.de>
2019-03-29 20:34:57 +0100

    [NONE] Fix typo in the MVC FX UI FitToViewportAction class.

--- org.eclipse.gef.mvc.fx.ui/src/org/eclipse/gef/mvc/fx/ui/actions/FitToViewportAction.java
+++ org.eclipse.gef.mvc.fx.ui/src/org/eclipse/gef/mvc/fx/ui/actions/FitToViewportAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 itemis AG and others.
+ * Copyright (c) 2017, 2019 itemis AG and others.
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
@@ -70,7 +70,7 @@ public class FitToViewportAction extends AbstractViewerAction {
 		InfiniteCanvas infiniteCanvas = getInfiniteCanvas();
 		if (infiniteCanvas == null) {
 			throw new IllegalStateException(
-					"Cannot perform FitToSizeAction, because no InfiniteCanvas can be determiend.");
+					"Cannot perform FitToViewportAction, because no InfiniteCanvas can be determined.");
 		}
 		if (infiniteCanvas.getContentBounds().isEmpty()) {
 			// nothing to do
