35f9a26ff535c9a621ee0d62219ffac1308d1ea9
mwienand <matthias.wienand@itemis.de>
2019-05-09 18:29:38 +0200

    [NONE] Copy newer Eclipse into PWD during promotion.

--- org.eclipse.gef.repository/publish.sh
+++ org.eclipse.gef.repository/publish.sh
@@ -169,7 +169,7 @@ cd $tmpDir
 
 # Download and prepare Eclipse SDK, which is needed to merge update site and postprocess repository 
 echo "Downloading eclipse to $PWD"
-cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.8-201806110500/eclipse-SDK-4.8-linux-gtk-x86_64.tar.gz .
+cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.9-201809060745/eclipse-SDK-4.9-linux-gtk-x86_64.tar.gz .
 tar -xvzf eclipse-SDK-4.8-linux-gtk-x86_64.tar.gz
 cd eclipse
 chmod 700 eclipse
