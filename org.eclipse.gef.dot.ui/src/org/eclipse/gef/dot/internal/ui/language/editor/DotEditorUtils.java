dce4c072d739a76ac25d2ce18739ffcdbc0fc5c3
Tamas Miklossy <miklossy@itemis.de>
2020-01-31 19:52:15 +0100

    [559286] Update Copyright headers.

--- org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/language/editor/DotEditorUtils.java
+++ org.eclipse.gef.dot.ui/src/org/eclipse/gef/dot/internal/ui/language/editor/DotEditorUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 itemis AG and others.
+ * Copyright (c) 2018, 2020 itemis AG and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
