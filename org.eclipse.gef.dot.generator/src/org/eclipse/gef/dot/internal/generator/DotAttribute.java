852260f087053fefe9c0ffd64eadc0fe1d7d8cf0
Tamas Miklossy <miklossy@itemis.de>
2020-05-08 09:15:51 +0200

    [NONE] Cleaning up the GEF DOT DotAttribute class.
    - Remove unused import.
    - Formatting source code.

--- org.eclipse.gef.dot.generator/src/org/eclipse/gef/dot/internal/generator/DotAttribute.java
+++ org.eclipse.gef.dot.generator/src/org/eclipse/gef/dot/internal/generator/DotAttribute.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016, 2017 itemis AG and others.
+ * Copyright (c) 2016, 2020 itemis AG and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,7 +13,7 @@ package org.eclipse.gef.dot.internal.generator;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-import org.eclipse.gef.dot.internal.generator.DotAttributeProcessor;
+
 import org.eclipse.xtend.lib.macro.Active;
 
 /**
@@ -22,14 +22,15 @@ import org.eclipse.xtend.lib.macro.Active;
 @Target(ElementType.FIELD)
 @Active(DotAttributeProcessor.class)
 public @interface DotAttribute {
-  /**
-   * A string matching an ID.Type to use for this attribute
-   */
-  public String[] rawType() default { "" };
-  /**
-   * Type of the attribute.
-   *
-   * @return The type of the attribute.
-   */
-  public Class<?>[] parsedType();
+	/**
+	 * A string matching an ID.Type to use for this attribute
+	 */
+	public String[] rawType() default { "" };
+
+	/**
+	 * Type of the attribute.
+	 *
+	 * @return The type of the attribute.
+	 */
+	public Class<?>[] parsedType();
 }
