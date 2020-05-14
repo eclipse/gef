f1ec8e446939f8e927349c087c4b072b306493b6
LENOVO <zprigge@itemis.com>
2020-05-14 16:54:28 +0200

    [562913] Relicense Copyright Headers to EPL2
    - use bash script to change headers to EPL2 (script 1/5)
    
    Signed-Off-By: Zoey Prigge <zprigge@itemis.com>

--- org.eclipse.gef.geometry/src/org/eclipse/gef/geometry/euclidean/Straight.java
+++ org.eclipse.gef.geometry/src/org/eclipse/gef/geometry/euclidean/Straight.java
@@ -2,10 +2,11 @@
  * Copyright (c) 2010, 2016 Research Group Software Construction,
  *                          RWTH Aachen University and others.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     Alexander Nyßen (Research Group Software Contruction, RWTH Aachen University) - initial API and implementation
--- org.eclipse.gef.layout/src/org/eclipse/gef/layout/algorithms/AlgorithmHelper.java
+++ org.eclipse.gef.layout/src/org/eclipse/gef/layout/algorithms/AlgorithmHelper.java
@@ -1,10 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2005, 2016 The Chisel Group and others.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
  * 
  * Contributors: Ian Bull (The Chisel Group) - initial API and implementation
  *               Mateusz Matela - "Tree Views for Zest" contribution, Google Summer of Code 2009
