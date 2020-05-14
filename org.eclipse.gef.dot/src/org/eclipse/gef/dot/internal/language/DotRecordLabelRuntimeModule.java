f51b213c0db9fc6596072ca9546d22f5ac4228a5
Zoey Prigge <zprigge@itemis.com>
2020-05-14 12:52:26 +0200

    [562913] Manual fixups relicensing to EPL2
    - adapted target files and tpd files
    - manually fixed some issues where scripts didn't work
    - fixed some autoformatted headers in pom.xml
    - fixed headers with typos
    - fixed some sample dot-file headers
    - fixed windows line endings
    
    Signed-off-by: Zoey Prigge <zprigge@itemis.com>

--- org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/DotRecordLabelRuntimeModule.java
+++ org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/DotRecordLabelRuntimeModule.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2017, 2020 itemis AG and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Zoey Gerrit Prigge  - initial API and implementation (bug #454629)
+ *     Zoey Gerrit Prigge - initial API and implementation (bug #454629)
  *
  *******************************************************************************/
 package org.eclipse.gef.dot.internal.language;
